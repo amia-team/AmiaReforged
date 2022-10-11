59b4d8814522e1e5a461db34b0261520409b0d2b
Caelan L. Talmadge <34071000+cltalmadge@users.noreply.github.com>
2022-10-11 19:05:39 -0400

    Added AmiaReforged.Core.Specs

---
+++ AmiaReforged.Core.Specs/Drivers/Driver.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace AmiaReforged.Core.Specs.Drivers
+{
+    public class Driver
+    {
+
+    }
+}
---
+++ AmiaReforged.Core.Specs/Hooks/Hook.cs
@@ -0,0 +1,11 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace AmiaReforged.Core.Specs.Hooks
+{
+    [Binding]
+    public class Hooks
+    {
+        
+    }
+}
