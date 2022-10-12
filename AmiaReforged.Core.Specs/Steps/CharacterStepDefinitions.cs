e25e5017a7627fd8b0d408ea11b1af11ca96e277
Caelan L. Talmadge <34071000+cltalmadge@users.noreply.github.com>
2022-10-11 20:00:26 -0400

    Filled out step: GivenAPlayerWithThePublicCdKey

--- AmiaReforged.Core.Specs/Steps/CharacterStepDefinitions.cs
+++ AmiaReforged.Core.Specs/Steps/CharacterStepDefinitions.cs
@@ -1,12 +1,30 @@
-﻿namespace Amia.Core.Specs.Steps;
+﻿using AmiaReforged.Core.Entities;
+using BoDi;
+
+namespace Amia.Core.Specs.Steps;
 
 [Binding]
 public class CharacterStepDefinitions
 {
+    // IObjectContainer is a BoDi container that is used to resolve dependencies.
+    private IObjectContainer _container;
+    private AmiaPlayer _player;
+    
+    // Dependency injection container is passed in the constructor
+    public CharacterStepDefinitions(IObjectContainer container)
+    {
+        _container = container;
+    }
+
+    [BeforeScenario]
+    public void BeforeScenario()
+    {
+    }
+
     [Given(@"a player with the public CD key '(.*)'")]
     public void GivenAPlayerWithThePublicCdKey(string p0)
     {
-        ScenarioContext.StepIsPending();
+        _player = new AmiaPlayer(p0);
     }
 
     [When(@"the player makes a character named ""(.*)""")]
