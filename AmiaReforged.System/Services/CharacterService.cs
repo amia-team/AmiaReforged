c5260772be644ab95af2fc9532bd0a95cb42dd65
Caelan L. Talmadge <34071000+cltalmadge@users.noreply.github.com>
2022-10-15 13:09:42 -0400

    Added a test project Added CharacterService to System
    Co-authored-by: Jes A Condrey <Jesacondrey@users.noreply.github.com>
    Co-authored-by: Albacksen <Albacksen@users.noreply.github.com>

---
+++ AmiaReforged.System/Services/CharacterService.cs
@@ -0,0 +1,28 @@
+using Anvil.API;
+using Anvil.API.Events;
+using Anvil.Services;
+using NLog;
+
+namespace AmiaReforged.System.Services;
+
+public class CharacterService {
+    private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+    /**
+    * Class CharacterService
+    * Once the character is created, create a reference 
+    */
+    public CharacterService() {
+        Log.Info("Character Service initialized.");
+        
+    }
+
+    public void StoreCharacter(String PCKey, String CDKey) {
+        // We'll be storing:
+        // PCKey (that little wand in your inventory)
+        // CDKey
+        // Character Name
+        // 
+    }
+    
+}
