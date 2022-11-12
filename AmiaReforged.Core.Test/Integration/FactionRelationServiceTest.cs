caca8f065cccd7767822a94f69b8acf2c774a89c
Caelan L. Talmadge <34071000+cltalmadge@users.noreply.github.com>
2022-11-12 15:45:28 -0500

    Added FactionRelationService with tests

---
+++ AmiaReforged.Core.Test/Integration/FactionRelationServiceTest.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AmiaReforged.Core.Models;
+using AmiaReforged.System.Services;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AmiaReforged.Core.Test.Integration;
+
+public class FactionRelationServiceTest : IDisposable
+{
+    private ITestOutputHelper _outputHelper;
+    private readonly FactionRelationService _factionRelationService;
+    private readonly FactionService _factionService;
+
+    public FactionRelationServiceTest(ITestOutputHelper outputHelper)
+    {
+        _outputHelper = outputHelper;
+        _factionService = new FactionService(new CharacterService());
+        _factionRelationService = new FactionRelationService(_factionService);
+    }
+
+    [Fact]
+    public async void ShouldAddFactionRelation()
+    {
+        Faction factionOne = new()
+        {
+            Name = "Faction One",
+            Description = "Faction One Description",
+            Members = new List<Guid>()
+        };
+        Faction factionTwo = new()
+        {
+            Name = "Faction Two",
+            Description = "Faction Two Description"
+        };
+
+        await _factionService.AddFaction(factionOne);
+        await _factionService.AddFaction(factionTwo);
+
+        FactionRelation relation = new()
+        {
+            Id = Guid.NewGuid(),
+            FactionName = factionOne.Name,
+            TargetFactionName = factionTwo.Name,
+            Relation = -50
+        };
+
+        await _factionRelationService.AddFactionRelation(relation);
+
+        IEnumerable<FactionRelation> actual = await _factionRelationService.GetRelationsForFaction(factionOne);
+
+        actual.ToList().ToList().Should().NotBeEmpty("because we added a relation").And
+            .Contain(relation, "because we added a relation");
+    }
+
+    [Fact]
+    public async void ShouldNotAddRelationForNonExistentFaction()
+    {
+        Faction nonExistentFaction = new()
+        {
+            Name = "Non Existent Faction",
+            Description = "Non Existent Faction Description"
+        };
+
+        FactionRelation relation = new()
+        {
+            Id = Guid.NewGuid(),
+            FactionName = nonExistentFaction.Name,
+            TargetFactionName = "Non Existent Faction",
+            Relation = -50
+        };
+
+        await _factionRelationService.AddFactionRelation(relation);
+
+
+        IEnumerable<FactionRelation> actual = await _factionRelationService.GetRelationsForFaction(nonExistentFaction);
+
+        actual.Should().BeEmpty("because we added a relation for a non existent faction");
+    }
+
+    [Fact]
+    public async void ShouldNotAddRelationForNonExistentTargetFaction()
+    {
+        Faction factionOne = new()
+        {
+            Name = "Faction Exists",
+            Description = "Faction does exist",
+            Members = new List<Guid>()
+        };
+
+        await _factionService.AddFaction(factionOne);
+
+        FactionRelation relation = new()
+        {
+            Id = Guid.NewGuid(),
+            FactionName = factionOne.Name,
+            TargetFactionName = "Non Existent Faction",
+            Relation = -50
+        };
+
+        await _factionRelationService.AddFactionRelation(relation);
+
+        IEnumerable<FactionRelation> actual = await _factionRelationService.GetRelationsForFaction(factionOne);
+
+        actual.Should().BeEmpty("because we added a relation for a non existent target faction");
+    }
+
+    [Fact]
+    public async Task ShouldUpdateFactionRelationship()
+    {
+        Faction factionOne = new()
+        {
+            Name = "A Faction To Update",
+            Description = "A Faction To Update",
+            Members = new List<Guid>()
+        };
+
+        Faction factionTwo = new()
+        {
+            Name = "Faction Doesn't Have A Relationship With Faction One",
+            Description = "Faction Two Description",
+            Members = new List<Guid>()
+        };
+
+        await _factionService.AddFaction(factionOne);
+        await _factionService.AddFaction(factionTwo);
+
+        FactionRelation relation = new()
+        {
+            Id = Guid.NewGuid(),
+            FactionName = factionOne.Name,
+            TargetFactionName = factionTwo.Name,
+            Relation = -50
+        };
+
+        await _factionRelationService.AddFactionRelation(relation);
+
+        relation.Relation = 50;
+
+        await _factionRelationService.UpdateFactionRelation(relation);
+
+        IEnumerable<FactionRelation> actual = await _factionRelationService.GetRelationsForFaction(factionOne);
+
+        IEnumerable<FactionRelation> factionRelations = actual as FactionRelation[] ?? actual.ToArray();
+        factionRelations.Should().Contain(relation, "because we updated the relation");
+        factionRelations.First().Relation.Should().Be(relation.Relation);
+    }
+
+    public void Dispose() => GC.SuppressFinalize(this);
+}
---
+++ AmiaReforged.System/Services/FactionRelationService.cs
@@ -0,0 +1,87 @@
+﻿using AmiaReforged.Core;
+using AmiaReforged.Core.Models;
+using AmiaReforged.System.Helpers;
+using Anvil.Services;
+using NLog;
+
+namespace AmiaReforged.System.Services;
+
+[ServiceBinding(typeof(FactionRelation))]
+public class FactionRelationService
+{
+    private readonly FactionService _factionService;
+    private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+    private readonly AmiaContext _ctx;
+    private readonly NwTaskHelper _taskHelper;
+
+    public FactionRelationService(FactionService factionService)
+    {
+        _factionService = factionService;
+        _ctx = new AmiaContext();
+        _taskHelper = new NwTaskHelper();
+    }
+
+    public async Task AddFactionRelation(FactionRelation relation)
+    {
+        Faction? faction = await _factionService.GetFactionByName(relation.FactionName);
+        Faction? targetFaction = await _factionService.GetFactionByName(relation.TargetFactionName);
+
+        ReportNonExistentFactions(relation, faction, targetFaction);
+
+        await AddAsyncOnlyIfFactionsExist(relation, faction, targetFaction);
+
+        await _taskHelper.TrySwitchToMainThread();
+    }
+
+    private static void ReportNonExistentFactions(FactionRelation relation, Faction? faction, Faction? targetFaction)
+    {
+        if (faction is null)
+        {
+            Log.Error($"Faction {relation.FactionName} not found.");
+        }
+
+        if (targetFaction is null)
+        {
+            Log.Error($"Target Faction {relation.TargetFactionName} not found.");
+        }
+    }
+
+    private async Task AddAsyncOnlyIfFactionsExist(FactionRelation relation, Faction? faction, Faction? targetFaction)
+    {
+        if (faction is not null && targetFaction is not null)
+        {
+            try
+            {
+                await _ctx.FactionRelations.AddAsync(relation);
+                await _ctx.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, "Error while adding faction relation");
+            }
+        }
+    }
+
+    public async Task<IEnumerable<FactionRelation>> GetRelationsForFaction(Faction faction)
+    {
+        IEnumerable<FactionRelation> relation =
+            _ctx.FactionRelations.Where(f1 => f1.FactionName == faction.Name).AsEnumerable();
+        await _taskHelper.TrySwitchToMainThread();
+        return relation;
+    }
+
+    public async Task UpdateFactionRelation(FactionRelation newRelation)
+    {
+        try
+        {
+            _ctx.FactionRelations.Update(newRelation);
+            await _ctx.SaveChangesAsync();
+        }
+        catch (Exception e)
+        {
+            Log.Error(e, "Error while updating faction relation");
+        }
+
+        await _taskHelper.TrySwitchToMainThread();
+    }
+}
