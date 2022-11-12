fce380ae858cffd3287598a04a5c64b22b9c829c
Caelan L. Talmadge <34071000+cltalmadge@users.noreply.github.com>
2022-11-12 15:21:25 -0500

    Made AmiaCharacter.CdKey nullable

--- AmiaReforged.Core/Models/AmiaCharacter.cs
+++ AmiaReforged.Core/Models/AmiaCharacter.cs
@@ -5,7 +5,7 @@ namespace AmiaReforged.Core.Entities;
 public class AmiaCharacter
 {
     [Key] public Guid Id { get; set; }
-    public string CdKey { get; set; } = null!;
+    public string? CdKey { get; set; } 
     public string FirstName { get; set; } = null!;
     public string LastName { get; set; } = null!;
     public bool IsPlayerCharacter { get; set; }
