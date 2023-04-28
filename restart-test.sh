9c05dcbd04285e626211d9d7184017e64ecd15c3
Caelan L. Talmadge <talmadge12@gmail.com>
2023-04-27 19:40:53 -0500

    Simplify restart-test.sh

--- restart-test.sh
+++ restart-test.sh
@@ -1,5 +1,4 @@
-﻿# Restarts the test server
-pushd /home/amia/amia_server || exit
+﻿pushd /home/amia/amia_server || exit
 docker-compose stop test-server
 docker-compose rm -f test-server
 docker-compose up -d test-server
