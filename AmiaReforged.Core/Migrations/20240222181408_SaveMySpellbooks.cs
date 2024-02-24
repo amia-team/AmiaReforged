50a230a45b3f781eee7f8de4a4845c6e46a7b09d
Caelan L. Talmadge <34071000+cltalmadge@users.noreply.github.com>
2024-02-24 17:11:51 -0600

    Add missing migration files.

---
+++ AmiaReforged.Core/Migrations/20240222181408_SaveMySpellbooks.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AmiaReforged.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class SaveMySpellbooks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SavedSpellbooks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    SpellbookName = table.Column<string>(type: "text", nullable: false),
+                    SpellbookJson = table.Column<string>(type: "text", nullable: false),
+                    ClassId = table.Column<int>(type: "integer", nullable: false),
+                    PlayerCharacterId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SavedSpellbooks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SavedSpellbooks_Characters_PlayerCharacterId",
+                        column: x => x.PlayerCharacterId,
+                        principalTable: "Characters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SavedSpellbooks_PlayerCharacterId",
+                table: "SavedSpellbooks",
+                column: "PlayerCharacterId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SavedSpellbooks");
+        }
+    }
+}
