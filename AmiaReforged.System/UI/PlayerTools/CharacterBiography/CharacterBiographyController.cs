7447c94c80be68e3f969c9713ecbf21d59434b22
Caelan L. Talmadge <34071000+cltalmadge@users.noreply.github.com>
2024-02-24 17:11:33 -0600

    Add character bio editor

---
+++ AmiaReforged.System/UI/PlayerTools/CharacterBiography/CharacterBiographyController.cs
@@ -0,0 +1,59 @@
+﻿using AmiaReforged.Core.UserInterface;
+using Anvil.API;
+using Anvil.API.Events;
+
+namespace AmiaReforged.System.UI.PlayerTools.CharacterBiography;
+
+public class CharacterBiographyController : WindowController<CharacterBiographyView>
+{
+    public override void Init()
+    {
+        string? characterBio = Token.Player.LoginCreature?.Description;
+        Token.SetBindValue(View.CharacterBiography!, characterBio);
+    }
+
+    public override void ProcessEvent(ModuleEvents.OnNuiEvent eventData)
+    {
+        switch (eventData.EventType)
+        {
+            case NuiEventType.Click:
+                HandleButtonClick(eventData);
+                break;
+        }
+    }
+
+    private void HandleButtonClick(ModuleEvents.OnNuiEvent eventData)
+    {
+        if (eventData.ElementId == View.SaveButton.Id)
+        {
+            SaveCharacterBiography();
+        }
+        else if (eventData.ElementId == View.DiscardButton.Id)
+        {
+            DiscardCharacterBiography();
+        }
+    }
+
+    private void SaveCharacterBiography()
+    {
+        string? characterBio = Token.GetBindValue(View.CharacterBiography);
+        
+        if (Token.Player.LoginCreature != null && characterBio != null)
+        {
+            Token.Player.LoginCreature.Description = characterBio;
+        }
+
+        Token.Player.ExportCharacter();
+        Token.Close();
+    }
+
+    private void DiscardCharacterBiography()
+    {
+        Token.Close();
+    }
+
+    protected override void OnClose()
+    {
+        // Do nothing
+    }
+}
---
+++ AmiaReforged.System/UI/PlayerTools/CharacterBiography/CharacterBiographyView.cs
@@ -0,0 +1,61 @@
+﻿using AmiaReforged.Core.UserInterface;
+using Anvil.API;
+using NWN.Core;
+
+namespace AmiaReforged.System.UI.PlayerTools.CharacterBiography;
+
+public sealed class CharacterBiographyView : WindowView<CharacterBiographyView>
+{
+    public override string Id => "playertools.charbiography";
+    public override string Title => "Character Biography";
+
+    public override IWindowController? CreateDefaultController(NwPlayer player)
+    {
+        return CreateController<CharacterBiographyController>(player);
+    }
+
+    public readonly NuiBind<string> CharacterBiography = new NuiBind<string>("character_biography");
+
+
+    public readonly NuiButton SaveButton;
+    public readonly NuiButton DiscardButton;
+
+    public override NuiWindow? WindowTemplate { get; }
+
+    public CharacterBiographyView()
+    {
+        NuiColumn root = new NuiColumn
+        {
+            Width = 600f,
+            Height = 600f,
+            Children = new List<NuiElement>
+            {
+                new NuiTextEdit("Edit Bio", CharacterBiography, 10000, true)
+                {
+                    WordWrap = true,
+                    Height = 600f,
+                    Width = 600f
+                },
+                new NuiRow
+                {
+                    Children = new List<NuiElement>
+                    {
+                        new NuiButton("Save")
+                        {
+                            Id = "save",
+                        }.Assign(out SaveButton),
+                        new NuiButton("Discard")
+                        {
+                            Id = "discard",
+                        }.Assign(out DiscardButton),
+                    },
+                },
+            },
+        };
+
+        WindowTemplate = new NuiWindow(root, Title)
+        {
+            Geometry = new NuiRect(500f, 100f, 600f, 720f),
+        };
+    }
+}
