305543ac99c282c1569a99072765c368c03e4a49
Mitro Hyvärinen <mitro.hyvarinen@gmail.com>
2024-03-16 07:18:32 +0200

    Update AssociateCustomizerService.cs

--- AmiaReforged.DMS/Services/AssociateCustomizerService.cs
+++ AmiaReforged.DMS/Services/AssociateCustomizerService.cs
@@ -3,7 +3,7 @@ using Anvil.API.Events;
 using Anvil.Services;
 using NLog;
 
-namespace AmiaReforged.Classes.Services;
+namespace AmiaReforged.DMS.Services;
 
 [ServiceBinding(typeof(AssociateCustomizerService))]
 public class AssociateCustomizerService
