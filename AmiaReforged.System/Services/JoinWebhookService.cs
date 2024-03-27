c8c51c79da750dbcbd18a40642a8e17d4c3da4e5
Maverick00053 <38230616+Maverick00053@users.noreply.github.com>
2024-03-26 21:10:39 -0700

    Created a Generic Message Sender Function

--- AmiaReforged.System/Services/JoinWebhookService.cs
+++ AmiaReforged.System/Services/JoinWebhookService.cs
@@ -14,12 +14,14 @@ public class JoinWebhookService
     private readonly SchedulerService _schedulerService;
     private static readonly Logger Log = LogManager.GetCurrentClassLogger();
     private readonly WebhookSender _webhookSender;
+    private readonly WebhookSender _webhookSenderGeneric;
 
     public JoinWebhookService(SchedulerService schedulerService)
     {
         _schedulerService = schedulerService;
         _schedulerService.ScheduleRepeating(ListPlayers, TimeSpan.FromMinutes(30));
         _webhookSender = new WebhookSender(UtilPlugin.GetEnvironmentVariable("SERVER_NOTIFICATION_WEBHOOK"));
+        _webhookSenderGeneric = new WebhookSender(UtilPlugin.GetEnvironmentVariable("SERVER_NOTIFICATION_GENERIC"));
         Log.Info("JoinWebhook Service Initialized.");
     }
 
@@ -33,7 +35,16 @@ public class JoinWebhookService
             return;
         }
 
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
         string players = NwModule.Instance.Players.Where(player => player.LoginCreature != null).Aggregate("Players online: \n", (current, player) => current + $"\t\t{player.PlayerName} - {player.LoginCreature.Name}\n");
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
         await _webhookSender.SendMessage(Username, players, Avatar);
     }
+    
+    private async void LaunchDiscordMessage(string messageSent)
+    {
+       await _webhookSenderGeneric.SendMessage("",messageSent,""); 
+    }
 }
+
+
