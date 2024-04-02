d1b2e0ffd40c1e1e08f708c8277024c7f387aacc
Maverick00053 <38230616+Maverick00053@users.noreply.github.com>
2024-04-01 19:50:49 -0700

    Elyon Boss Spawner Test

---
+++ AmiaReforged.System/Services/ElyonBossSpawner.cs
@@ -0,0 +1,34 @@
+using System;
+using Anvil.API;
+using Anvil.Services;
+using NWN.Core;
+
+namespace AmiaReforged.System.Services;
+
+[ServiceBinding(typeof(ElyonBossSpawner))]
+
+
+public class ElyonBossSpawner
+{
+    private readonly SchedulerService _schedulerService;
+
+    public ElyonBossSpawner(SchedulerService schedulerService)
+    {
+        _schedulerService = schedulerService;
+        _schedulerService.ScheduleRepeating(LaunchBoss, TimeSpan.FromMinutes(GenerateSpawnTime()));
+    }
+
+    private int GenerateSpawnTime()
+    {
+    int MaxSpawnTime = 320; // Minutes
+    Random random = new Random();
+    int Time = random.Next(1, MaxSpawnTime);
+    return Time;
+    }
+
+    private void LaunchBoss()
+    {
+        NWScript.SetLocalString(NWScript.GetModule(),"announcerMessage","ElyonBossSpawner FIRED! Should be between 1 to 320 minutes after Test Launch text.");
+        NWScript.ExecuteScript("webhook_announce");
+    }
+}
