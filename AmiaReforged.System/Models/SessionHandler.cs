c36070dd937a99775032dd06af129fd88d91d905
Caelan L. Talmadge <34071000+cltalmadge@users.noreply.github.com>
2024-05-14 23:08:38 -0500

    Add Session, SessionHandler to Models

---
+++ AmiaReforged.System/Models/SessionHandler.cs
@@ -0,0 +1,21 @@
+﻿using Anvil.Services;
+
+namespace AmiaReforged.System.Services;
+
+[ServiceBinding(typeof(SessionHandler))]
+public class SessionHandler
+{
+    private Dictionary<string, Session> _sessions = new();
+
+    public void StartSessionFor(string name)
+    {
+        _sessions.TryAdd(name, new Session());
+    }
+
+    public Session GetSessionFor(string name) => _sessions[name];
+
+    public void EndSessionFor(string name)
+    {
+        _sessions.Remove(name);
+    }
+}
