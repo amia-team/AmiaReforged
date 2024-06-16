35a923dc8837d920e92da577cd4b71a0acc71e61
Caelan L. Talmadge <talmadge12@gmail.com>
2024-06-16 10:39:53 -0500

    Tell players how much time until reset

--- AmiaReforged.System/Services/ResetService.cs
+++ AmiaReforged.System/Services/ResetService.cs
@@ -9,6 +9,7 @@ namespace AmiaReforged.System.Services;
 [ServiceBinding(typeof(ResetService))]
 public class ResetService
 {
+    private const string ResetTimerLVar = "minutesToReset";
     private readonly SchedulerService _schedulerService;
     private static readonly Logger Log = LogManager.GetCurrentClassLogger();
 
@@ -16,7 +17,7 @@ public class ResetService
     {
         _schedulerService = schedulerService;
         ScheduleAutosaveAndReset();
-        
+
         NwModule.Instance.OnModuleLoad += SetInitialResetValue;
         NwModule.Instance.OnPlayerRest += DisplayResetTimer;
 
@@ -26,13 +27,29 @@ public class ResetService
 
     private void ScheduleAutosaveAndReset()
     {
+        _schedulerService.ScheduleRepeating(LogRemainingReset, TimeSpan.FromMinutes(10));
         _schedulerService.ScheduleRepeating(SavePCs, TimeSpan.FromMinutes(30));
         _schedulerService.ScheduleRepeating(CheckShutdown, TimeSpan.FromMinutes(1));
     }
 
+    private void LogRemainingReset()
+    {
+        float resetAtMinutes = NWScript.GetLocalFloat(NwModule.Instance, ResetTimerLVar);
+        // Calculate time until reset in minutes
+        float uptime = (float)ResetTimeKeeperSingleton.Instance.Uptime() / 60;
+        float estimatedReset = resetAtMinutes - uptime;
+        Log.Info($"Estimated time until reset: {(int)estimatedReset}");
+
+        // Tell everyone in the server how long until reset
+        NwModule.Instance.Players.ToList().ForEach(p =>
+        {
+            p.SendServerMessage($"Estimated time until reset: {(int)estimatedReset}", Color.FromRGBA("#e6e600"));
+        });
+    }
+
     private void SetInitialResetValue(ModuleEvents.OnModuleLoad obj)
     {
-        NWScript.SetLocalFloat(NwModule.Instance, "minutesToReset", 480.0f);
+        NWScript.SetLocalFloat(NwModule.Instance, ResetTimerLVar, 480.0f);
 
         ResetTimeKeeperSingleton.Instance.ResetStartTime = ((DateTimeOffset)DateTime.Now).ToUnixTimeSeconds();
         Log.Info($"Initial restart time: {NWScript.GetLocalInt(NwModule.Instance, "hoursToReset")} hours");
@@ -42,16 +59,16 @@ public class ResetService
     {
         if (obj.RestEventType != RestEventType.Cancelled) return;
 
-        float resetAtMinutes = NWScript.GetLocalFloat(NwModule.Instance, "minutesToReset");
+        float resetAtMinutes = NWScript.GetLocalFloat(NwModule.Instance, ResetTimerLVar);
         float uptime = (float)ResetTimeKeeperSingleton.Instance.Uptime() / 60;
 
         float estimatedReset = resetAtMinutes - uptime;
-        obj.Player.SendServerMessage($"Estimated reset time: {(int) estimatedReset}", Color.FromRGBA("#5be9ffcc"));
+        obj.Player.SendServerMessage($"Estimated reset time: {(int)estimatedReset}", Color.FromRGBA("#5be9ffcc"));
     }
 
     private static void SavePCs()
     {
-        foreach (NwPlayer instancePlayer in (IEnumerable<NwPlayer>)NwModule.Instance.Players)
+        foreach (NwPlayer instancePlayer in NwModule.Instance.Players)
         {
             if (instancePlayer.IsDM)
             {
@@ -59,7 +76,7 @@ public class ResetService
                 continue;
             }
 
-            if (instancePlayer.LoginCreature.ActiveEffects.Any(e => e.EffectType == EffectType.Polymorph))
+            if (instancePlayer.LoginCreature!.ActiveEffects.Any(e => e.EffectType == EffectType.Polymorph))
             {
                 instancePlayer.SendServerMessage(
                     "-- Polymorphed PCs can't be saved. Please unpolymorph to save your PC. --");
@@ -74,7 +91,7 @@ public class ResetService
     private void CheckShutdown()
     {
         long uptime = ResetTimeKeeperSingleton.Instance.Uptime() / 60;
-        float resetAtMinutes = NWScript.GetLocalFloat(NwModule.Instance, "minutesToReset");
+        float resetAtMinutes = NWScript.GetLocalFloat(NwModule.Instance, ResetTimerLVar);
         Log.Info($"Time since reset timer began: {((double)uptime / 60):0.00}");
 
         if (!(uptime >= resetAtMinutes)) return;
