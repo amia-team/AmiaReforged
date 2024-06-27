b6e2f92e194560039679bb3efb0def0482635a54
Caelan L. Talmadge <talmadge12@gmail.com>
2024-06-27 10:43:04 -0500

    Require name field for Faction model

--- AmiaReforged.Core/Models/Faction/FactionEntity.cs
+++ AmiaReforged.Core/Models/Faction/FactionEntity.cs
@@ -5,11 +5,10 @@ namespace AmiaReforged.Core.Models.Faction;
 public class FactionEntity
 {
     [Key] public long Id { get; set; }
-    
-    public string Name { get; set; } = null!;
 
-    [Required]
-    public string Description { get; set; } = null!;
-    
+    [Required] public string Name { get; set; } = null!;
+
+    [Required] public string Description { get; set; } = null!;
+
     public List<PlayerFactionMember> PlayerMembers { get; set; } = null!;
 }
