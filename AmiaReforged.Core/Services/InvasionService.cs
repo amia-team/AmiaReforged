edc51b778d3001640d83334adf8fbe7f16b185bc
Maverick00053 <38230616+Maverick00053@users.noreply.github.com>
2024-10-19 22:51:29 -0700

    Update InvasionService.cs

--- AmiaReforged.Core/Services/InvasionService.cs
+++ AmiaReforged.Core/Services/InvasionService.cs
@@ -84,9 +84,8 @@ public class InvasionService
         {
             Log.Error(e, "Error getting all invasion records");
         }
-
         await _nwTaskHelper.TrySwitchToMainThread();
-        return invasions;
+        return invasions; 
     }
 
     private async Task<List<InvasionRecord>> GetCertainInvasionRecord(Expression<Func<InvasionRecord, bool>> predicate)
@@ -108,26 +107,6 @@ public class InvasionService
         await _nwTaskHelper.TrySwitchToMainThread();
         return invasions;
     }
-    private async Task<List<InvasionRecord>> GetCertainInvasionRecordWithID(string invasionID)
-    {
-        AmiaDbContext amiaDbContext = _ctxFactory.CreateDbContext();
-
-        List<InvasionRecord> invasions = new();
-        try
-        {
-            invasions = await amiaDbContext.InvasionRecord
-                .Where(AreaZone.get(invasionID))
-                .ToListAsync();
-        }
-        catch (Exception e)
-        {
-            Log.Error(e, "Error getting certain invasion record");
-        }
-
-        await _nwTaskHelper.TrySwitchToMainThread();
-        return invasions;
-    }
-
     public async Task<bool> InvasionRecordExists(string invasionId)
     {
         AmiaDbContext amiaDbContext = _ctxFactory.CreateDbContext();
