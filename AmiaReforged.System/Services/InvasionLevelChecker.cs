00f09fbeb6b9aaf86c7746944eb50bed892b23b9
Maverick00053 <38230616+Maverick00053@users.noreply.github.com>
2024-10-26 23:56:06 -0700

    Fixing Invasion Level Checker

--- AmiaReforged.System/Services/InvasionLevelChecker.cs
+++ AmiaReforged.System/Services/InvasionLevelChecker.cs
@@ -25,8 +25,9 @@ public class InvasionLevelChecker
     [ScriptHandler("invasion_checker")]
     public async void InvasionChecker(CallInfo info)
     {
-      uint oUser = NWScript.GetArea(NWScript.OBJECT_SELF);
-      string AreaResRef = NWScript.GetResRef(oUser);
+      Location Location = NWScript.GetItemActivatedTargetLocation();
+      uint Area = NWScript.GetAreaFromLocation(Location);
+      string AreaResRef = NWScript.GetResRef(Area);
       List<InvasionRecord> invasions = await _invasionService.GetAllInvasionRecords();
       InvasionRecord invasionRecord = invasions.Find(x => x.AreaZone == AreaResRef); 
       NWScript.SendMessageToAllDMs("Area: " + AreaResRef + " | Invasion Percent: " + invasionRecord.InvasionPercent.ToString() + " | Realm Chaos: "  + invasionRecord.RealmChaos.ToString());
