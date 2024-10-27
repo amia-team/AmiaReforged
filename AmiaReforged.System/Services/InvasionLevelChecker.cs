0748831431d10be31f1187d748f87b083a689438
Maverick00053 <38230616+Maverick00053@users.noreply.github.com>
2024-10-27 00:27:06 -0700

    Update InvasionLevelChecker.cs

--- AmiaReforged.System/Services/InvasionLevelChecker.cs
+++ AmiaReforged.System/Services/InvasionLevelChecker.cs
@@ -31,6 +31,7 @@ public class InvasionLevelChecker
       InvasionRecord invasionRecord = new();
       invasionRecord.AreaZone = "N/A";
       invasionRecord.InvasionPercent = 0; 
+      invasionRecord.RealmChaos = 0; 
       List<InvasionRecord> invasions = await _invasionService.GetAllInvasionRecords();
       InvasionRecord invasionRecordTemp = invasions.Find(x => x.AreaZone == AreaResRef); 
       if(invasionRecordTemp != null)
