8b288037133f992f220802a1fcb0d17bd210bc57
Maverick00053 <38230616+Maverick00053@users.noreply.github.com>
2024-11-16 19:14:59 -0800

    Fixed DB entries

--- AmiaReforged.Core/Models/LastLocation.cs
+++ AmiaReforged.Core/Models/LastLocation.cs
@@ -1,12 +1,18 @@
 using System.ComponentModel.DataAnnotations;
 
 namespace AmiaReforged.Core.Models;
+
+using System.Numerics;
 using Anvil.API;
 
 public class LastLocation
 {
     [Key] public int Id { get; set; }
     public string PCKey { get; set; }
-    public Location Location { get; set; }
+    public uint Area { get; set; }
+    public float Orientation { get; set; }
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float Z { get; set; }
 
 }
--- AmiaReforged.Core/Models/PersistPLC.cs
+++ AmiaReforged.Core/Models/PersistPLC.cs
@@ -7,6 +7,10 @@ public class PersistPLC
 {
     [Key] public int Id { get; set; }
     public uint PLC { get; set; }
-    public Location Location { get; set; }
+    public uint Area { get; set; }
+    public float Orientation { get; set; }
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float Z { get; set; }
     
 }
