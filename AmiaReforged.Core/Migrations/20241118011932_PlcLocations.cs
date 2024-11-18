cec62d45a4588c2dc376a280d2765523f5311c44
Caelan L. Talmadge <talmadge12@gmail.com>
2024-11-17 19:20:49 -0600

    Add migration file for plcs

---
+++ AmiaReforged.Core/Migrations/20241118011932_PlcLocations.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace AmiaReforged.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class PlcLocations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LastLocation",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PCKey = table.Column<string>(type: "text", nullable: false),
+                    Area = table.Column<long>(type: "bigint", nullable: false),
+                    Orientation = table.Column<float>(type: "real", nullable: false),
+                    X = table.Column<float>(type: "real", nullable: false),
+                    Y = table.Column<float>(type: "real", nullable: false),
+                    Z = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LastLocation", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersistPLC",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PLC = table.Column<long>(type: "bigint", nullable: false),
+                    Area = table.Column<long>(type: "bigint", nullable: false),
+                    Orientation = table.Column<float>(type: "real", nullable: false),
+                    X = table.Column<float>(type: "real", nullable: false),
+                    Y = table.Column<float>(type: "real", nullable: false),
+                    Z = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersistPLC", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LastLocation");
+
+            migrationBuilder.DropTable(
+                name: "PersistPLC");
+        }
+    }
+}
