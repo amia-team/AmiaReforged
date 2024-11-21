4047167dbc8b2e99a69c8431df34e4665de0d753
Caelan L. Talmadge <34071000+cltalmadge@users.noreply.github.com>
2024-11-21 15:29:17 -0600

    Update Jenkinsfile to include PwEngine alpha

--- publish.sh
+++ publish.sh
@@ -9,3 +9,4 @@ fi
 dotnet publish ./AmiaReforged.Classes/AmiaReforged.Classes.csproj -o $1/AmiaReforged.Classes
 dotnet publish ./AmiaReforged.Races/AmiaReforged.Races.csproj -o $1/AmiaReforged.Races
 dotnet publish ./AmiaReforged.System/AmiaReforged.System.csproj -o $1/AmiaReforged.System
+dotnet publish ./AmiaReforged.System/AmiaReforged.PwEngine.csproj -o $1/AmiaReforged.PwEngine
