a11a343e9a031b598894e6b2b6834d08a20802fa
Caelan L. Talmadge <talmadge12@gmail.com>
2024-11-22 12:40:19 -0600

    Add CraftingPropertyService.cs

---
+++ AmiaReforged.PwEngine/Systems/Crafting/CraftingPropertyService.cs
@@ -0,0 +1,21 @@
+﻿using Anvil.API;
+using Anvil.Services;
+using NLog;
+
+namespace AmiaReforged.PwEngine.Systems.Crafting;
+
+[ServiceBinding(typeof(CraftingPropertyService))]
+public class CraftingPropertyService
+{
+    private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+    public CraftingPropertyService()
+    {
+        LoadDefinitionsFromDisk();
+    }
+
+    private void LoadDefinitionsFromDisk()
+    {
+        
+    }
+}
