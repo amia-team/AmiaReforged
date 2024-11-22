04191aa704aa039436134e6450bce0c01165f3bf
Caelan L. Talmadge <talmadge12@gmail.com>
2024-11-22 12:45:56 -0600

    Add initial check for path definition

--- AmiaReforged.PwEngine/Systems/Crafting/CraftingPropertyService.cs
+++ AmiaReforged.PwEngine/Systems/Crafting/CraftingPropertyService.cs
@@ -1,12 +1,19 @@
-﻿using Anvil.API;
+﻿using AmiaReforged.PwEngine.Systems.Crafting.Models;
+using Anvil.API;
 using Anvil.Services;
 using NLog;
+using NWN.Core.NWNX;
 
 namespace AmiaReforged.PwEngine.Systems.Crafting;
 
+/// <summary>
+/// This service is responsible for loading and providing item property definitions to the crafting system.
+/// For example, it can provide a matrix of properties given an item type.
+/// </summary>
 [ServiceBinding(typeof(CraftingPropertyService))]
 public class CraftingPropertyService
 {
+    private const string AmiaCraftingPropertiesDirectory = "AMIA_CRAFTING_PROPERTIES_DIRECTORY";
     private static readonly Logger Log = LogManager.GetCurrentClassLogger();
 
     public CraftingPropertyService()
@@ -16,6 +23,15 @@ public class CraftingPropertyService
 
     private void LoadDefinitionsFromDisk()
     {
-        
+        string path = UtilPlugin.GetEnvironmentVariable(AmiaCraftingPropertiesDirectory);
+        if (path == "")
+        {
+            Log.Error("Could not load crafting properties. Environment variable AMIA_CRAFTING_PROPERTIES_DIRECTORY is not set.");
+        }
+    }
+    
+    public IReadOnlyCollection<ItemPropertyCategory> GetPropertiesForItem(NwItem item)
+    {
+        return new List<ItemPropertyCategory>();
     }
 }
