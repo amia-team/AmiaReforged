37e058682d1149355a7015ba9974eb86207d1b23
Mitro Hyvärinen <mitro.hyvarinen@gmail.com>
2024-11-26 15:06:20 +0200

    Change function used to equip Associate Customizer
    Troubleshooting a bug

--- AmiaReforged.DMS/Services/AssociateCustomizerService.cs
+++ AmiaReforged.DMS/Services/AssociateCustomizerService.cs
@@ -404,7 +404,7 @@ public class AssociateCustomizerService
             if (associate.GetItemInSlot(InventorySlot.Chest) == null)
             {
                 NwItem? armor = await NwItem.Create("cloth024", associate);
-                associate.RunEquip(armor, InventorySlot.Chest);
+                associate.ActionEquipItem(armor, InventorySlot.Chest);
                 armor.Appearance.SetArmorColor(ItemAppearanceArmorColor.Cloth1, armorCopy.Appearance.GetArmorColor(ItemAppearanceArmorColor.Cloth1));
                 armor.Appearance.SetArmorColor(ItemAppearanceArmorColor.Cloth2, armorCopy.Appearance.GetArmorColor(ItemAppearanceArmorColor.Cloth2));
                 armor.Appearance.SetArmorColor(ItemAppearanceArmorColor.Leather1, armorCopy.Appearance.GetArmorColor(ItemAppearanceArmorColor.Leather1));
