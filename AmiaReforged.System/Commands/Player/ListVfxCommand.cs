d91149102e4dd9b5726f69961504e1d2d9a1752d
Mitro Hyvärinen <mitro.hyvarinen@gmail.com>
2024-12-11 18:18:46 +0200

    Add param to listvfx command

--- AmiaReforged.System/Commands/Player/ListVfxCommand.cs
+++ AmiaReforged.System/Commands/Player/ListVfxCommand.cs
@@ -13,20 +13,27 @@ public class ListVfx : IChatCommand
     public Task ExecuteCommand(NwPlayer caller, string message)
     {
         // string originalDescription = caller.ControlledCreature.Description;
-        string vfxList = "ID    LABEL";
+        string vfxList = "ID  LABEL";
 
         string vfxLabel;
         int vfxId;
-
+    
         for (int i = 0; i < NwGameTables.VisualEffectTable.RowCount; i++)
         {
-            if (NwGameTables.VisualEffectTable[i].TypeFd != "D") continue;
+            if (message.Split(' ')[1] == "instant" && NwGameTables.VisualEffectTable[i].TypeFd == "F")
+            {
+                vfxLabel = NwGameTables.VisualEffectTable[i].Label.ToLower();
+                vfxId = NwGameTables.VisualEffectTable[i].RowIndex;
+                vfxList += $"\n{vfxId} {vfxLabel}";
+            }
+            else if (NwGameTables.VisualEffectTable[i].TypeFd == "D") 
+            {
+                vfxLabel = NwGameTables.VisualEffectTable[i].Label.ToLower();
+                vfxId = NwGameTables.VisualEffectTable[i].RowIndex;
+                vfxList += $"\n{vfxId} {vfxLabel}";
+            }
             
-            vfxLabel = NwGameTables.VisualEffectTable[i].Label;
-            vfxId = NwGameTables.VisualEffectTable[i].RowIndex;
-            vfxList += $"\n{vfxId}  {vfxLabel}";
         }
-
         caller.ControlledCreature.Description = vfxList;
         caller.ActionExamine(caller.ControlledCreature);
         // caller.ControlledCreature.Description = originalDescription;
