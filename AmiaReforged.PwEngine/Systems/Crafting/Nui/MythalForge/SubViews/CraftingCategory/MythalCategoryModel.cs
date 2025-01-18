09754da05681a9173dfb9c0b98ed944b64ed2233
Caelan L. Talmadge <34071000+cltalmadge@users.noreply.github.com>
2025-01-18 13:27:46 -0600

    Actually add category to category model...

--- AmiaReforged.PwEngine/Systems/Crafting/Nui/MythalForge/SubViews/CraftingCategory/MythalCategoryModel.cs
+++ AmiaReforged.PwEngine/Systems/Crafting/Nui/MythalForge/SubViews/CraftingCategory/MythalCategoryModel.cs
@@ -55,6 +55,8 @@ public class MythalCategoryModel
                 
                 modelCategory.Properties.Add(modelProperty);
             }
+            
+            Categories.Add(modelCategory);
         }
     }
 
