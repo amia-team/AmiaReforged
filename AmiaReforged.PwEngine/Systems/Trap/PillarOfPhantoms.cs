2bc4d0a8b4ba736bf7d007c312172d62e143ee95
Caelan L. Talmadge <34071000+cltalmadge@users.noreply.github.com>
2025-01-25 13:07:44 -0600

    Give pillar of gooners same treatment

--- AmiaReforged.PwEngine/Systems/Trap/PillarOfPhantoms.cs
+++ AmiaReforged.PwEngine/Systems/Trap/PillarOfPhantoms.cs
@@ -24,8 +24,8 @@ public class PillarOfPhantoms
 
             if (trap.Area != null) _activeTraps[trap.Area].Add(trap);
 
+            trap.OnHeartbeat += DoSpawnRoutine;
             trap.OnDeath += OnTrapDeath;
-            trap.Area.OnHeartbeat += DoSpawnRoutine;
         }
 
         NwModule.Instance.OnDMSpawnObject += OnTrapSpawn;
@@ -39,7 +39,6 @@ public class PillarOfPhantoms
         if (!_activeTraps.ContainsKey(obj.Area))
         {
             _activeTraps.Add(obj.Area, new List<NwPlaceable>());
-            obj.Area.OnHeartbeat += DoSpawnRoutine;
         }
 
         RegisterNewTraps(obj.Area);
@@ -53,7 +52,47 @@ public class PillarOfPhantoms
         foreach (NwPlaceable trap in traps)
         {
             _activeTraps[area].Add(trap);
+            trap.OnHeartbeat += DoSpawnRoutine;
             trap.OnDeath += OnTrapDeath;
+        }
+    }
+
+    private void DoSpawnRoutine(PlaceableEvents.OnHeartbeat obj)
+    {
+        if (obj.Placeable.Tag != PillarOfPhantomsTag) return;
+
+
+        // Get creatures that are player controlled within 25 meters of the trap
+        List<NwCreature> creatures = obj.Placeable.Area.FindObjectsOfTypeInArea<NwCreature>()
+            .Where(c => c.IsPlayerControlled && c.Distance(obj.Placeable) <= 10f).ToList();
+
+        foreach (NwCreature unused in creatures)
+        {
+            // Pick a random, valid location within 4 meters of the trap
+            int randomXOffset = Random.Shared.Next(-4, 4);
+            int randomYOffset = Random.Shared.Next(-4, 4);
+            // The Z axis is shared with the trap, so we don't need to randomize it
+            Location spawnLocation = Location.Create(obj.Placeable.Area,
+                new Vector3(obj.Placeable.Position.X + randomXOffset, obj.Placeable.Position.Y + randomYOffset, obj.Placeable.Position.Z),
+                obj.Placeable.Rotation);
+
+            // Is it safe to spawn the phantom here?
+            if (spawnLocation.IsWalkable)
+            {
+                // Just to make sure, we're going to set the spawn location's Z value to its walkable height
+                Location trueSpawnLocation = Location.Create(obj.Placeable.Area,
+                    new Vector3(spawnLocation.Position.X, spawnLocation.Position.Y, spawnLocation.GroundHeight),
+                    obj.Placeable.Rotation);
+                NwCreature.Create(PillarPhantomTag, trueSpawnLocation);
+                Effect spawnVfx =
+                    Effect.VisualEffect(VfxType.FnfSummonUndead, false, Random.Shared.NextFloat(-1.5f, 1.5f));
+                Effect secondaryVfx =
+                    Effect.VisualEffect(VfxType.ImpRaiseDead, false, Random.Shared.NextFloat(-1.5f, 1.5f));
+
+                // Apply at location
+                trueSpawnLocation.ApplyEffect(EffectDuration.Instant, spawnVfx);
+                trueSpawnLocation.ApplyEffect(EffectDuration.Instant, secondaryVfx);
+            }
         }
     }
 
@@ -64,40 +103,5 @@ public class PillarOfPhantoms
         if (!_activeTraps.ContainsKey(obj.KilledObject.Area)) return;
 
         _activeTraps[obj.KilledObject.Area].Remove(obj.KilledObject);
-    }
-
-    private void DoSpawnRoutine(AreaEvents.OnHeartbeat obj)
-    {
-        foreach (NwPlaceable trap in _activeTraps[obj.Area])
-        {
-            if(trap.Area == null) continue;
-            
-            // Get creatures that are player controlled within 25 meters of the trap
-            List<NwCreature> creatures = trap.Area.FindObjectsOfTypeInArea<NwCreature>()
-                .Where(c => c.IsPlayerControlled && c.Distance(trap) <= 10f).ToList();
-
-            foreach (NwCreature unused in creatures)
-            {
-                // Pick a random, valid location within 4 meters of the trap
-                int randomXOffset = Random.Shared.Next(-4, 4);
-                int randomYOffset = Random.Shared.Next(-4, 4); 
-                // The Z axis is shared with the trap, so we don't need to randomize it
-                Location spawnLocation = Location.Create(trap.Area, new Vector3(trap.Position.X + randomXOffset, trap.Position.Y + randomYOffset, trap.Position.Z), trap.Rotation);
-                
-                // Is it safe to spawn the phantom here?
-                if (spawnLocation.IsWalkable)
-                {
-                    // Just to make sure, we're going to set the spawn location's Z value to its walkable height
-                    Location trueSpawnLocation = Location.Create(trap.Area, new Vector3(spawnLocation.Position.X, spawnLocation.Position.Y, spawnLocation.GroundHeight), trap.Rotation);
-                    NwCreature.Create(PillarPhantomTag, trueSpawnLocation);
-                    Effect spawnVfx = Effect.VisualEffect(VfxType.FnfSummonUndead, false, Random.Shared.NextFloat(-1.5f, 1.5f));
-                    Effect secondaryVfx = Effect.VisualEffect(VfxType.ImpRaiseDead, false, Random.Shared.NextFloat(-1.5f, 1.5f));
-                    
-                    // Apply at location
-                    trueSpawnLocation.ApplyEffect(EffectDuration.Instant, spawnVfx);
-                    trueSpawnLocation.ApplyEffect(EffectDuration.Instant, secondaryVfx);
-                }
-            }
-        }
     }
 }
