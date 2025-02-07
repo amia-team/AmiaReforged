d7a49855758d6c5bcac310637e1c8a87a0610745
Mitro Hyvärinen <mitro.hyvarinen@gmail.com>
2025-02-07 05:49:10 +0200

    Add generic quest constants

---
+++ AmiaReforged.System/Dynamic/GenericQuest/QuestConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace AmiaReforged.System.Dynamic.GenericQuest;
+
+public class QuestConstants
+{
+    public const int QuestNotTaken = 0;
+    public const int QuestTaken = 1;
+    public const int QuestCompleted = 2;
+}
