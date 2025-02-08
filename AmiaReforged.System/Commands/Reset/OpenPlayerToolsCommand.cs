63ecb411ec1361deb683198a396d06e49b85add4
Caelan L. Talmadge <talmadge12@gmail.com>
2025-02-08 12:15:41 -0600

    Deprecate old PlayerTools

--- AmiaReforged.System/Commands/Reset/OpenPlayerToolsCommand.cs
+++ AmiaReforged.System/Commands/Reset/OpenPlayerToolsCommand.cs
@@ -1,5 +1,4 @@
 ﻿using AmiaReforged.Core.UserInterface;
-using AmiaReforged.System.UI.PlayerTools;
 using Anvil.API;
 using Anvil.Services;
 
@@ -9,15 +8,16 @@ namespace AmiaReforged.System.Commands.Reset;
 public class OpenPlayerToolsCommand : IChatCommand
 {
     [Inject] private WindowManager WindowManager { get; set; }
-    
+
     public string Command { get; } = "./playertools";
+
     public Task ExecuteCommand(NwPlayer caller, string message)
     {
-        if(caller.IsDM) return Task.CompletedTask;
-        
-        caller.SendServerMessage("Opening Player Tools.");
-        
-        WindowManager.OpenWindow<PlayerToolButtonView>(caller);
+        if (caller.IsDM) return Task.CompletedTask;
+
+        caller.SendServerMessage(
+            "This command has been deprecated. Use the Player Tools feat (has a wrench icon) from your class's radial menu instead.", ColorConstants.Yellow);
+
         return Task.CompletedTask;
     }
 }
