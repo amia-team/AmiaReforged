e7ad2365c8464264245207030f886fc4f6d0177b
Mitro Hyvärinen <mitro.hyvarinen@gmail.com>
2025-02-09 13:19:59 +0200

    Add prestige class constants

--- AmiaReforged.System/Dynamic/GenericQuest/QuestConstants.cs
+++ AmiaReforged.System/Dynamic/GenericQuest/QuestConstants.cs
@@ -5,4 +5,19 @@ public class QuestConstants
     public const int QuestNotTaken = 0;
     public const int QuestTaken = 1;
     public const int QuestCompleted = 2;
+
+    public const int EscapeArtist = 42;
+    public const int Warslinger = 43;
+    public const int Cavalry = 45;
+    public const int TwoWeaponFighter = 46;
+    public const int BowMaster = 47;
+    public const int Defender = 48;
+    public const int CombatMedic = 49;
+    public const int Arbalest = 51;
+    public const int Duelist = 52;
+    public const int Lycanthrope = 53;
+    public const int Peerage = 54;
+    public const int FiendishCorrupted = 55;
+    public const int Bloodsworn = 56;
+    public const int Warlock = 57;
 }
