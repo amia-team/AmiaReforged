4780238ae8d403d50a646ef625b056f20d181a51
Mitro Hyvärinen <mitro.hyvarinen@gmail.com>
2025-02-12 01:43:10 +0200

    Move quest requirements to separate class

---
+++ AmiaReforged.System/Dynamic/GenericQuest/QuestRequirements.cs
@@ -0,0 +1,141 @@
+﻿using static AmiaReforged.System.Dynamic.GenericQuest.QuestConstants;
+using Anvil.API;
+using NWN.Core;
+
+namespace AmiaReforged.System.Dynamic.GenericQuest;
+
+public static class QuestRequirements
+{
+    public static bool CheckQuestRequirements(NwCreature questGiver, NwCreature playerCharacter)
+    {
+        if (CheckRequiredQuests(questGiver, playerCharacter) is false)
+            return false;
+
+        if (CheckRequiredClasses(questGiver, playerCharacter) is false)
+            return false;
+
+        if (CheckRequiredAlignments(questGiver, playerCharacter) is false)
+            return false;
+
+        if (CheckRequiredSkills(questGiver, playerCharacter) is false)
+            return false;
+
+        // set as eg "[message]"
+        LocalVariableString rejectionMessage = questGiver.GetObjectVariable<LocalVariableString>("rejection message");
+
+        return false;
+    }
+
+    private static bool CheckRequiredSkills(NwCreature questGiver, NwCreature playerCharacter)
+    {
+        // set as eg "perform 5 |  persuade 10" or "perform 5 & persuade 10"
+        LocalVariableString requiredSkills = questGiver.GetObjectVariable<LocalVariableString>("required skills");
+
+        return false;
+    }
+
+    private static bool CheckRequiredAlignments(NwCreature questGiver, NwCreature playerCharacter)
+    {
+        // set as eg "neutral | evil" or "neutral & evil"
+        LocalVariableString requiredAlignments =
+            questGiver.GetObjectVariable<LocalVariableString>("required alignments");
+
+        return false;
+    }
+
+    private static bool CheckRequiredClasses(NwCreature questGiver, NwCreature playerCharacter)
+    {
+        // set as eg "fighter 5 |  barbarian 10" or "fighter 5 & barbarian 10"
+        LocalVariableString requiredClasses = questGiver.GetObjectVariable<LocalVariableString>("required classes");
+        
+        return false;
+    }
+
+    /// <summary>
+    /// Checks that the quests required to pick up the new quest are completed
+    /// </summary>
+    /// <returns>True if required quests are completed, false if not</returns>
+    private static bool CheckRequiredQuests(NwCreature questGiver, NwCreature playerCharacter)
+    {
+        // set in the toolset as eg "[quest name 1] | [quest name 2]" or "[quest name 1] & [quest name 2]"
+        LocalVariableString requiredQuests = questGiver.GetObjectVariable<LocalVariableString>("required quests");
+
+        // If no requirements are set, return true so the quest script knows that the quest can be taken
+        if (requiredQuests.HasNothing)
+            return true;
+
+
+        // If there is a quest requirement, check that the required quest has been completed to return true and allow
+        // character to take on the quest; otherwise play rejection message, inform the player of the quest they need
+        // to complete, and return false in the quest script to not start the quest
+        if (requiredQuests.HasValue)
+        {
+            string[] requiredQuestsAny = requiredQuests.Value!.Split(" | ");
+            string[] requiredQuestsAll = requiredQuests.Value!.Split(" & ");
+
+            NwItem pcKey = playerCharacter.Inventory.Items.First(item => item.ResRef == "ds_pckey");
+
+            // If there's only one required quest, check for it first
+            if (requiredQuestsAny.Length == 1)
+            {
+                int requiredQuestStatus = pcKey.GetObjectVariable<LocalVariableInt>(requiredQuestsAny[0]).Value;
+                if (requiredQuestStatus is QuestCompleted)
+                    return true;
+
+                string requiredQuest = requiredQuestsAny[0].ColorString(ColorConstants.Green);
+
+                playerCharacter.LoginPlayer!.SendServerMessage
+                    ($"You must complete {requiredQuest} before you may begin this one.");
+
+                return false;
+            }
+
+            // If required quests are seperated by " | ", character must have completed one of the required quests
+            if (requiredQuestsAny.Length > 1)
+                for (int i = 0; i < requiredQuestsAny.Length; i++)
+                {
+                    int requiredQuestStatus = pcKey.GetObjectVariable<LocalVariableInt>(requiredQuestsAny[i]).Value;
+
+                    if (requiredQuestStatus is QuestCompleted) return true;
+
+                    if (i != requiredQuestsAny.Length - 1) continue;
+
+                    // Last loop iteration if there's still no qualifying quest completed
+
+                    string requiredQuestsJoined =
+                        string.Join(", ", requiredQuestsAny).ColorString(ColorConstants.Green);
+                    playerCharacter.LoginPlayer!.SendServerMessage
+                        ($"You must complete one of the following quests before you may begin this one: {requiredQuestsJoined}");
+
+                    return false;
+                }
+
+            // If required quests are seperated by " & ", character must have completed all the required quests
+            List<string> requiredQuestsList = null!;
+
+            foreach (string requiredQuest in requiredQuestsAll)
+            {
+                int requiredQuestStatus = pcKey.GetObjectVariable<LocalVariableInt>(requiredQuest).Value;
+                if (requiredQuestStatus is not QuestCompleted)
+                    requiredQuestsList.Add(requiredQuest);
+            }
+
+            switch (requiredQuestsList.Count)
+            {
+                case 0:
+                    return true;
+                case < 0:
+                {
+                    string requiredQuestsJoined =
+                        string.Join(", ", requiredQuestsList).ColorString(ColorConstants.Green);
+                    playerCharacter.LoginPlayer!.SendServerMessage
+                        ($"You must complete one of the following quests before you may begin this one: {requiredQuestsJoined}");
+
+                    return false;
+                }
+            }
+        }
+
+        return false;
+    }
+}
--- AmiaReforged.System/Dynamic/GenericQuest/QuestService.cs
+++ AmiaReforged.System/Dynamic/GenericQuest/QuestService.cs
@@ -3,10 +3,13 @@ using Anvil.API;
 using Anvil.API.Events;
 using Anvil.Services;
 using NLog;
-using NWN.Core;
 
 namespace AmiaReforged.System.Dynamic.GenericQuest;
 
+/// <summary>
+/// This code injects new and improved behaviour and setup for the old ee_generic_quest script.
+/// The old ee_generic_quest script is kept unchanged, so people preferring that method can use it.
+/// </summary>
 [ServiceBinding(typeof(QuestService))]
 public class QuestService
 {
@@ -23,177 +26,46 @@ public class QuestService
         
         CreatureEvents.OnConversation eventData = new();
         
-        // Indexing for the quest variables starts at 1 for legacy reasons;
-        // non-indexed quest variables like "questname" are treated as "questname1"
-        
-        int index = 1;
-        
-        // START VARIABLE DECLARATION
+        // START VARIABLE DECLARATION FOR LEGACY VARIABLES
 
         NwCreature playerCharacter = eventData.PlayerSpeaker!.ControlledCreature!;
         NwCreature questGiver = eventData.Creature;
         
         NwItem pcKey = playerCharacter.Inventory.Items.First(item => item.ResRef == "ds_pckey");
-
-        // The next three local vars must be found on any quest, so we give debug messages if they're not provided!
         
-        LocalVariableString questName = questGiver.GetObjectVariable<LocalVariableString>("questname"+index);
-        if (questName.HasNothing)
-        {
-            questName = questGiver.GetObjectVariable<LocalVariableString>("questname");
-            if (questName.HasNothing) playerCharacter.LoginPlayer!.SendServerMessage("DEBUG: No quest name found.");
-        }
+        LocalVariableString legacyQuestName = questGiver.GetObjectVariable<LocalVariableString>("questname");
+        LocalVariableString questNames = questGiver.GetObjectVariable<LocalVariableString>("quest names");
+        if (legacyQuestName.HasNothing && questNames.HasNothing)
+            playerCharacter.LoginPlayer!.SendServerMessage
+                ("DEBUG: No quest names found in quest NPC's local variables.");
         
-        LocalVariableString speechWithQuest = questGiver.GetObjectVariable<LocalVariableString>("speechwithquest"+index);
-        if (speechWithQuest.HasNothing)
-        {
-            speechWithQuest = questGiver.GetObjectVariable<LocalVariableString>("speechwithquest");
-            if (speechWithQuest.HasNothing) playerCharacter.LoginPlayer!.SendServerMessage("DEBUG: No speech with quest found.");
-        }
+        LocalVariableString legacySpeechWithQuest = questGiver.GetObjectVariable<LocalVariableString>("speechwithquest");
+        LocalVariableString messagesWithQuest = questGiver.GetObjectVariable<LocalVariableString>("messages with quest taken");
+        if (legacySpeechWithQuest.HasNothing && messagesWithQuest.HasNothing) 
+            playerCharacter.LoginPlayer!.SendServerMessage
+                ("DEBUG: No messages with quest taken found in quest NPC's local variables.");
         
-        LocalVariableString speechQuestDone = questGiver.GetObjectVariable<LocalVariableString>("speechquestdone"+index);
-        if (speechQuestDone.HasNothing)
-        {
-            speechQuestDone = questGiver.GetObjectVariable<LocalVariableString>("speechquestdone");
-            if (speechQuestDone.HasNothing) playerCharacter.LoginPlayer!.SendServerMessage("DEBUG: No speech quest done found.");
-        }
+        LocalVariableString legacySpeechQuestDone = questGiver.GetObjectVariable<LocalVariableString>("speechquestdone");
+        LocalVariableString messagesWithQuestDone = questGiver.GetObjectVariable<LocalVariableString>("messages with quest completed");
+        if (legacySpeechQuestDone.HasNothing && messagesWithQuestDone.HasNothing)
+            playerCharacter.LoginPlayer!.SendServerMessage
+                ("DEBUG: No messages with quest completed found in quest NPC's local variables.");
         
-        LocalVariableString questItemTag = questGiver.GetObjectVariable<LocalVariableString>("questitem"+index);
+        
+        LocalVariableString questItemTag = questGiver.GetObjectVariable<LocalVariableString>("questitem");
+        LocalVariableString questItemTags = questGiver.GetObjectVariable<LocalVariableString>("quest item tags");
+        if (questItemTag.HasNothing && questItemTags.HasNothing)
+            playerCharacter.LoginPlayer!.SendServerMessage
+                ("DEBUG: No quest item tags found in quest NPC's local variables.");
         
         NwItem questItem = playerCharacter.Inventory.Items.First(item => item.Tag == questItemTag);
         
         // END VARIABLE DECLARATION
-    
         questGiver.FaceToObject(playerCharacter);
         
-        CheckQuestRequirements(questGiver, playerCharacter);
+        QuestRequirements.CheckQuestRequirements(questGiver, playerCharacter);
 
     }
         
-    /// <summary>
-    /// Checks for class, skill, alignment, and prequel quest requirements when trying to take the quest
-    /// and makes NPC say the rejection message
-    /// </summary>
-    /// <returns>true if requirements are met; false if not</returns>
-    private static bool CheckQuestRequirements(NwCreature questGiver, NwCreature playerCharacter)
-    {
-        // Indexing for the quest variables starts at 1 for legacy reasons;
-        // non-indexed quest variables like "questname" are treated as "questname1"
-        int index = 1;
-        
-        LocalVariableString questRequired  = questGiver.GetObjectVariable<LocalVariableString>("questrequired"+index);
-        LocalVariableString classRequired = questGiver.GetObjectVariable<LocalVariableString>("classrequired"+index);
-        LocalVariableInt classLevelRequired = questGiver.GetObjectVariable<LocalVariableInt>("classlevelrequired"+index);
-        LocalVariableString skillRequired = questGiver.GetObjectVariable<LocalVariableString>("skillrequired"+index);
-        LocalVariableInt skillRankRequired = questGiver.GetObjectVariable<LocalVariableInt>("skillrankrequired"+index);
-        LocalVariableString alignmentRequired = questGiver.GetObjectVariable<LocalVariableString>("alignmentrequired"+index);
-        LocalVariableString rejectionMessage = questGiver.GetObjectVariable<LocalVariableString>("rejectionmessage");
-        
-        // Legacy restriction variables to keep existing quests functional
-        LocalVariableString classRestrictedOn = questGiver.GetObjectVariable<LocalVariableString>("classrestrictedon");
-        LocalVariableString skillRestrictedOn = questGiver.GetObjectVariable<LocalVariableString>("skillrestrictedon");
-        LocalVariableInt classRestricted = questGiver.GetObjectVariable<LocalVariableInt>("classrestricted");
-        LocalVariableInt skillRestricted = questGiver.GetObjectVariable<LocalVariableInt>("skillrestricted");
-        LocalVariableInt skillRestrictedPoints = questGiver.GetObjectVariable<LocalVariableInt>("skillrestrictedpoints");
-        LocalVariableInt skillRestrictedPointsBase = questGiver.GetObjectVariable<LocalVariableInt>("skillrestrictedpointsbase");
-        LocalVariableString speechQuestRequired = questGiver.GetObjectVariable<LocalVariableString>("speechquestrequired");
-        
-        // If no requirements are set, return to quest script
-        if (questRequired.HasNothing && classRequired.HasNothing && skillRequired.HasNothing
-            && classRestrictedOn.HasNothing && skillRestrictedOn.HasNothing) return true;
 
-        if (questRequired.HasValue)
-        {
-            NwItem pcKey = playerCharacter.Inventory.Items.First(item => item.ResRef == "ds_pckey");
-            int requiredQuestStatus = pcKey.GetObjectVariable<LocalVariableInt>(questRequired.Value!);
-
-            if (requiredQuestStatus is not QuestCompleted)
-            {
-                if (rejectionMessage.HasNothing) 
-                    playerCharacter.LoginPlayer!.SendServerMessage("DEBUG: No quest rejection message.");
-                else
-                    questGiver.SpeakString(rejectionMessage!);
-
-                string requiredQuestGreen = questRequired.Value!.ColorString(ColorConstants.Green);
-                playerCharacter.LoginPlayer!.SendServerMessage
-                    ($"You must complete {requiredQuestGreen} before you may begin this one.");
-
-                return false;
-            }
-        }
-
-        if (classRequired.HasValue)
-        {
-            NwClass? requiredClass = classRequired.Value switch
-            {
-                "barb" or "barbarian" or "Barb" or "Barbarian" 
-                    => NwClass.FromClassId(NWScript.CLASS_TYPE_BARBARIAN),
-                "bard" or "Bard" => NwClass.FromClassId(NWScript.CLASS_TYPE_BARD),
-                "cleric" or "Cleric" => NwClass.FromClassId(NWScript.CLASS_TYPE_CLERIC),
-                "druid" or "Druid" => NwClass.FromClassId(NWScript.CLASS_TYPE_DRUID),
-                "fighter" or "Fighter" => NwClass.FromClassId(NWScript.CLASS_TYPE_FIGHTER),
-                "monk" or "Monk" => NwClass.FromClassId(NWScript.CLASS_TYPE_MONK),
-                "pala" or "paladin" or "Pala" or "Paladin"
-                    => NwClass.FromClassId(NWScript.CLASS_TYPE_PALADIN),
-                "ranger" or "Ranger" => NwClass.FromClassId(NWScript.CLASS_TYPE_RANGER),
-                "rogue" or "rouge" or "Rogue" or "Rouge" 
-                    => NwClass.FromClassId(NWScript.CLASS_TYPE_ROGUE),
-                "sorc" or "sorcerer" or "sorceror" or "Sorc" or "Sorcerer" or "Sorceror"
-                    => NwClass.FromClassId(NWScript.CLASS_TYPE_SORCERER),
-                "wiz" or "wizard" or "Wiz" or "Wizard" 
-                    => NwClass.FromClassId(NWScript.CLASS_TYPE_WIZARD),
-                "shifter" or "Shifter" => NwClass.FromClassId(NWScript.CLASS_TYPE_SHIFTER),
-                "warlock" or "Warlock" => NwClass.FromClassId(Warlock),
-                "sd" or "shadowdancer" or "shadow dancer" or "SD" or "Shadowdancer" or "Shadow Dancer"
-                    => NwClass.FromClassId(NWScript.CLASS_TYPE_SHADOWDANCER),
-                "ms" or "masterscout" or "master scout" or "MS" or "Masterscout" or "Master Scout" 
-                    => NwClass.FromClassId(NWScript.CLASS_TYPE_HARPER),
-                "aa" or "arcane archer" or "AA" or "Arcane Archer"
-                    => NwClass.FromClassId(NWScript.CLASS_TYPE_ARCANE_ARCHER),
-                "ass" or "assassin" or "Ass" or "Assassin"
-                    => NwClass.FromClassId(NWScript.CLASS_TYPE_ASSASSIN),
-                "bg" or "blackguard" or "BG" or "Blackguard" 
-                    => NwClass.FromClassId(NWScript.CLASS_TYPE_BLACKGUARD),
-                "dc" or "cot" or "divine champion" or "DC" or "CoT" or "Divine Champion"
-                    => NwClass.FromClassId(NWScript.CLASS_TYPE_DIVINE_CHAMPION),
-                "wm" or "weapon master" or "WM" or "Weapon Master" => NwClass.FromClassId(NWScript.CLASS_TYPE_WEAPON_MASTER),
-                "pm" or "palemaster" or "pale master" or "PM" or "Palemaster" or "Pale Master"
-                    => NwClass.FromClassId(NWScript.CLASS_TYPE_PALE_MASTER),
-                "dwd" or "dwarven defender" or "DwD" or "Dwarven Defender"
-                    => NwClass.FromClassId(NWScript.CLASS_TYPE_DWARVEN_DEFENDER),
-                "dd" or "rdd" or "dragon disciple" or "DD" or "RDD" or "Dragon Disciple"
-                    => NwClass.FromClassId(NWScript.CLASS_TYPE_DRAGON_DISCIPLE),
-                "kc" or "pdk" or "knight commander" or "KC" or "PDK" or "Knight Commander"
-                    => NwClass.FromClassId(NWScript.CLASS_TYPE_PURPLE_DRAGON_KNIGHT),
-                "warslinger" or "Warslinger" => NwClass.FromClassId(Warslinger),
-                "cavalry" or "cavalier" or "Cavalry" or "Cavalier" => NwClass.FromClassId(Cavalry),
-                "twf" or "two weapon fighter" or "two-weapon fighter" or "TWF" or "Two Weapon Fighter" or "Two-Weapon Fighter"
-                    => NwClass.FromClassId(TwoWeaponFighter),
-                "bowmaster" or "bow master" or "Bowmaster" or "Bow Master" => NwClass.FromClassId(BowMaster),
-                "def" or "defender" or "Def" or "Defender" => NwClass.FromClassId(Defender),
-                "combat medic" or "Combat Medic" => NwClass.FromClassId(CombatMedic),
-                "arbalest" or "Arbalest" => NwClass.FromClassId(Arbalest),
-                "duelist" or "duellist" or "Duelist" or "Duellist" => NwClass.FromClassId(Duelist),
-                "lycan" or "lycanthrope" or "Lycan" or "Lycanthrope" => NwClass.FromClassId(Lycanthrope),
-                "peer" or "peerage" or "Peer" or "Peerage" => NwClass.FromClassId(Peerage),
-                "fiendish corrupted" or "Fiendish Corrupted" => NwClass.FromClassId(FiendishCorrupted),
-                "bloodsworn" or "Bloodsworn" => NwClass.FromClassId(Bloodsworn),
-                _ => null
-            };
-            if (requiredClass is null) playerCharacter.ControllingPlayer!.SendServerMessage
-                ($"DEBUG: \"{classRequired.Value}\" is a wrong input for quest's required class.");
-
-            if (playerCharacter.GetClassInfo(requiredClass).Level < classLevelRequired.Value)
-            {
-                if (rejectionMessage.HasNothing) 
-                    playerCharacter.LoginPlayer!.SendServerMessage("DEBUG: No quest rejection message.");
-                else
-                    questGiver.SpeakString(rejectionMessage!);
-            }
-            
-            
-        }
-
-        return true;
-    }
 }
