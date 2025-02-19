9ba7593525db7ffc0cf0a4c857ba984e7037ea0e
Caelan L. Talmadge <34071000+cltalmadge@users.noreply.github.com>
2025-02-18 23:41:48 -0600

    Make SpellDecorator for adding behaviors dynamically at runtime

---
+++ AmiaReforged.Classes/Spells/SpellDecorator.cs
@@ -0,0 +1,21 @@
+﻿using Anvil.API.Events;
+
+namespace AmiaReforged.Classes.Spells;
+
+public abstract class SpellDecorator : ISpell
+{
+    protected readonly ISpell _spell;
+
+    protected SpellDecorator(ISpell spell)
+    {
+        _spell = spell;
+    }
+    
+    public virtual string ImpactScript => _spell.ImpactScript;
+    
+    public virtual void OnSpellImpact(SpellEvents.OnSpellCast eventData)
+    {
+        _spell.OnSpellImpact(eventData);
+    }
+    
+}
