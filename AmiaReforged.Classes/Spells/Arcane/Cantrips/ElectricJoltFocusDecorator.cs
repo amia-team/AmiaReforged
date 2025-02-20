dba12ec1c6dbdbfbc18c1688d7993419d6925c63
Caelan L. Talmadge <talmadge12@gmail.com>
2025-02-20 10:17:53 -0600

    Flesh out electric jolt focus boons

--- AmiaReforged.Classes/Spells/Arcane/Cantrips/ElectricJoltFocusDecorator.cs
+++ AmiaReforged.Classes/Spells/Arcane/Cantrips/ElectricJoltFocusDecorator.cs
@@ -8,6 +8,8 @@ namespace AmiaReforged.Classes.Spells.Arcane.Cantrips;
 [DecoratesSpell(typeof(ElectricJolt))]
 public class ElectricJoltFocusDecorator : SpellDecorator
 {
+    private const double TwoRounds = 12;
+
     public ElectricJoltFocusDecorator(ISpell spell) : base(spell)
     {
         Spell = spell;
@@ -24,14 +26,29 @@ public class ElectricJoltFocusDecorator : SpellDecorator
 
         if (target is not NwCreature creature) return;
         if (caster is not NwCreature casterCreature) return;
-        
+
         LogManager.GetCurrentClassLogger().Info("Electric Jolt focus decorator");
 
-        bool isEvocationFocused = casterCreature.Feats.Any(f => f.Id == (ushort)Feat.SpellFocusEvocation) ||
-                                  casterCreature.Feats.Any(f => f.Id == (ushort)Feat.GreaterSpellFocusEvocation) ||
-                                  casterCreature.Feats.Any(f => f.Id == (ushort)Feat.EpicSpellFocusEvocation);
+        bool basicFocus = casterCreature.Feats.Any(f => f.Id == (ushort)Feat.SpellFocusEvocation);
+        bool greaterFocus = casterCreature.Feats.Any(f => f.Id == (ushort)Feat.GreaterSpellFocusEvocation);
+        bool epicFocus = casterCreature.Feats.Any(f => f.Id == (ushort)Feat.EpicSpellFocusEvocation);
 
-        if (isEvocationFocused) creature.SpeakString("Zoom!");
+        bool isEvocationFocused = basicFocus || greaterFocus || epicFocus;
+
+        if (isEvocationFocused && Result == ResistSpellResult.Failed)
+        {
+            int electricSavePenalty = epicFocus ? 3 : greaterFocus ? 2 : basicFocus ? 1 : 0;
+
+            Effect savePenalty =
+                Effect.SavingThrowDecrease(SavingThrow.All, electricSavePenalty, SavingThrowType.Electricity);
+            savePenalty.Tag = "ElecJoltFocusDecorator";
+            
+            Effect? existing = creature.ActiveEffects.FirstOrDefault(e => e.Tag == "ElecJoltFocusDecorator");
+            
+            if(existing != null) creature.RemoveEffect(existing);
+            
+            target.ApplyEffect(EffectDuration.Temporary, savePenalty, TimeSpan.FromSeconds(TwoRounds));
+        }
 
         Spell.OnSpellImpact(eventData);
     }
