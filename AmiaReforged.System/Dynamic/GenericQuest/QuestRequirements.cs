ec04ad5a41e057d2cb920268f39335b5202d0be3
Mitro Hyvärinen <mitro.hyvarinen@gmail.com>
2025-02-20 06:05:06 +0200

    Add class requirement check

--- AmiaReforged.System/Dynamic/GenericQuest/QuestRequirements.cs
+++ AmiaReforged.System/Dynamic/GenericQuest/QuestRequirements.cs
@@ -1,6 +1,8 @@
 ﻿using System.Text.RegularExpressions;
 using static AmiaReforged.System.Dynamic.GenericQuest.QuestConstants;
 using Anvil.API;
+using Microsoft.IdentityModel.Tokens;
+using NWN.Core;
 using static System.Int32;
 
 namespace AmiaReforged.System.Dynamic.GenericQuest;
@@ -55,43 +57,89 @@ public static class QuestRequirements
     private static bool CheckRequiredClasses(NwCreature questGiver, NwCreature playerCharacter)
     {
         // set as eg "fighter 5 || barbarian 10" or "fighter 5 && barbarian 10"
-        LocalVariableString requiredClasses = questGiver.GetObjectVariable<LocalVariableString>("required classes");
+        string? requiredClasses = questGiver.GetObjectVariable<LocalVariableString>("required classes").Value;
         
         // If no requirements are set, return true
-        if (requiredClasses.HasNothing)
+        if (requiredClasses is null)
             return true;
-        
-        NwItem pcKey = playerCharacter.Inventory.Items.First(item => item.ResRef == "ds_pckey");
 
+        string[] requiredClassesAny = QuestUtil.SanitizeAndSplit(requiredClasses, "||");
+        string[] requiredClassesAll = QuestUtil.SanitizeAndSplit(requiredClasses, "&&");
 
-        string[] requiredClassesAny = QuestUtilFuncs.SanitizeAndSplit(requiredClasses!, "||");
-        string[] requiredClassesAll = QuestUtilFuncs.SanitizeAndSplit(requiredClasses!, "&&");
-        string[] requiredClassesNone = QuestUtilFuncs.SanitizeAndSplit(requiredClasses!, "!!");
+        string requiredClassesJoined;
         
-        // Populate an int array with values from the requiredClasses string;
-        // replace null values with 1s in case quest maker hasn't specified the class level
-        int?[] requiredClassesAnyLevels = new int?[requiredClassesAny.Length];
-        for (int i = 0; i < requiredClassesAny.Length; i++)
+        if (requiredClassesAny.Length >= requiredClassesAll.Length)
         {
-            requiredClassesAny[i] = Regex.Match(requiredClassesAny[i], @"\d+").Value;
-            requiredClassesAnyLevels[i] = 
-                TryParse(requiredClassesAny[i], out int level) ? level : (int?)null ?? 1;
-        }
+            // Populate an int array with class levels from the required classes string;
+            // replace null values with 1s in case quest maker hasn't specified the class level
             
+            int?[] requiredClassesAnyLevels = new int?[requiredClassesAny.Length];
+            for (int i = 0; i < requiredClassesAny.Length; i++)
+            {
+                requiredClassesAny[i] = Regex.Match(requiredClassesAny[i], @"\d+").Value;
+                requiredClassesAnyLevels[i] = 
+                    TryParse(requiredClassesAny[i], out int level) ? level : (int?)null ?? 1;
+            }
+            
+            for (int i = 0; i < requiredClassesAny.Length; i++)
+            {
+                NwClass? requiredClass = QuestUtil.GetRequiredClass(requiredClassesAny[i]);
+                int? requiredLevel = requiredClassesAnyLevels[i];
+                
+                if (requiredClass is null) playerCharacter.LoginPlayer!.SendServerMessage
+                    ($"DEBUG: Required class \"{requiredClassesAny[i]}\" is an invalid input.");
+                
+                // If the class requirement has '!' in it, then that class is banned from taking the quest
+                if (requiredClassesAny[i].Contains('!') 
+                    && playerCharacter.GetClassInfo(requiredClass)!.Level >= requiredLevel)
+                {
+                    playerCharacter.LoginPlayer!.SendServerMessage
+                        ($"Class {requiredClassesAny[i]} cannot take this quest.");
+                    return false;
+                }
+
+                if (playerCharacter.GetClassInfo(requiredClass)!.Level >= requiredLevel)
+                    return true;
+            }
+            
+            requiredClassesJoined = string.Join("; ", requiredClassesAny);
+            
+            playerCharacter.LoginPlayer!.SendServerMessage
+                ($"One of the following classes is required to take this quest: {requiredClassesJoined}");
+            return false;
+        }
         
         int?[] requiredClassesAllLevels = new int?[requiredClassesAll.Length];
         for (int i = 0; i < requiredClassesAll.Length; i++)
         {
-            requiredClassesAny[i] = Regex.Match(requiredClassesAny[i], @"\d+").Value;
+            requiredClassesAll[i] = Regex.Match(requiredClassesAll[i], @"\d+").Value;
             requiredClassesAllLevels[i] = 
                 TryParse(requiredClassesAll[i], out int level) ? level : (int?)null ?? 1;
         }
-            
         
-        if (requiredClasses is null)
-            playerCharacter.ControllingPlayer!.SendServerMessage
-                ($"DEBUG: \"{requiredClasses.Value}\" is a wrong input for quest's required class.");
-
+        List<string> requiredClassesList = null!;
+        
+        for (int i = 0; i < requiredClassesAll.Length; i++)
+        {
+            NwClass? requiredClass = QuestUtil.GetRequiredClass(requiredClassesAll[i]);
+            int? requiredLevel = requiredClassesAllLevels[i];
+            
+            if (requiredClass is null)
+                playerCharacter.LoginPlayer!.SendServerMessage
+                    ($"DEBUG: Required class \"{requiredClassesAll[i]}\" is an invalid input.");
+            
+            // Add required classes that the PC doesn't have to the naughty list
+            if (playerCharacter.GetClassInfo(requiredClass)!.Level < requiredLevel)
+                requiredClassesList.Add(requiredClassesAll[i]);
+        }
+        
+        // If nothing was added to the naughty list, it means that PC has all required classes and quest can be taken 
+        if (requiredClassesList.IsNullOrEmpty()) return true;
+        
+        requiredClassesJoined = string.Join("; ", requiredClassesList).ColorString(ColorConstants.Green);
+        
+        playerCharacter.LoginPlayer!.SendServerMessage
+            ($"The following classes are required to take this quest: {requiredClassesJoined}");
         
         return false;
     }
@@ -116,68 +164,49 @@ public static class QuestRequirements
         // character to take on the quest; otherwise play rejection message, inform the player of the quest they need
         // to complete, and return false in the quest script to not start the quest
         
-        string[] requiredQuestsAny = QuestUtilFuncs.SanitizeAndSplit(requiredQuests, "||");
-        string[] requiredQuestsAll = QuestUtilFuncs.SanitizeAndSplit(requiredQuests, "&&");
+        string[] requiredQuestsAny = QuestUtil.SanitizeAndSplit(requiredQuests, "||");
+        string[] requiredQuestsAll = QuestUtil.SanitizeAndSplit(requiredQuests, "&&");
 
-        // If there's only one required quest, check for it first
-        if (requiredQuestsAny.Length == 1)
+        // If required quests are seperated by "||", character must have completed one of the required quests
+        string requiredQuestsJoined;
+
+        if (requiredQuestsAny.Length >= requiredQuestsAll.Length)
         {
-            int requiredQuestStatus = pcKey.GetObjectVariable<LocalVariableInt>(requiredQuestsAny[0]).Value;
-            if (requiredQuestStatus is QuestCompleted)
-                return true;
+            foreach (string requiredQuest in requiredQuestsAny)
+            {
+                int requiredQuestStatus = pcKey.GetObjectVariable<LocalVariableInt>(requiredQuest).Value;
 
-            string requiredQuest = requiredQuestsAny[0].ColorString(ColorConstants.Green);
-
+                if (requiredQuestStatus is QuestCompleted) return true;
+            }
+            
+            requiredQuestsJoined = string.Join("; ", requiredQuestsAny).ColorString(ColorConstants.Green);
+            
             playerCharacter.LoginPlayer!.SendServerMessage
-                ($"You must complete {requiredQuest} before you may begin this one.");
+                ($"You must complete one of the following quests before you may begin this one: {requiredQuestsJoined}");
 
             return false;
         }
-
-        // If required quests are seperated by " | ", character must have completed one of the required quests
-        if (requiredQuestsAny.Length > 1)
-            for (int i = 0; i < requiredQuestsAny.Length; i++)
-            {
-                int requiredQuestStatus = pcKey.GetObjectVariable<LocalVariableInt>(requiredQuestsAny[i]).Value;
-
-                if (requiredQuestStatus is QuestCompleted) return true;
-
-                if (i != requiredQuestsAny.Length - 1) continue;
-
-                // Last loop iteration if there's still no qualifying quest completed
-
-                string requiredQuestsJoined =
-                    string.Join(", ", requiredQuestsAny).ColorString(ColorConstants.Green);
-                playerCharacter.LoginPlayer!.SendServerMessage
-                    ($"You must complete one of the following quests before you may begin this one: {requiredQuestsJoined}");
-
-                return false;
-            }
-
-        // If required quests are seperated by " & ", character must have completed all the required quests
+            
+        
+        // If required quests are seperated by "&&", character must have completed all the required quests
         List<string> requiredQuestsList = null!;
 
         foreach (string requiredQuest in requiredQuestsAll)
         {
             int requiredQuestStatus = pcKey.GetObjectVariable<LocalVariableInt>(requiredQuest).Value;
+            
+            // Add uncompleted required quests to a naughty list
             if (requiredQuestStatus is not QuestCompleted)
                 requiredQuestsList.Add(requiredQuest);
         }
-
-        switch (requiredQuestsList.Count)
-        {
-            case 0:
-                return true;
-            case < 0:
-            {
-                string requiredQuestsJoined =
-                    string.Join(", ", requiredQuestsList).ColorString(ColorConstants.Green);
-                playerCharacter.LoginPlayer!.SendServerMessage
-                    ($"You must complete one of the following quests before you may begin this one: {requiredQuestsJoined}");
-
-                return false;
-            }
-        }
+        
+        // If nothing was added to the naughty list, it means that all required quests are completed and quest can be taken
+        if (requiredQuestsList.IsNullOrEmpty()) return true;
+        
+        requiredQuestsJoined = string.Join("; ", requiredQuestsList).ColorString(ColorConstants.Green);
+        
+        playerCharacter.LoginPlayer!.SendServerMessage
+            ($"You must complete all of the following quests before you may begin this one: {requiredQuestsJoined}");
 
         return false;
     }
