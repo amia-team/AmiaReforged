59c872c7a8e16ba41753e0265314141f63cbbb55
Caelan L. Talmadge <talmadge12@gmail.com>
2025-02-20 08:38:24 -0600

    Flesh out Ray of Harm

--- AmiaReforged.Classes/Spells/Arcane/Cantrips/RayofHarm.cs
+++ AmiaReforged.Classes/Spells/Arcane/Cantrips/RayofHarm.cs
@@ -1,6 +1,7 @@
 ﻿using Anvil.API;
 using Anvil.API.Events;
 using Anvil.Services;
+using NWN.Core;
 
 namespace AmiaReforged.Classes.Spells.Arcane.Cantrips;
 
@@ -10,7 +11,28 @@ public class RayofHarm : ISpell
     public string ImpactScript => "am_s_rayofharm";
     public void OnSpellImpact(SpellEvents.OnSpellCast eventData)
     {
-        throw new NotImplementedException();
+        NwGameObject? caster = eventData.Caster;
+        if(caster == null) return;
+        if(caster is not NwCreature casterCreature) return;
+        
+        NwGameObject? target = eventData.TargetObject;
+        if(target == null) return;
+        if(target is not NwCreature creature) return;
+        
+        Effect beam = Effect.Beam(VfxType.BeamBlack, caster, BodyNode.Hand);
+        target.ApplyEffect(EffectDuration.Instant, beam);
+        
+        int numberOfDie = caster.CasterLevel / 2;
+        
+        bool isSpecialist = casterCreature.GetSpecialization(NwClass.FromClassType(ClassType.Wizard)) == SpellSchool.Necromancy;
+        int damage = isSpecialist ? NWScript.d4(numberOfDie) : NWScript.d3(numberOfDie);
+                
+        Effect damageEffect = Effect.Damage(damage, DamageType.Negative);
+
+        if (Result == ResistSpellResult.Failed)
+        {
+            target.ApplyEffect(EffectDuration.Instant, damageEffect);
+        }
     }
 
     public ResistSpellResult Result { get; set; }
