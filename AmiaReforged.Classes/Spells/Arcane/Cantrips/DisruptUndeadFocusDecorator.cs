01b27abdac718b6edfc37a1bad5448ddf6f75cf0
Caelan L. Talmadge <34071000+cltalmadge@users.noreply.github.com>
2025-02-19 21:42:11 -0600

    Flesh out decorator for disrupt undead

--- AmiaReforged.Classes/Spells/Arcane/Cantrips/DisruptUndeadFocusDecorator.cs
+++ AmiaReforged.Classes/Spells/Arcane/Cantrips/DisruptUndeadFocusDecorator.cs
@@ -1,5 +1,6 @@
 ﻿using Anvil.API;
 using Anvil.API.Events;
+using NWN.Core;
 
 namespace AmiaReforged.Classes.Spells.Arcane.Cantrips;
 
@@ -13,7 +14,31 @@ public class DisruptUndeadFocusDecorator : SpellDecorator
 
     public override void OnSpellImpact(SpellEvents.OnSpellCast eventData)
     {
-        
+        NwGameObject? caster = eventData.Caster;
+        if (caster == null) return;
+        NwGameObject? target = eventData.TargetObject;
+        if (target == null) return;
+
+        if (target is not NwCreature creature) return;
+        if (caster is not NwCreature casterCreature) return;
+
+        bool hasNecroFocus = casterCreature.Feats.Any(f => f.Id == (ushort)Feat.SpellFocusNecromancy);
+        bool hasGreaterNecroFocus = casterCreature.Feats.Any(f => f.Id == (ushort)Feat.GreaterSpellFocusNecromancy);
+        bool hasEpicNecroFocus = casterCreature.Feats.Any(f => f.Id == (ushort)Feat.EpicSpellFocusNecromancy);
+
+        bool anyFocus = hasNecroFocus || hasGreaterNecroFocus || hasEpicNecroFocus;
+
+        if (creature.Race == NwRace.FromRacialType(RacialType.Undead) && anyFocus)
+        {
+            int reductionAmount = hasNecroFocus ? 1 : hasGreaterNecroFocus ? 2 : hasEpicNecroFocus ? 3 : 0;
+            
+            Effect acReduce = Effect.ACDecrease(reductionAmount);
+            Effect abReduce = Effect.AttackDecrease(reductionAmount);
+            abReduce = Effect.LinkEffects(abReduce, acReduce);
+            
+            target.ApplyEffect(EffectDuration.Temporary, abReduce, TimeSpan.FromSeconds(6));
+        }
+
         Spell.OnSpellImpact(eventData);
     }
 }
