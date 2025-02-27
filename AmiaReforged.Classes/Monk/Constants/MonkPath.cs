aab009c997c26a1eac752cc3acb1be7c1e1ba182
Mitro Hyvärinen <mitro.hyvarinen@gmail.com>
2025-02-27 22:52:38 +0200

    Rename constants

--- AmiaReforged.Classes/Monk/Constants/MonkPath.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AmiaReforged.Classes.Monk.Constants;
-
-public class MonkPath
-{
-    public const int PathTierOne = 12;
-    public const int PathTierTwo = 18;
-    public const int PathTierThree = 24;
-    public const int AugmentationTierFour = 30;
-}
