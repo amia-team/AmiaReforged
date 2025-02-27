24ef382979d5bc508da322b231762c04d1317061
Mitro Hyvärinen <mitro.hyvarinen@gmail.com>
2025-02-27 20:30:07 +0200

    Add monk path constants

---
+++ AmiaReforged.Classes/Monk/Constants/MonkPath.cs
@@ -0,0 +1,9 @@
+﻿namespace AmiaReforged.Classes.Monk.Constants;
+
+public class MonkPath
+{
+    public const int PathTierOne = 12;
+    public const int PathTierTwo = 18;
+    public const int PathTierThree = 24;
+    public const int AugmentationTierFour = 30;
+}
