398865efa282fa7c4fe1f372c4cb2b171e8c53fc
Maverick00053 <38230616+Maverick00053@users.noreply.github.com>
2025-02-27 22:52:10 -0800

    Cleaned up SwingingCenser added delay

--- AmiaReforged.Classes/Monk/Augmentations/SwingingCenser.cs
+++ AmiaReforged.Classes/Monk/Augmentations/SwingingCenser.cs
@@ -53,75 +53,98 @@ public static class SwingingCenser
         int pulseAmount = 1;
         int healAmount = monkLevel*2;
         double level30Heal = healAmount * 1.5;
+        TimeSpan regenTime = TimeSpan.FromSeconds(6);
+        TimeSpan effectTime = TimeSpan.FromSeconds(monkLevel*6); // Adjust as appropriate. 1 round per monk level.
+        
+        // Wholeness is gained at 7
+        if (monkLevel == 30) 
+        {
+            pulseAmount = 3;
+            healAmount = (int)level30Heal;
+        }
+        else if (monkLevel >= 24)
+        {
+            pulseAmount = 3; 
+        }
+        else if (monkLevel >= 18)
+        {
+            pulseAmount = 2; 
+        }
+        else if (monkLevel >= 12)
+        {
+            pulseAmount = 1; 
+        }
+
+        Effect wholenessEffect = Effect.Heal(healAmount);
+        Effect wholenessVfx = Effect.VisualEffect(VfxType.ImpHealingL, false, 0.7f);
+        Effect wholenessLink = Effect.LinkEffects(wholenessEffect, wholenessVfx);
+        Effect aoeVfx = Effect.VisualEffect(VfxType.ImpPulseHoly, false, 1.0f);
+
+        
+        WholenessPulse();
+        return;
+        
+        async void WholenessPulse()
+        {
+            for (int i = 0; i < pulseAmount; i++) // Pulse amount
+            { 
+            
+                // If monk is dead or monk isn't valid, return
+                if (monk.IsDead || !monk.IsValid) return;
+                
+                monk.ApplyEffect(EffectDuration.Instant,aoeVfx); // AoE Holy Pulse fire at the base of the Monk  
+                
+                foreach (NwGameObject nwObject in monk.Location!.GetObjectsInShape(Shape.Sphere, RadiusSize.Large, false))
+                {
+                    NwCreature creatureInShape = (NwCreature)nwObject;
+                
+                    if (monk.IsReactionTypeFriendly(creatureInShape)) continue;
+                
+                    creatureInShape.ApplyEffect(EffectDuration.Instant, wholenessLink);
+                }
+            
+                await NwTask.Delay(TimeSpan.FromSeconds(3));
+            }
+        }
+
+    }
+
+    private static void AugmentEmptyBody(OnSpellCast castData)
+    { 
+        
+        // WIP 
+        
+        NwCreature monk = (NwCreature)castData.Caster;
+        Location monkLocation = monk.Location;
+        int monkLevel = monk.GetClassInfo(ClassType.Monk)!.Level;
         int regen = 1;
         int concealment = 25;
         TimeSpan regenTime = TimeSpan.FromSeconds(6);
         TimeSpan effectTime = TimeSpan.FromSeconds(monkLevel*6); // Adjust as appropriate. 1 round per monk level.
         
         // Wholeness is gained at 7
-        if (monkLevel == 30)
+        if (monkLevel == 30) 
         {
-            pulseAmount = 3;
-            healAmount = (int)level30Heal;
             regen = 7;
             concealment = 55;
         }
-        else if (monkLevel >= 28)
-        {
-            pulseAmount = 3; 
-            regen = 5;
-            concealment = 50;
-        }
         else if (monkLevel >= 24)
         {
-            pulseAmount = 2; 
-            regen = 4;
+            regen = 5;
             concealment = 45;
         }
-        else if (monkLevel >= 20)
+        else if (monkLevel >= 18)
         {
-            pulseAmount = 2; 
-            regen = 3;
-            concealment = 40;
-        }
-        else if (monkLevel >= 16)
-        {
-            pulseAmount = 1; 
-            regen = 2;
-            concealment = 30;
+            regen = 4;
+            concealment = 35;
         }
         else if (monkLevel >= 12)
         {
-            // Set by default, here just in case
+            regen = 2;
+            concealment = 25;
         }
-
         Effect wholenessRegen = Effect.Regenerate(regen,regenTime);
         Effect wholenessConcealment = Effect.Concealment(concealment, MissChanceType.Normal);
-        Effect wholenessEffect = Effect.Heal(healAmount);
-        Effect wholenessVfx = Effect.VisualEffect(VfxType.ImpHealingL, false, 0.7f);
-        Effect aoeVfx = Effect.VisualEffect(VfxType.ImpPulseHoly, false, 1.0f);
-        
-
-        for (int i = 0; i < pulseAmount; i++) // Pulse amount
-        { 
-            monk.ApplyEffect(EffectDuration.Instant,aoeVfx); // AoE fire at the base of the Monk  
-        }
-            foreach (NwGameObject nwObject in monk.Location!.GetObjectsInShape(Shape.Sphere, RadiusSize.Large, false))
-            {
-                NwCreature creatureInShape = (NwCreature)nwObject;
-                if (monk.IsReactionTypeFriendly(creatureInShape)) continue;
-                creatureInShape.ApplyEffect(EffectDuration.Temporary,wholenessRegen,effectTime);
-                creatureInShape.ApplyEffect(EffectDuration.Temporary,wholenessConcealment,effectTime);
-                for (int e = 0; e < pulseAmount; e++) // Pulse amount
-                {
-                    creatureInShape.ApplyEffect(EffectDuration.Instant, wholenessEffect);
-                    creatureInShape.ApplyEffect(EffectDuration.Instant, wholenessVfx);
-                }
-            }
-        
-    }
-    private static void AugmentEmptyBody(OnSpellCast castData)
-    {
        
     }
     
