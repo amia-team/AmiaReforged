8441ff0f89ee70fdfeb27b99d221e5d07e22e405
Caelan L. Talmadge <talmadge12@gmail.com>
2025-03-03 13:05:44 -0600

    First pass at vicious mockery

---
+++ AmiaReforged.Classes/Spells/Arcane/Cantrips/ViciousMockery/ViciousMockery.cs
@@ -0,0 +1,91 @@
+using Anvil.API;
+using Anvil.API.Events;
+using Anvil.Services;
+using NWN.Core;
+
+namespace AmiaReforged.Classes.Spells.Arcane.Cantrips.ViciousMockery;
+
+[ServiceBinding(typeof(ISpell))]
+public class ViciousMockery : ISpell
+{
+    public ResistSpellResult Result { get; set; }
+
+    public void DoSpellResist(NwCreature creature, NwCreature caster)
+    {
+        Result = creature.CheckResistSpell(caster);
+    }
+
+    public string ImpactScript => "am_c_vicsmock";
+
+    public void OnSpellImpact(SpellEvents.OnSpellCast eventData)
+    {
+        NwGameObject? caster = eventData.Caster;
+        if (caster == null) return;
+
+        bool hasFocus = false;
+        bool hasGreaterFocus = false;
+        bool hasEpicFocus = false;
+        int chaMod = 0;
+        if (caster is NwCreature casterCreature)
+        {
+            hasFocus = casterCreature.Feats.Any(f => f.Id == (ushort)Feat.SpellFocusEnchantment);
+            hasGreaterFocus = casterCreature.Feats.Any(f => f.Id == (ushort)Feat.GreaterSpellFocusEnchantment);
+            hasEpicFocus = casterCreature.Feats.Any(f => f.Id == (ushort)Feat.EpicSpellFocusEnchantment);
+
+            chaMod = casterCreature.GetAbilityModifier(Ability.Charisma);
+        }
+
+        NwGameObject? target = eventData.TargetObject;
+        if (target == null) return;
+
+        // Only works on creatures
+        if (target is not NwCreature targetCreature) return;
+
+        if (target == caster)
+        {
+            NWScript.FloatingTextStringOnCreature("You can't mock yourself!", targetCreature, NWScript.FALSE);
+            return;
+        }
+
+        int focusDice = hasFocus ? 1 : hasGreaterFocus ? 2 : hasEpicFocus ? 3 : 0;
+        int damage = NWScript.d4((caster.CasterLevel / 3) + focusDice);
+
+        const int concentrationPenalty = 10;
+
+        if (Result == ResistSpellResult.Failed)
+        {
+            Effect damageEffect = Effect.Damage(damage, DamageType.Sonic);
+            Effect skillPenalty =
+                Effect.SkillDecrease(NwSkill.FromSkillType(Skill.Concentration)!, concentrationPenalty);
+            skillPenalty.Tag = "VICIOUS_MOCKERY";
+            target.ApplyEffect(EffectDuration.Instant, damageEffect);
+
+
+            Effect? existingSkillPenalty = targetCreature.ActiveEffects.SingleOrDefault(e => e.Tag == "VICIOUS_MOCKERY");
+            if (existingSkillPenalty != null)
+            {
+                targetCreature.RemoveEffect(existingSkillPenalty);
+            }
+
+            if (hasEpicFocus)
+            {
+                targetCreature.ApplyEffect(EffectDuration.Temporary, skillPenalty, TimeSpan.FromSeconds(18));
+            }
+            else
+            {
+                SavingThrowResult result = targetCreature.RollSavingThrow(SavingThrow.Will,
+                    10 + caster.CasterLevel + chaMod, SavingThrowType.Spell);
+
+                if (result == SavingThrowResult.Failure)
+                {
+                    targetCreature.ApplyEffect(EffectDuration.Temporary, skillPenalty, TimeSpan.FromSeconds(18));
+                }
+            }
+        }
+    }
+
+    public void SetResult(ResistSpellResult result)
+    {
+        Result = result;
+    }
+}
