d73ffa81e5f1ac02bec98a45dbb8cdfb5399ce6c
Caelan L. Talmadge <talmadge12@gmail.com>
2025-03-03 13:42:08 -0600

    Accept IDE formatting for Percussion.cs

--- AmiaReforged.Classes/Spells/Arcane/Cantrips/Percussion/Percussion.cs
+++ AmiaReforged.Classes/Spells/Arcane/Cantrips/Percussion/Percussion.cs
@@ -32,8 +32,6 @@ public class Percussion : ISpell
         if (caster == null) return;
         if (caster.Location == null) return;
 
-        NwPlaceable? originObject = NwPlaceable.Create("invisibile", caster.Location);
-
         // Just plays a single looping drum for now.
         string soundTag = $"Percussion_{caster.Name}";
         NwSound? existingSound = caster.GetNearestObjectsByType<NwSound>().FirstOrDefault(s => s.Tag == soundTag);
@@ -44,21 +42,16 @@ public class Percussion : ISpell
 
         NwSound? sound = NwSound.Create("percussion", caster.Location);
         if (sound == null) return;
-
         sound.Tag = soundTag;
-
         Effect aoe = Effect.AreaOfEffect(PersistentVfxType.MobCircchaos!,
             _handleFactory.CreateUniqueHandler(OnEnterPercussion),
             _handleFactory.CreateUniqueHandler(HeartbeatPercussion),
             _handleFactory.CreateUniqueHandler(OnExitPercussion));
-
         aoe.Tag = soundTag;
-
         int roundsToSeconds = caster.CasterLevel * 6;
 
         // Applies the effect at the caster's position.
         caster.Location.ApplyEffect(EffectDuration.Temporary, aoe, TimeSpan.FromSeconds(roundsToSeconds));
-
         NwGameObject? createdArea = caster.GetNearestObjectsByType<NwAreaOfEffect>()
             .SingleOrDefault(a => a.Tag == soundTag);
 
@@ -70,15 +63,12 @@ public class Percussion : ISpell
     {
         AreaOfEffectEvents.OnEnter eventData = new();
         eventData.Entering.SpeakString("You hear a drumming sound.");
-
         return ScriptHandleResult.Handled;
     }
 
     private ScriptHandleResult HeartbeatPercussion(CallInfo arg)
     {
         AreaOfEffectEvents.OnHeartbeat eventData = new();
-
-
         if (eventData.Effect.Creator is NwCreature creator)
         {
             foreach (NwCreature nwCreature in eventData.Effect.GetNearestObjectsByType<NwCreature>()
@@ -100,7 +90,6 @@ public class Percussion : ISpell
     {
         AreaOfEffectEvents.OnExit eventData = new();
         eventData.Exiting.SpeakString("The drumming sound fades away.");
-
         return ScriptHandleResult.Handled;
     }
 
