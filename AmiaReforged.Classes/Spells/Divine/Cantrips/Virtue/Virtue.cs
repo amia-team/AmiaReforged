746b4ca070ffeffa51e2b3d7821d5c13d0ec7969
Caelan L. Talmadge <talmadge12@gmail.com>
2025-03-04 10:54:19 -0600

    First pass at new virtue

---
+++ AmiaReforged.Classes/Spells/Divine/Cantrips/Virtue/Virtue.cs
@@ -0,0 +1,48 @@
+using Anvil.API;
+using Anvil.API.Events;
+using NWN.Core;
+
+namespace AmiaReforged.Classes.Spells.Divine.Cantrips.Virtue;
+
+public class Virtue : ISpell
+{
+    public ResistSpellResult Result { get; set; }
+    public string ImpactScript => "NW_S0_Virtue";
+
+    public void DoSpellResist(NwCreature creature, NwCreature caster)
+    {
+        Result = creature.CheckResistSpell(caster);
+    }
+
+    public void OnSpellImpact(SpellEvents.OnSpellCast eventData)
+    {
+        NwGameObject? caster = eventData.Caster;
+        if (caster == null) return;
+
+        NwGameObject? target = eventData.TargetObject;
+        if (target == null) return;
+
+        if (caster is not NwCreature casterCreature) return;
+
+        bool hasFocus = casterCreature.Feats.Any(f => f.Id == (ushort)Feat.SpellFocusTransmutation);
+        bool hasGreaterFocus = casterCreature.Feats.Any(f => f.Id == (ushort)Feat.GreaterSpellFocusTransmutation);
+        bool hasEpicFocus = casterCreature.Feats.Any(f => f.Id == (ushort)Feat.EpicSpellFocusTransmutation);
+
+        int bonusDice = hasFocus ? 1 : hasGreaterFocus ? 2 : hasEpicFocus ? 3 : 0;
+        int numDice = caster.CasterLevel / 2 + bonusDice;
+
+        int tempHp = hasEpicFocus ? NWScript.d3(numDice) : NWScript.d2(numDice);
+
+        Effect hpEffect = Effect.TemporaryHitpoints(tempHp);
+        hpEffect.Tag = "Virtue";
+
+        if (target.ActiveEffects.Any(e => e.Tag == "Virtue")) return;
+
+        target.ApplyEffect(EffectDuration.Temporary, hpEffect, TimeSpan.FromMinutes(caster.CasterLevel));
+    }
+
+    public void SetResult(ResistSpellResult result)
+    {
+        Result = result;
+    }
+}
