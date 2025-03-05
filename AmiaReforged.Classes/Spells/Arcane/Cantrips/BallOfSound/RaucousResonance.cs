4fe811784164eb246f8c66ff520c00c4fb55d115
Caelan L. Talmadge <talmadge12@gmail.com>
2025-03-05 13:36:21 -0600

    Flesh out Raucous Resonance

---
+++ AmiaReforged.Classes/Spells/Arcane/Cantrips/BallOfSound/RaucousResonance.cs
@@ -0,0 +1,47 @@
+using Anvil.API;
+using Anvil.API.Events;
+
+namespace AmiaReforged.Classes.Spells.Arcane.Cantrips.BallOfSound;
+
+[DecoratesSpell(typeof(BallOfSound))]
+public class RaucousResonance : SpellDecorator
+{
+    private const double TwoRounds = 12;
+
+    public RaucousResonance(ISpell spell) : base(spell)
+    {
+        Spell = spell;
+    }
+
+    public override void OnSpellImpact(SpellEvents.OnSpellCast eventData)
+    {
+        if (eventData.Caster == null) return;
+        if (eventData.TargetObject == null) return;
+
+        if (eventData.Caster is NwCreature casterCreature)
+        {
+            bool isSpecialized = casterCreature.GetSpecialization(NwClass.FromClassType(ClassType.Wizard)) ==
+                                 SpellSchool.Transmutation;
+
+            if (isSpecialized)
+            {
+                Effect sonicVulnerability = Effect.DamageImmunityDecrease(DamageType.Sonic, 10);
+                sonicVulnerability.Tag = "RaucousResonance";
+
+                Effect? existingEffect =
+                    eventData.TargetObject.ActiveEffects.FirstOrDefault(e => e.Tag == "RaucousResonance");
+
+                if (existingEffect != null)
+                {
+                    eventData.TargetObject.RemoveEffect(existingEffect);
+                }
+
+
+                eventData.TargetObject.ApplyEffect(EffectDuration.Temporary, sonicVulnerability,
+                    TimeSpan.FromSeconds(TwoRounds));
+            }
+        }
+
+        Spell.OnSpellImpact(eventData);
+    }
+}
