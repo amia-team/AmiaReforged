0ba94b1ef59fd072a0e78a13e87d8de2b74e4bc2
Caelan L. Talmadge <talmadge12@gmail.com>
2025-03-05 14:03:37 -0600

    First pass at Daze

---
+++ AmiaReforged.Classes/Spells/Arcane/Cantrips/Daze/Daze.cs
@@ -0,0 +1,61 @@
+using Anvil.API;
+using Anvil.API.Events;
+using Anvil.Services;
+
+namespace AmiaReforged.Classes.Spells.Arcane.Cantrips.Daze;
+
+[ServiceBinding(typeof(ISpell))]
+public class Daze : ISpell
+{
+    private const double TwoRounds = 12;
+    public ResistSpellResult Result { get; set; }
+    public string ImpactScript => "NW_S0_Daze";
+
+    public void DoSpellResist(NwCreature creature, NwCreature caster)
+    {
+        Result = creature.CheckResistSpell(caster);
+    }
+
+    public void OnSpellImpact(SpellEvents.OnSpellCast eventData)
+    {
+        if (eventData.Caster == null) return;
+        if (eventData.Caster is not NwCreature casterCreature) return;
+        if (eventData.TargetObject == null) return;
+
+        if (Result != ResistSpellResult.Failed) return;
+
+        Ability primaryAbility = Ability.Charisma;
+        if (eventData.SpellCastClass != null) primaryAbility = eventData.SpellCastClass.PrimaryAbility;
+
+        SavingThrowResult result = SavingThrowResult.Failure;
+
+
+        if (eventData.TargetObject is NwCreature targetCreature)
+        {
+            int dc = 10 + eventData.SpellLevel + casterCreature.GetAbilityModifier(primaryAbility);
+
+            result = targetCreature.RollSavingThrow(SavingThrow.Will, dc, SavingThrowType.MindSpells);
+        }
+
+        if (result != SavingThrowResult.Failure) return;
+
+        ApplyEffect(eventData);
+    }
+
+    private void ApplyEffect(SpellEvents.OnSpellCast eventData)
+    {
+        Effect? existingDaze =
+            eventData.TargetObject!.ActiveEffects.SingleOrDefault(e => e.EffectType == EffectType.Dazed);
+
+        if (existingDaze != null) return;
+
+        Effect dazeEffect = Effect.Dazed();
+
+        eventData.TargetObject.ApplyEffect(EffectDuration.Temporary, dazeEffect, TimeSpan.FromSeconds(TwoRounds));
+    }
+
+    public void SetSpellResistResult(ResistSpellResult result)
+    {
+        Result = result;
+    }
+}
