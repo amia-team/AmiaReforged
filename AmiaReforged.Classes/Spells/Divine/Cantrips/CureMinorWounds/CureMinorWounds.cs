0da224bad91f3d23d18be3984c0aead6b9452241
Caelan L. Talmadge <talmadge12@gmail.com>
2025-03-05 10:21:13 -0600

    Flesh out Cure Minor Wounds

--- AmiaReforged.Classes/Spells/Divine/Cantrips/CureMinorWounds/CureMinorWounds.cs
+++ AmiaReforged.Classes/Spells/Divine/Cantrips/CureMinorWounds/CureMinorWounds.cs
@@ -2,6 +2,7 @@ using Anvil.API;
 using Anvil.API.Events;
 using Anvil.Services;
 using NUnit.Framework;
+using NWN.Core;
 
 namespace AmiaReforged.Classes.Spells.Divine.Cantrips.CureMinorWounds;
 
@@ -10,6 +11,7 @@ public class CureMinorWounds : ISpell
 {
     public ResistSpellResult Result { get; set; }
     public string ImpactScript => "NW_S0_CurMinW";
+    
     public void DoSpellResist(NwCreature creature, NwCreature caster)
     {
         Result = creature.CheckResistSpell(caster);
@@ -17,14 +19,49 @@ public class CureMinorWounds : ISpell
 
     public void OnSpellImpact(SpellEvents.OnSpellCast eventData)
     {
-        if(eventData.Caster == null) return;
-        if(eventData.Caster is not NwCreature casterCreature) return;
+        if (eventData.Caster == null) return;
+        if (eventData.Caster is not NwCreature casterCreature) return;
+
+        if (eventData.TargetObject == null) return;
+
+        bool skipTouchAttack = NWScript.GetRacialType(eventData.TargetObject) != NWScript.RACIAL_TYPE_UNDEAD;
+        Task<TouchAttackResult> result = casterCreature.TouchAttackRanged(eventData.TargetObject, !skipTouchAttack);
+
+        if (result.Result != TouchAttackResult.Hit || !skipTouchAttack) return;
+
+
+        int healAmount = CalculateHealAmount(casterCreature);
         
-        if(eventData.TargetObject == null) return;
+        if (Result != ResistSpellResult.Failed || !skipTouchAttack) return;
         
-        Task<TouchAttackResult> result = casterCreature.TouchAttackRanged(eventData.TargetObject, true);
+        ApplyEffect(eventData, healAmount);
+    }
+
+    private int CalculateHealAmount(NwCreature casterCreature)
+    {
+        bool hasFocus = casterCreature.Feats.Any(f => f.Id == (ushort)Feat.SpellFocusNecromancy);
+        bool hasGreaterFocus = casterCreature.Feats.Any(f => f.Id == (ushort)Feat.GreaterSpellFocusNecromancy);
+        bool hasEpicFocus = casterCreature.Feats.Any(f => f.Id == (ushort)Feat.EpicSpellFocusNecromancy);
+
+        int bonusHeal = hasFocus ? 1 : hasGreaterFocus ? 2 : hasEpicFocus ? 3 : 0;
+
+        int numDie = casterCreature.CasterLevel / 2 + bonusHeal;
+
+        return NWScript.d2(numDie);
+    }
+
+    private void ApplyEffect(SpellEvents.OnSpellCast eventData, int healAmount)
+    {
+        Effect effectBasedOnType = NWScript.GetRacialType(eventData.TargetObject) == NWScript.RACIAL_TYPE_UNDEAD
+            ? Effect.Damage(healAmount, DamageType.Negative)
+            : Effect.Heal(healAmount);
         
+        Effect vfx = NWScript.GetRacialType(eventData.TargetObject) == NWScript.RACIAL_TYPE_UNDEAD
+            ? Effect.VisualEffect(VfxType.ComHitDivine)
+            : Effect.VisualEffect(VfxType.ImpHealingS);
         
+        eventData.TargetObject!.ApplyEffect(EffectDuration.Instant, effectBasedOnType);
+        eventData.TargetObject!.ApplyEffect(EffectDuration.Instant, vfx);
     }
 
     public void SetSpellResistResult(ResistSpellResult result)
--- AmiaReforged.Classes/Spells/Divine/Cantrips/InflictMinorWounds/InflictMinorWounds.cs
+++ AmiaReforged.Classes/Spells/Divine/Cantrips/InflictMinorWounds/InflictMinorWounds.cs
@@ -11,10 +11,6 @@ public class InflictMinorWounds : ISpell
     public ResistSpellResult Result { get; set; }
     public string ImpactScript => "X0_S0_Inflict";
 
-    private bool _hasFocus;
-    private bool _hasGreaterFocus;
-    private bool _hasEpicFocus;
-
     public void DoSpellResist(NwCreature creature, NwCreature caster)
     {
         Result = creature.CheckResistSpell(caster);
@@ -32,10 +28,6 @@ public class InflictMinorWounds : ISpell
 
         if (result.Result != TouchAttackResult.Hit || !skipTouchAttack) return;
 
-        _hasFocus = casterCreature.Feats.Any(f => f.Id == (ushort)Feat.SpellFocusNecromancy);
-        _hasGreaterFocus = casterCreature.Feats.Any(f => f.Id == (ushort)Feat.GreaterSpellFocusNecromancy);
-        _hasEpicFocus = casterCreature.Feats.Any(f => f.Id == (ushort)Feat.EpicSpellFocusNecromancy);
-
         int damage = CalculateDamage(casterCreature);
 
         if (Result != ResistSpellResult.Failed || !skipTouchAttack) return;
@@ -43,27 +35,31 @@ public class InflictMinorWounds : ISpell
         ApplyEffect(eventData, damage);
     }
 
+    private int CalculateDamage(NwCreature casterCreature)
+    {
+        bool hasFocus = casterCreature.Feats.Any(f => f.Id == (ushort)Feat.SpellFocusNecromancy);
+        bool hasGreaterFocus = casterCreature.Feats.Any(f => f.Id == (ushort)Feat.GreaterSpellFocusNecromancy);
+        bool hasEpicFocus = casterCreature.Feats.Any(f => f.Id == (ushort)Feat.EpicSpellFocusNecromancy);
+
+        int bonusDie = hasFocus ? 1 : hasGreaterFocus ? 2 : hasEpicFocus ? 3 : 0;
+
+        int numDie = casterCreature.CasterLevel / 2 + bonusDie;
+
+        return NWScript.d3(numDie);
+    }
+
     private static void ApplyEffect(SpellEvents.OnSpellCast eventData, int damage)
     {
         Effect effectBasedOnType = NWScript.GetRacialType(eventData.TargetObject) == NWScript.RACIAL_TYPE_UNDEAD
             ? Effect.Heal(damage)
             : Effect.Damage(damage, DamageType.Negative);
-     
+
         Effect vfx = NWScript.GetRacialType(eventData.TargetObject) == NWScript.RACIAL_TYPE_UNDEAD
             ? Effect.VisualEffect(VfxType.ImpHealingS)
             : Effect.VisualEffect(VfxType.ImpNegativeEnergy);
-        
+
         eventData.TargetObject!.ApplyEffect(EffectDuration.Instant, effectBasedOnType);
         eventData.TargetObject!.ApplyEffect(EffectDuration.Instant, vfx);
-    }
-
-    private int CalculateDamage(NwCreature casterCreature)
-    {
-        int bonusDie = _hasFocus ? 1 : _hasGreaterFocus ? 2 : _hasEpicFocus ? 3 : 0;
-
-        int numDie = casterCreature.CasterLevel / 2 + bonusDie;
-
-        return NWScript.d3(numDie);
     }
 
     public void SetSpellResistResult(ResistSpellResult result)
