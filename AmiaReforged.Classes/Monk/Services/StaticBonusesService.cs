f56dc3e95a98041aadd7c07306407e8bee8548ad
Caelan L. Talmadge <talmadge12@gmail.com>
2025-03-05 21:52:10 -0600

    Try to await (and guard) the ApplyStaticBonuses method

--- AmiaReforged.Classes/Monk/Services/StaticBonusesService.cs
+++ AmiaReforged.Classes/Monk/Services/StaticBonusesService.cs
@@ -122,25 +122,34 @@ public class StaticBonusesService
         monk.ApplyEffect(EffectDuration.Permanent, monkEffects);
     }
 
-    private static void OnWisdomApplyCheckBonuses(OnEffectApply eventData)
+    private static async void OnWisdomApplyCheckBonuses(OnEffectApply eventData)
     {
-        if (eventData.Object is not NwCreature monk) return;
-        if (monk.GetClassInfo(ClassType.Monk)?.Level < StaticBonusLevel) return;
-        if (eventData.Effect.IntParams[0] is not (int)Ability.Wisdom) return;
-
-        Effect? monkEffects = monk.ActiveEffects.FirstOrDefault(effect => effect.Tag == "monk_staticbonuses");
-
-        if (monkEffects is not null) monk.RemoveEffect(monkEffects);
-
-        ApplyStaticBonuses();
-
-        return;
-
-        async void ApplyStaticBonuses()
+        try
         {
-            await NwTask.Delay(TimeSpan.FromMilliseconds(1));
-            monkEffects = StaticBonuses.GetEffect(monk);
-            monk.ApplyEffect(EffectDuration.Permanent, monkEffects);
+            if (eventData.Object is not NwCreature monk) return;
+            if (monk.GetClassInfo(ClassType.Monk)?.Level < StaticBonusLevel) return;
+            if (eventData.Effect.IntParams[0] is not (int)Ability.Wisdom) return;
+
+            Effect? monkEffects = monk.ActiveEffects.FirstOrDefault(effect => effect.Tag == "monk_staticbonuses");
+
+            if (monkEffects is not null) monk.RemoveEffect(monkEffects);
+
+            await ApplyStaticBonuses();
+
+            await NwTask.SwitchToMainThread();
+
+            return;
+
+            async Task ApplyStaticBonuses()
+            {
+                await NwTask.Delay(TimeSpan.FromMilliseconds(1));
+                monkEffects = StaticBonuses.GetEffect(monk);
+                monk.ApplyEffect(EffectDuration.Permanent, monkEffects);
+            }
+        }
+        catch (Exception e)
+        {
+            Log.Error(e.Message);
         }
     }
 
