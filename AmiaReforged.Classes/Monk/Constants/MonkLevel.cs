f9e4503ffefbfefb1bc9c740b65503fe53b1ab86
Maverick00053 <38230616+Maverick00053@users.noreply.github.com>
2025-03-05 20:28:02 -0800

    Add in Spirit/Body point constants

--- AmiaReforged.Classes/Monk/Constants/MonkLevel.cs
+++ AmiaReforged.Classes/Monk/Constants/MonkLevel.cs
@@ -6,4 +6,14 @@ public class MonkLevel
     public const int KiFocusI = 18;
     public const int KiFocusIi = 24;
     public const int KiFocusIii = 30;
+    public const int BodyKiPointsI = 7;
+    public const int BodyKiPointsIi = 11;
+    public const int BodyKiPointsIii = 15;
+    public const int BodyKiPointsIv = 19;
+    public const int BodyKiPointsV = 24;
+    public const int BodyKiPointsVi = 27;
+    public const int SpiritKiPointsI = 17;
+    public const int SpiritPointsIi = 22;
+    public const int SpiritPointsIii = 27;
+    
 }
