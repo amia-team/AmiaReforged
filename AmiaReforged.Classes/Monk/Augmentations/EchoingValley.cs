4ae123b9f33d6b0c10aa60042088c448948c2716
Mitro Hyvärinen <mitro.hyvarinen@gmail.com>
2025-03-10 23:34:36 +0200

    Start AugmentStunning

--- AmiaReforged.Classes/Monk/Augmentations/EchoingValley.cs
+++ AmiaReforged.Classes/Monk/Augmentations/EchoingValley.cs
@@ -1,6 +1,9 @@
+using AmiaReforged.Classes.EffectUtils;
+using AmiaReforged.Classes.Monk.Constants;
 using AmiaReforged.Classes.Monk.Techniques.Body;
 using AmiaReforged.Classes.Monk.Techniques.Martial;
 using AmiaReforged.Classes.Monk.Types;
+using Anvil.API;
 using Anvil.API.Events;
 
 namespace AmiaReforged.Classes.Monk.Augmentations;
@@ -11,9 +14,6 @@ public static class EchoingValley
     {
         switch (technique)
         {
-            case TechniqueType.Axiomatic:
-                AugmentAxiomatic(attackData);
-                break;
             case TechniqueType.Stunning:
                 AugmentStunning(attackData);
                 break;
@@ -29,6 +29,9 @@ public static class EchoingValley
             case TechniqueType.Eagle:
                 EagleStrike.DoEagleStrike(attackData);
                 break;
+            case TechniqueType.Axiomatic:
+                AxiomaticStrike.DoAxiomaticStrike(attackData);
+                break;
             case TechniqueType.Wholeness:
                 WholenessOfBody.DoWholenessOfBody(castData);
                 break;
@@ -37,20 +40,59 @@ public static class EchoingValley
                 break;
         }
     }
-
-    private static void AugmentAxiomatic(OnCreatureAttack attackData)
-    {
-    }
-
+    
+    /// <summary>
+    /// Stunning Strike summons an Echo to empower Stunning Strike with +1d4 bonus magical damage.
+    /// Echoes last for two turns. Each Ki Focus allows an additional Echo to be summoned for an additional
+    /// 1d4 bonus magical damage to a maximum of 4d4.
+    /// </summary>
     private static void AugmentStunning(OnCreatureAttack attackData)
     {
-    }
+        StunningStrike.DoStunningStrike(attackData);
+        
+        if (attackData.Target is not NwCreature targetCreature) return;
+        
+        NwCreature monk = attackData.Attacker;
+        int monkLevel = monk.GetClassInfo(ClassType.Monk)!.Level;
+        int echoCap = monkLevel switch
+        {
+            >= MonkLevel.KiFocusI and < MonkLevel.KiFocusIi => 2,
+            >= MonkLevel.KiFocusIi and < MonkLevel.KiFocusIii => 3,
+            MonkLevel.KiFocusIii => 4,
+            _ => 1
+        };
 
-    private static void AugmentKiShout(OnSpellCast castData)
+        async void SummonEcho()
+        {
+            // Check how many echoes monk has
+            int echoCount = monk.Associates.Count(associate => 
+                associate is { AssociateType: AssociateType.Summoned, Tag: "stunningstrike_echoingvalley" });
+        
+            // Return if capped out
+            if (echoCount <= echoCap) return;
+        
+            // Create the new echo and attach it as a summon
+            Location? summonLocation = SummonUtility.GetRandomLocationAroundPoint(monk.Location!, 4f);
+            Effect summonEcho = Effect.SummonCreature("echo", VfxType.FnfPwstun);
+            TimeSpan summonDuration = NwTimeSpan.FromTurns(2);
+
+            await monk.WaitForObjectContext();
+            summonLocation?.ApplyEffect(EffectDuration.Temporary,summonEcho, summonDuration);
+
+            /*newEcho.SetIsDestroyable(false, false);
+            newEcho.ApplyEffect(EffectDuration.Permanent, Effect.VisualEffect(VfxType.DurCutsceneInvisibility));
+            newEcho.Location!.ApplyEffect(EffectDuration.Instant, Effect.VisualEffect(VfxType.FnfPwstun));
+        
+            monk.ControllingPlayer.AddToParty();*/
+        }
+        
+    }
+    
+    private static void AugmentEmptyBody(OnSpellCast castData)
     {
     }
-
-    private static void AugmentEmptyBody(OnSpellCast castData)
+    
+    private static void AugmentKiShout(OnSpellCast castData)
     {
     }
 
