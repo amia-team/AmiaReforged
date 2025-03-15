4c3df2eae7665658e571bb9533f75fb0023330c6
Mitro Hyvärinen <mitro.hyvarinen@gmail.com>
2025-03-15 21:23:28 +0200

    Do AugmentStunningStrike

--- AmiaReforged.Classes/Monk/Augmentations/CrystalTides.cs
+++ AmiaReforged.Classes/Monk/Augmentations/CrystalTides.cs
@@ -1,6 +1,7 @@
 using AmiaReforged.Classes.Monk.Techniques.Body;
 using AmiaReforged.Classes.Monk.Techniques.Martial;
 using AmiaReforged.Classes.Monk.Types;
+using Anvil.API;
 using Anvil.API.Events;
 
 namespace AmiaReforged.Classes.Monk.Augmentations;
@@ -37,9 +38,42 @@ public static class CrystalTides
                 break;
         }
     }
-
+    
+    /// <summary>
+    /// Wisdom modifier applies to attacks rolls instead of strength or dexterity. Ki Focus II allows the use of
+    /// Martial Techniques with ranged weapons.
+    /// </summary>
     private static void AugmentStunningStrike(OnCreatureAttack attackData)
     {
+        SavingThrowResult stunningStrikeResult = StunningStrike.DoStunningStrike(attackData);
+
+        if (attackData.Target is not NwCreature targetCreature) return;
+
+        if (stunningStrikeResult != SavingThrowResult.Immune) return;
+
+        NwCreature monk = attackData.Attacker;
+
+        Effect? stunningEffect = null, visualEffect = null;
+        TimeSpan effectDuration = NwTimeSpan.FromRounds(1);
+        
+        switch (MonkUtilFunctions.GetKiFocus(monk))
+        {
+            case KiFocus.KiFocus1 or KiFocus.KiFocus2:
+                stunningEffect = Effect.Pacified();
+                visualEffect = Effect.VisualEffect(VfxType.FnfHowlOdd, false, 0.06f);
+                break;
+            case KiFocus.KiFocus3:
+                stunningEffect = Effect.Paralyze();
+                visualEffect = Effect.VisualEffect(VfxType.DurParalyzeHold);
+                break;
+        }
+
+        if (stunningEffect is null || visualEffect is null) return;
+        
+        stunningEffect.IgnoreImmunity = true;
+        
+        targetCreature.ApplyEffect(EffectDuration.Temporary, stunningEffect, effectDuration);
+        targetCreature.ApplyEffect(EffectDuration.Instant, visualEffect);
     }
 
     private static void AugmentAxiomaticStrike(OnCreatureAttack attackData)
