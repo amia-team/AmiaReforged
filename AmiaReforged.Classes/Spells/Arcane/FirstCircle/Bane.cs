1d8f02f5fa5ffb4de802594a75d8959bea474a96
Mitro Hyvärinen <mitro.hyvarinen@gmail.com>
2025-03-18 00:17:59 +0200

    First pass at Bane

---
+++ AmiaReforged.Classes/Spells/Arcane/FirstCircle/Bane.cs
@@ -0,0 +1,86 @@
+﻿using Anvil.API;
+using Anvil.API.Events;
+using Anvil.Services;
+
+namespace AmiaReforged.Classes.Spells.Arcane.FirstCircle;
+
+/// <summary>
+/// Bane fills the caster's enemies with fear and doubt. They suffer a -1 penalty on their attack rolls
+/// and a -1 penalty on saving throws against fear. Epic Spell Focus makes this saveless.
+/// </summary>
+[ServiceBinding(typeof(ISpell))]
+public class FireBolt : ISpell
+{
+    public bool CheckedSpellResistance { get; set; }
+    public bool ResistedSpell { get; set; }
+
+    public string ImpactScript => "X0_S0_Bane";
+
+    public void OnSpellImpact(SpellEvents.OnSpellCast eventData)
+    {
+        if (eventData.Caster is not NwCreature casterCreature) return;
+
+        NwGameObject? target = eventData.TargetObject;
+        if (target == null) return;
+        
+        Location? location = eventData.TargetLocation;
+        if (location == null) return;
+
+        NwClass? spellClass = eventData.SpellCastClass;
+        if (spellClass == null) return;
+
+        int casterLevel = casterCreature.GetClassInfo(spellClass)!.Level;
+        int spellDc = eventData.SaveDC;
+        TimeSpan spellDuration = NwTimeSpan.FromRounds(casterLevel);
+        
+        DoBane();
+        return;
+
+
+        void DoBane()
+        {
+            bool hasEpicFocus = casterCreature.Feats.Any(f => f.FeatType == Feat.EpicSpellFocusEnchantment);
+        
+            location.ApplyEffect(EffectDuration.Instant, Effect.VisualEffect(VfxType.FnfLosEvil30));
+        
+            foreach (NwGameObject nwObject in location.GetObjectsInShape(Shape.Sphere, RadiusSize.Colossal, false))
+            {
+                if (nwObject is not NwCreature targetCreature || !casterCreature.IsReactionTypeHostile(targetCreature)) continue;
+            
+                if (ResistedSpell) continue;
+            
+                if (hasEpicFocus)
+                {
+                    ApplyEffect(targetCreature, spellDuration);
+                    continue;
+                }
+
+                SavingThrowResult savingThrowResult = 
+                    targetCreature.RollSavingThrow(SavingThrow.Will, spellDc, SavingThrowType.MindSpells);
+
+                if (savingThrowResult is SavingThrowResult.Success)
+                {
+                    targetCreature.ApplyEffect(EffectDuration.Instant, Effect.VisualEffect(VfxType.ImpWillSavingThrowUse));
+                    continue;
+                }
+            
+                ApplyEffect(targetCreature, spellDuration);
+            }
+        }
+    }
+
+    public void SetSpellResisted(bool result)
+    {
+        ResistedSpell = result;
+    }
+
+    private static void ApplyEffect(NwCreature targetCreature, TimeSpan spellDuration)
+    {
+        Effect baneEffect = Effect.LinkEffects(Effect.SavingThrowDecrease(SavingThrow.Will, 1, SavingThrowType.Fear),
+            Effect.AttackDecrease(1), Effect.VisualEffect(VfxType.DurCessateNegative));
+        Effect baneVfx = Effect.VisualEffect(VfxType.ImpHeadEvil);
+        
+        targetCreature.ApplyEffect(EffectDuration.Instant, baneVfx);
+        targetCreature.ApplyEffect(EffectDuration.Temporary, baneEffect, spellDuration);
+    }
+}
