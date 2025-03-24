dcd9ea4a92cab9e61c94ff9863a43ec1fab581a9
Mitro Hyvärinen <mitro.hyvarinen@gmail.com>
2025-03-24 02:48:16 +0200

    First pass at Ghoul Touch

---
+++ AmiaReforged.Classes/Spells/Arcane/SecondCircle/Necromancy/GhoulTouch.cs
@@ -0,0 +1,58 @@
+﻿using Anvil.API;
+using Anvil.API.Events;
+using Anvil.Services;
+
+namespace AmiaReforged.Classes.Spells.Arcane.SecondCircle.Necromancy;
+
+[ServiceBinding(typeof(ISpell))]
+public class GhoulTouch : ISpell
+{
+    public bool CheckedSpellResistance { get; set; }
+    public bool ResistedSpell { get; set; }
+    public string ImpactScript => "NW_S0_GhoulTch";
+    
+
+    public void OnSpellImpact(SpellEvents.OnSpellCast eventData)
+    {
+        if (eventData.Caster == null) return;
+        if (eventData.Caster is not NwCreature casterCreature) return;
+        if (eventData.TargetObject is null) return;
+        
+        SpellUtils.SignalSpell(casterCreature, eventData.TargetObject, eventData.Spell);
+        
+        if (ResistedSpell) return;
+
+        ApplyEffect(eventData);
+    }
+
+    public void SetSpellResisted(bool result)
+    {
+        ResistedSpell = result;
+    }
+    
+    private static void ApplyEffect(SpellEvents.OnSpellCast eventData)
+    {
+        Effect fogGhoul = Effect.AreaOfEffect(PersistentVfxType.PerFogghoul!);
+        
+        Effect ghoulTouchEffect = Effect.LinkEffects(Effect.Paralyze(), 
+            Effect.VisualEffect(VfxType.DurCessateNegative), Effect.VisualEffect(VfxType.DurParalyzed));
+        
+        TimeSpan effectDuration = NwTimeSpan.FromRounds(Random.Shared.Roll(6) + 2);
+        effectDuration = SpellUtils.CheckExtend(eventData.MetaMagicFeat, effectDuration);
+
+        int dc = SpellUtils.GetSpellDc(eventData);
+        
+        NwCreature targetCreature = (NwCreature)eventData.TargetObject!;
+
+        SavingThrowResult savingThrowResult =
+            targetCreature.RollSavingThrow(SavingThrow.Fortitude, dc, SavingThrowType.Negative, eventData.Caster);
+        
+        if (savingThrowResult == SavingThrowResult.Success)
+            targetCreature.ApplyEffect(EffectDuration.Instant, Effect.VisualEffect(VfxType.ImpFortitudeSavingThrowUse));
+
+        if (savingThrowResult != SavingThrowResult.Failure) return;
+        
+        targetCreature.ApplyEffect(EffectDuration.Temporary, ghoulTouchEffect, effectDuration);
+        targetCreature.Location!.ApplyEffect(EffectDuration.Temporary, fogGhoul, effectDuration);
+    }
+}
---
+++ AmiaReforged.Classes/Spells/Arcane/SecondCircle/Necromancy/GhoulTouchOnEnter.cs
@@ -0,0 +1,53 @@
+﻿using Anvil.API;
+using Anvil.API.Events;
+using Anvil.Services;
+
+namespace AmiaReforged.Classes.Spells.Arcane.SecondCircle.Necromancy;
+
+public class GhoulTouchOnEnter
+{
+    [ScriptHandler("NW_S0_GhoulTchA")]
+    public void OnEnter(AreaOfEffectEvents.OnEnter eventData)
+    {
+        if (eventData.Effect.Creator is not NwCreature caster) return;
+        if (eventData.Entering is not NwCreature creature) return;
+
+        bool resistedSpell = creature.SpellAbsorptionLimitedCheck(caster) 
+                             || creature.SpellAbsorptionUnlimitedCheck(caster)
+                             || creature.SpellImmunityCheck(caster) 
+                             || creature.SpellAbsorptionUnlimitedCheck(caster)
+                             || creature.SpellResistanceCheck(caster);
+        
+        if (resistedSpell) return;
+        
+        ApplyEffect(eventData);
+    }
+    
+    private static void ApplyEffect(AreaOfEffectEvents.OnEnter eventData)
+    {
+        NwCreature caster = (NwCreature)eventData.Effect.Creator!;
+        NwCreature enteringCreature = (NwCreature)eventData.Entering;
+        
+        if (caster.IsReactionTypeFriendly(enteringCreature)) return;
+        
+        Effect ghoulVfx = Effect.VisualEffect(VfxType.ImpDoom);
+        Effect ghoulEffect = Effect.LinkEffects(Effect.AttackDecrease(2), 
+            Effect.DamageDecrease(2, DamageType.BaseWeapon), 
+            Effect.SavingThrowDecrease(SavingThrow.All, 2), Effect.SkillDecrease(Skill.AllSkills!, 2));
+        
+        TimeSpan effectDuration = NwTimeSpan.FromRounds(Random.Shared.Roll(6) + 2);
+
+        int dc = SpellUtils.GetAoeSpellDc(eventData);
+        
+        SavingThrowResult savingThrowResult =
+            enteringCreature.RollSavingThrow(SavingThrow.Fortitude, dc, SavingThrowType.Negative, caster);
+        
+        if (savingThrowResult == SavingThrowResult.Success)
+            enteringCreature.ApplyEffect(EffectDuration.Instant, Effect.VisualEffect(VfxType.ImpFortitudeSavingThrowUse));
+
+        if (savingThrowResult != SavingThrowResult.Failure) return;
+        
+        enteringCreature.ApplyEffect(EffectDuration.Temporary, ghoulEffect, effectDuration);
+        enteringCreature.ApplyEffect(EffectDuration.Instant, ghoulVfx);
+    }
+}
