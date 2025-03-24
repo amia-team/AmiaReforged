28ccbac76306a762d50ed721dd616891e6cb0f4f
Mitro Hyvärinen <mitro.hyvarinen@gmail.com>
2025-03-24 15:14:25 +0200

    Add damage to Quivering

--- AmiaReforged.Classes/Monk/Techniques/Spirit/QuiveringPalm.cs
+++ AmiaReforged.Classes/Monk/Techniques/Spirit/QuiveringPalm.cs
@@ -26,7 +26,7 @@ public static class QuiveringPalm
     
     /// <summary>
     /// On a successful melee touch attack against an enemy creature, the target must make a fortitude save or die.
-    /// Each use depletes a Spirit Ki Point.
+    /// If the target survives, it takes 1d6 bludgeoning damage per monk level.
     /// </summary>
     public static TouchAttackResult DoQuiveringPalm(OnSpellCast castData)
     {
@@ -39,22 +39,33 @@ public static class QuiveringPalm
         Effect quiveringEffect = Effect.Death(true);
         Effect quiveringVfx = Effect.VisualEffect(VfxType.ImpDeath, false, 0.7f);
 
+        
+
         CreatureEvents.OnSpellCastAt.Signal(monk, targetCreature, castData.Spell!);
 
-        if (monk.TouchAttackMelee(targetCreature) is TouchAttackResult.Miss) return TouchAttackResult.Miss;
+        TouchAttackResult touchAttackResult = monk.TouchAttackMelee(targetCreature);
+
+        if (touchAttackResult is TouchAttackResult.Miss) return TouchAttackResult.Miss;
 
         SavingThrowResult savingThrowResult =
             targetCreature.RollSavingThrow(SavingThrow.Fortitude, dc, SavingThrowType.Death, monk);
 
-        if (savingThrowResult is SavingThrowResult.Success)
-        {
-            targetCreature.ApplyEffect(EffectDuration.Instant, Effect.VisualEffect(VfxType.ImpFortitudeSavingThrowUse));
-        }
-        else
+        if (savingThrowResult is SavingThrowResult.Failure)
         {
             targetCreature.ApplyEffect(EffectDuration.Instant, quiveringEffect);
             targetCreature.ApplyEffect(EffectDuration.Instant, quiveringVfx);
         }
+        
+        if (savingThrowResult is SavingThrowResult.Success)
+            targetCreature.ApplyEffect(EffectDuration.Instant, Effect.VisualEffect(VfxType.ImpFortitudeSavingThrowUse));
+        
+        int monkLevel = monk.GetClassInfo(ClassType.Monk)!.Level;
+        int damageAmount = Random.Shared.Roll(6, monkLevel);
+
+        Effect quiveringDamage = Effect.LinkEffects(Effect.Damage(damageAmount, DamageType.Bludgeoning),
+            Effect.VisualEffect(VfxType.ImpDivineStrikeHoly, false, 0.2f));
+        
+        targetCreature.ApplyEffect(EffectDuration.Instant, quiveringDamage);
 
         return TouchAttackResult.Hit;
     }
