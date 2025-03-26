48dd7c981368a72fc22bab9e2da91c5c88ba4c97
Mitro Hyvärinen <mitro.hyvarinen@gmail.com>
2025-03-26 15:23:00 +0200

    Change damage decrease type

--- AmiaReforged.Classes/Spells/Arcane/SecondCircle/Necromancy/GhoulTouch.cs
+++ AmiaReforged.Classes/Spells/Arcane/SecondCircle/Necromancy/GhoulTouch.cs
@@ -102,7 +102,7 @@ public class GhoulTouch : ISpell
         
         Effect ghoulVfx = Effect.VisualEffect(VfxType.ImpDoom);
         Effect ghoulEffect = Effect.LinkEffects(Effect.AttackDecrease(2),
-            Effect.DamageDecrease(2,  DamageType.Slashing),
+            Effect.DamageDecrease(2),
             Effect.SavingThrowDecrease(SavingThrow.All, 2), Effect.SkillDecrease(Skill.AllSkills!, 2));
         
         enteringCreature.SpeakString("Declared effects, still gooey.");
