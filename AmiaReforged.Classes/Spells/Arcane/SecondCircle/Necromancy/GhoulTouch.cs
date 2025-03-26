748d71fb69409badfea35e44f0f4db3af9705666
Mitro Hyvärinen <mitro.hyvarinen@gmail.com>
2025-03-26 15:36:17 +0200

    Remove debug and fix bug

--- AmiaReforged.Classes/Spells/Arcane/SecondCircle/Necromancy/GhoulTouch.cs
+++ AmiaReforged.Classes/Spells/Arcane/SecondCircle/Necromancy/GhoulTouch.cs
@@ -87,48 +87,35 @@ public class GhoulTouch : ISpell
         AreaOfEffectEvents.OnEnter eventData = new();
 
         if (eventData.Entering is not NwCreature enteringCreature) return ScriptHandleResult.Handled;
-        enteringCreature.SpeakString("Teeheee I stepped on your goo");
         if (eventData.Effect.Creator is not NwCreature caster) return ScriptHandleResult.Handled;
-        caster.SpeakString("Oh no I stepped on my own goo!");
         
         if (caster.IsReactionTypeFriendly(enteringCreature)) return ScriptHandleResult.Handled;
-        enteringCreature.SpeakString("Still gooey after friendly check");
         
         if (ResistedSpell) return ScriptHandleResult.Handled;
-        enteringCreature.SpeakString("Yep, gooey after spell resist check");
         
         int dc = GetGhoulDc();
-        enteringCreature.SpeakString($"Gooey after getting goo DC! DC is {dc}");
         
         Effect ghoulVfx = Effect.VisualEffect(VfxType.ImpDoom);
-        Effect ghoulEffect = Effect.LinkEffects(Effect.AttackDecrease(2),
-            Effect.DamageDecrease(2),
-            Effect.SavingThrowDecrease(SavingThrow.All, 2), Effect.SkillDecrease(Skill.AllSkills!, 2));
         
-        enteringCreature.SpeakString("Declared effects, still gooey.");
+        Effect ghoulEffect = 
+            Effect.LinkEffects(Effect.AttackDecrease(2),
+            Effect.DamageDecrease(2),
+            Effect.SavingThrowDecrease(SavingThrow.All, 2), 
+            Effect.SkillDecreaseAll(2));
         
         TimeSpan effectDuration = NwTimeSpan.FromRounds(Random.Shared.Roll(6) + 2);
-        
-        enteringCreature.SpeakString("Duration set, gooing strong.");
         
         SavingThrowResult savingThrowResult =
             enteringCreature.RollSavingThrow(SavingThrow.Fortitude, dc, SavingThrowType.Negative, caster);
         
-        enteringCreature.SpeakString("Rolled fort against your mighty goo.");
-
         if (savingThrowResult == SavingThrowResult.Success)
-        {
             enteringCreature.ApplyEffect(EffectDuration.Instant, Effect.VisualEffect(VfxType.ImpFortitudeSavingThrowUse));
-            enteringCreature.SpeakString("Hah, your goo is weak.");
-        }
 
         if (savingThrowResult != SavingThrowResult.Failure) return ScriptHandleResult.Handled;
         
         enteringCreature.ApplyEffect(EffectDuration.Temporary, ghoulEffect, effectDuration);
         enteringCreature.ApplyEffect(EffectDuration.Instant, ghoulVfx);
         
-        enteringCreature.SpeakString("Your goo is too powerful!!!");
-
         return ScriptHandleResult.Handled;
     }
 }
