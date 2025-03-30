a5c87ea654ec389bad1f0e568bab9aec0a6fd302
Caelan L. Talmadge <talmadge12@gmail.com>
2025-03-30 13:04:11 -0500

    fr fr add test solution

---
+++ AmiaReforged.PwEngineTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace AmiaReforged.PwEngineTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
