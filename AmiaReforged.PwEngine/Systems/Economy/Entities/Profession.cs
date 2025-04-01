0a3d34ae7f7e32e1c2081f37671e4e9e8ff696a3
Caelan L. Talmadge <talmadge12@gmail.com>
2025-03-31 22:38:13 -0500

    Get econ yaml files into a better state

--- AmiaReforged.PwEngine/Systems/Economy/Entities/Profession.cs
+++ AmiaReforged.PwEngine/Systems/Economy/Entities/Profession.cs
@@ -2,5 +2,35 @@ namespace AmiaReforged.PwEngine.Systems.Economy.Entities;
 
 public class Profession
 {
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
     
+    public List<string> InnovationTags { get; set; } = new();
+    public List<Innovation> AllInnovations { get; set; } = new();
+}
+
+public class Innovation
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    
+    public List<string> EffectTags { get; set; } = new();
+    
+    public List<InnovationEffect> Effects { get; set; } = new();
+}
+
+public class InnovationEffect
+{
+    
+    
+}
+
+public enum InnovationModifier
+{
+    Additive,
+    Multiplicative,
+    Subtractive,
+    Divisive,
+    Logarithmic,
+    Exponential
 }
