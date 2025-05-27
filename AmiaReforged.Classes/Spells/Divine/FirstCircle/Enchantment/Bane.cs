557b3e07ac8b3a2d8f23c769c57246c103411183
Caelan L. Talmadge <talmadge12@gmail.com>
2025-05-27 09:40:38 -0500

    Fix formatting in Bane.cs

--- AmiaReforged.Classes/Spells/Divine/FirstCircle/Enchantment/Bane.cs
+++ AmiaReforged.Classes/Spells/Divine/FirstCircle/Enchantment/Bane.cs
@@ -21,51 +21,53 @@ public class FireBolt : ISpell
         NwGameObject? caster = eventData.Caster;
         if (caster == null) return;
         if (caster is not NwCreature casterCreature) return;
-        
+
         Location? location = eventData.TargetLocation;
         if (location == null) return;
 
         NwClass? spellClass = eventData.SpellCastClass;
         if (spellClass == null) return;
-        
+
         int casterLevel = casterCreature.GetClassInfo(spellClass)!.Level;
         int spellDc = SpellUtils.GetSpellDc(eventData);
         TimeSpan spellDuration = NwTimeSpan.FromRounds(casterLevel);
-        
+
         bool hasEpicFocus = casterCreature.Feats.Any(f => f.FeatType == Feat.EpicSpellFocusEnchantment);
-        
+
         DoBane();
         return;
 
 
         void DoBane()
         {
-
             location.ApplyEffect(EffectDuration.Instant, Effect.VisualEffect(VfxType.FnfLosEvil30));
-        
+
             foreach (NwGameObject nwObject in location.GetObjectsInShape(Shape.Sphere, RadiusSize.Colossal, false))
             {
-                if (nwObject is not NwCreature targetCreature || !casterCreature.IsReactionTypeHostile(targetCreature)) continue;
+                if (nwObject is not NwCreature targetCreature ||
+                    !casterCreature.IsReactionTypeHostile(targetCreature)) continue;
 
                 SpellUtils.SignalSpell(caster, nwObject, eventData.Spell);
-            
+
                 if (ResistedSpell) continue;
-            
+
                 if (hasEpicFocus)
                 {
                     ApplyEffect(targetCreature, spellDuration);
                     continue;
                 }
 
-                SavingThrowResult savingThrowResult = 
-                    targetCreature.RollSavingThrow(SavingThrow.Will, spellDc, SavingThrowType.MindSpells, casterCreature);
+                SavingThrowResult savingThrowResult =
+                    targetCreature.RollSavingThrow(SavingThrow.Will, spellDc, SavingThrowType.MindSpells,
+                        casterCreature);
 
                 if (savingThrowResult is SavingThrowResult.Success)
                 {
-                    targetCreature.ApplyEffect(EffectDuration.Instant, Effect.VisualEffect(VfxType.ImpWillSavingThrowUse));
+                    targetCreature.ApplyEffect(EffectDuration.Instant,
+                        Effect.VisualEffect(VfxType.ImpWillSavingThrowUse));
                     continue;
                 }
-            
+
                 ApplyEffect(targetCreature, spellDuration);
             }
         }
@@ -81,7 +83,7 @@ public class FireBolt : ISpell
         Effect baneEffect = Effect.LinkEffects(Effect.SavingThrowDecrease(SavingThrow.Will, 1, SavingThrowType.Fear),
             Effect.AttackDecrease(1), Effect.VisualEffect(VfxType.DurCessateNegative));
         Effect baneVfx = Effect.VisualEffect(VfxType.ImpHeadEvil);
-        
+
         targetCreature.ApplyEffect(EffectDuration.Instant, baneVfx);
         targetCreature.ApplyEffect(EffectDuration.Temporary, baneEffect, spellDuration);
     }
