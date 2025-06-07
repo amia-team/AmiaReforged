f9f7a0711ce73b4bc2c68977190bdb94c017d55b
Caelan L. Talmadge <talmadge12@gmail.com>
2025-06-07 16:11:16 -0500

    Add migration for NPC banks

---
+++ AmiaReforged.Core/Migrations/20250607200741_NpcsAndEncounters.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace AmiaReforged.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class NpcsAndEncounters : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Encounters",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    EncounterSize = table.Column<int>(type: "integer", nullable: false),
+                    DmId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Encounters", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Encounters_Dms_DmId",
+                        column: x => x.DmId,
+                        principalTable: "Dms",
+                        principalColumn: "CdKey",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Npcs",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    DmCdKey = table.Column<string>(type: "text", nullable: false),
+                    Public = table.Column<bool>(type: "boolean", nullable: false),
+                    Serialized = table.Column<byte[]>(type: "bytea", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Npcs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EncounterEntries",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SerializedString = table.Column<byte[]>(type: "bytea", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    EncounterId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EncounterEntries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EncounterEntries_Encounters_EncounterId",
+                        column: x => x.EncounterId,
+                        principalTable: "Encounters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EncounterEntries_EncounterId",
+                table: "EncounterEntries",
+                column: "EncounterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Encounters_DmId",
+                table: "Encounters",
+                column: "DmId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EncounterEntries");
+
+            migrationBuilder.DropTable(
+                name: "Npcs");
+
+            migrationBuilder.DropTable(
+                name: "Encounters");
+        }
+    }
+}
