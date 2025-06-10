dcdaef882a55ed28252409a7be100375ed66c778
Caelan L. Talmadge <talmadge12@gmail.com>
2025-06-10 15:11:43 -0500

    Fix nullable issues in GetVfxCommand.cs, JobItem.cs

--- AmiaReforged.PwEngine/Systems/Chat/Commands/DM/GetVfxCommand.cs
+++ AmiaReforged.PwEngine/Systems/Chat/Commands/DM/GetVfxCommand.cs
@@ -1,6 +1,7 @@
 using Anvil.API;
 using Anvil.API.Events;
 using Anvil.Services;
+using NLog;
 
 namespace AmiaReforged.PwEngine.Systems.Chat.Commands.DM;
 
@@ -20,51 +21,70 @@ public class GetVfx : IChatCommand
 
     private async void GetDurVfx(ModuleEvents.OnPlayerTarget obj)
     {
-        int vfxId;
-        int vfxIndex = 0;
-        string vfxLabel;
-        string vfxList = $"Visual effects on {obj.TargetObject.Name}:\n";
-        if (obj.TargetObject is NwCreature targetCreature)
-            foreach (Effect effect in targetCreature.ActiveEffects)
-            {
-                if (effect.EffectType == EffectType.VisualEffect)
+        try
+        {
+            int vfxId;
+            int vfxIndex = 0;
+            string? vfxLabel;
+            string vfxList = $"Visual effects on {obj.TargetObject?.Name}:\n";
+            if (obj.TargetObject is NwCreature targetCreature)
+                foreach (Effect effect in targetCreature.ActiveEffects)
                 {
-                    vfxId = effect.IntParams[0];
-                    vfxLabel = NwGameTables.VisualEffectTable[vfxId].Label;
-                    vfxIndex++;
-                    vfxList += $"\n{vfxIndex}. ID: {vfxId}. Label: {vfxLabel}\n";
+                    if (effect.EffectType == EffectType.VisualEffect)
+                    {
+                        vfxId = effect.IntParams[0];
+                        vfxLabel = NwGameTables.VisualEffectTable[vfxId].Label;
+                        vfxIndex++;
+                        vfxList += $"\n{vfxIndex}. ID: {vfxId}. Label: {vfxLabel}\n";
+                    }
                 }
-            }
 
-        if (obj.TargetObject is NwDoor targetDoor)
-            foreach (Effect effect in targetDoor.ActiveEffects)
-            {
-                if (effect.EffectType == EffectType.VisualEffect)
+            if (obj.TargetObject is NwDoor targetDoor)
+                foreach (Effect effect in targetDoor.ActiveEffects)
                 {
-                    vfxId = effect.IntParams[0];
-                    vfxLabel = NwGameTables.VisualEffectTable[vfxId].Label;
-                    vfxIndex++;
-                    vfxList += $"\n{vfxIndex}. ID: {vfxId}. Label: {vfxLabel}\n";
+                    if (effect.EffectType == EffectType.VisualEffect)
+                    {
+                        vfxId = effect.IntParams[0];
+                        vfxLabel = NwGameTables.VisualEffectTable[vfxId].Label;
+                        vfxIndex++;
+                        vfxList += $"\n{vfxIndex}. ID: {vfxId}. Label: {vfxLabel}\n";
+                    }
                 }
-            }
 
-        if (obj.TargetObject is NwPlaceable targetPlaceable)
-            foreach (Effect effect in targetPlaceable.ActiveEffects)
-            {
-                if (effect.EffectType == EffectType.VisualEffect)
+            if (obj.TargetObject is NwPlaceable targetPlaceable)
+                foreach (Effect effect in targetPlaceable.ActiveEffects)
                 {
-                    vfxId = effect.IntParams[0];
-                    vfxLabel = NwGameTables.VisualEffectTable[vfxId].Label;
-                    vfxIndex++;
-                    vfxList += $"\n{vfxIndex}. ID: {vfxId}. Label: {vfxLabel}\n";
+                    if (effect.EffectType == EffectType.VisualEffect)
+                    {
+                        vfxId = effect.IntParams[0];
+                        vfxLabel = NwGameTables.VisualEffectTable[vfxId].Label;
+                        vfxIndex++;
+                        vfxList += $"\n{vfxIndex}. ID: {vfxId}. Label: {vfxLabel}\n";
+                    }
                 }
-            }
 
-        NwPlaceable helperObject =
-            NwPlaceable.Create(template: "ds_invis_object3", obj.Player.ControlledCreature.Location);
-        helperObject.Description = vfxList;
-        obj.Player.ActionExamine(helperObject);
-        await NwTask.Delay(TimeSpan.FromSeconds(1));
-        helperObject.Destroy();
+            NwCreature? playerControlledCreature = obj.Player.ControlledCreature;
+            if(playerControlledCreature is null) return;
+            
+            if(playerControlledCreature.Location is null) return;
+            
+            NwPlaceable? helperObject =
+                NwPlaceable.Create(template: "ds_invis_object3", playerControlledCreature.Location);
+            if(helperObject is null) return;
+            
+            helperObject.Description = vfxList;
+            await obj.Player.ActionExamine(helperObject);
+            await NwTask.SwitchToMainThread();
+            
+            await NwTask.Delay(TimeSpan.FromSeconds(1));
+            await NwTask.SwitchToMainThread();
+            
+            helperObject.Destroy();
+
+        }
+        catch (Exception e)
+        {
+            LogManager.GetCurrentClassLogger().Error(e);
+        }
     }
 }
