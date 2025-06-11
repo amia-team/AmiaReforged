1ca48302597a1ff58044912bdc7ec683283f2809
Mitro Hyvärinen <mitro.hyvarinen@gmail.com>
2025-06-11 23:13:11 +0300

    Revert "Make frog death task"
    This reverts commit 5fc0043ecff94a3103f0a0449f4084c5e352e032.

--- AmiaReforged.Classes/Warlock/FrogDeathHandler.cs
+++ AmiaReforged.Classes/Warlock/FrogDeathHandler.cs
@@ -13,13 +13,45 @@ public class FrogDeathHandler
 
     public FrogDeathHandler()
     {
+        NwModule.Instance.OnAssociateAdd += AllowMultipleSummons;
         Log.Info(message: "Frog Death Handler initialized.");
     }
     
-    [ScriptHandler(scriptName: "wlk_frog_ondeath")]
-    public async Task OnFrogDeathRussianDoll(CallInfo callInfo)
+    /// <summary>
+    /// Allows multiple warlock summons
+    /// </summary>
+    private static void AllowMultipleSummons(OnAssociateAdd eventData)
     {
-        if (callInfo.TryGetEvent(out CreatureEvents.OnDeath? obj))
+        if (!eventData.Associate.ResRef.Contains("wlk")) return;
+
+        NwCreature warlock = eventData.Owner;
+        
+        // Hides the stupid "unsummoning creature" message
+        FeedbackPlugin.SetFeedbackMessageHidden(FeedbackPlugin.NWNX_FEEDBACK_ASSOCIATE_UNSUMMONING, 1, warlock);
+        
+        foreach (NwCreature associate in warlock.Associates)
+            if (associate.ResRef.Contains("wlk"))
+                associate.IsDestroyable = false;
+        
+        DelayedMakeDestroyable();
+        return;
+        
+        async void DelayedMakeDestroyable()
+        {
+            await NwTask.Delay(TimeSpan.FromSeconds(6));
+        
+            foreach (NwCreature associate in warlock.Associates)
+                if (associate.ResRef.Contains("wlk"))
+                    associate.IsDestroyable = true;
+            
+            FeedbackPlugin.SetFeedbackMessageHidden(FeedbackPlugin.NWNX_FEEDBACK_ASSOCIATE_UNSUMMONING, 0, warlock);
+        }
+    }
+    
+    [ScriptHandler(scriptName: "wlk_frog_ondeath")]
+    public async void OnFrogDeathRussianDoll(CallInfo callInfo)
+    {
+        if (callInfo.TryGetEvent(out CreatureEvents.OnDeath obj))
         {
             if (obj.KilledCreature.AssociateType != AssociateType.Summoned) return;
             bool isRussianDoll = obj.KilledCreature.ResRef == "wlkslaadblue" ||
@@ -29,7 +61,6 @@ public class FrogDeathHandler
 
             NwCreature summon = obj.KilledCreature;
             NwCreature warlock = summon.Master;
-            if (warlock == null) return;
 
             string slaadTier = summon.ResRef;
 
