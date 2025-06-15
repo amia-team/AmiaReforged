caab18c8596bc4b00f321b8250c2238974bfa611
Mitro Hyvärinen <mitro.hyvarinen@gmail.com>
2025-06-15 07:50:04 +0300

    First crack at two-handed bonus

---
+++ AmiaReforged.Classes/GeneralFeats/TwoHandedBonus.cs
@@ -0,0 +1,74 @@
+﻿using Anvil.API;
+
+namespace AmiaReforged.Classes.GeneralFeats;
+
+public static class TwoHandedBonus
+{
+    private static Effect TwoHandedBonusEffect(NwCreature creature)
+    {
+        // First calculate the base game 50% strength modifier bonus to twohanding
+        // This is important because ints are rounded down, so you'd lose bonus damage
+        // eg str mod 15 would be 7 base bonus + 7 extra bonus, while x2 str mod should add up to 15
+        int strengthModifier = creature.GetAbilityModifier(Ability.Strength);
+        int baseTwoHandedDamageBonus = (int)(strengthModifier * 1.5 - strengthModifier);
+        
+        // Infer Amia-specific extra damage based on the base bonus
+        int extraTwoHandedDamageBonus = strengthModifier - baseTwoHandedDamageBonus;
+
+        Effect twoHandedDamageEffect = Effect.DamageIncrease(extraTwoHandedDamageBonus, DamageType.BaseWeapon);
+
+        const int twoHandedExtraAbBonus = 2;
+
+        Effect twoHandedAbEffect = Effect.AttackIncrease(twoHandedExtraAbBonus);
+        
+        Effect twoHandedBonusEffects = Effect.LinkEffects(twoHandedDamageEffect, twoHandedAbEffect);
+        twoHandedBonusEffects.SubType = EffectSubType.Unyielding;
+        twoHandedBonusEffects.Tag = "twohandedbonus";
+        
+        return twoHandedBonusEffects;
+    }
+
+    public static void ApplyTwoHandedBonusEffect(NwCreature creature)
+    {
+        NwPlayer? player = creature.ControllingPlayer;
+        
+        // Before applying, always remove existing two-handed bonus first
+        Effect? twoHandedBonus = creature.ActiveEffects.FirstOrDefault(effect => effect.Tag == "twohandedbonus");
+        if (twoHandedBonus != null)
+            creature.RemoveEffect(twoHandedBonus);
+        
+        NwItem? rightHandItem = creature.GetItemInSlot(InventorySlot.RightHand);
+
+        if (rightHandItem == null)
+        {
+            if (twoHandedBonus != null)
+                player?.SendServerMessage("Two-handed bonus removed");
+            
+            return;
+        }
+        
+        // We know that in order for the weapon to be twohanded,
+        // the weapon size must always be (one size) larger than the creature
+        int weaponSize = (int)rightHandItem.BaseItem.WeaponSize;
+        int creatureSize = (int)creature.Size;
+        
+        if (weaponSize <= creatureSize)
+        {
+            if (twoHandedBonus != null)
+                player?.SendServerMessage("Two-handed bonus removed");
+
+            return;
+        }
+        
+        Effect twoHandedBonusEffect = TwoHandedBonusEffect(creature);
+        creature.ApplyEffect(EffectDuration.Permanent, twoHandedBonusEffect);
+        
+        // If the two-handed bonus was already there, it means that it was adjusted by levelling or str buffs/debuffs
+        if (twoHandedBonus != null)
+            player?.SendServerMessage("Two-handed bonus adjusted");
+        
+        // Otherwise we know that the bonus wasn't there and has been applied for the first time
+        player?.SendServerMessage("Two-handed bonus applied");
+    }
+    
+}
---
+++ AmiaReforged.Classes/GeneralFeats/TwoHandedBonusHandler.cs
@@ -0,0 +1,64 @@
+﻿using Anvil.API;
+using Anvil.API.Events;
+using Anvil.Services;
+using NLog;
+using NWN.Core.NWNX;
+
+namespace AmiaReforged.Classes.GeneralFeats;
+
+[ServiceBinding(typeof(TwoHandedBonusHandler))]
+public class TwoHandedBonusHandler
+{
+    private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+    public TwoHandedBonusHandler(EventService eventService)
+    {
+        string environment = UtilPlugin.GetEnvironmentVariable(sVarname: "SERVER_MODE");
+
+        if (environment == "live") return;
+
+        /*eventService.SubscribeAll<OnLoadCharacterFinish, OnLoadCharacterFinish.Factory>(OnLoadApplyTwoHanded,
+            EventCallbackType.After);*/
+        eventService.SubscribeAll<OnItemEquip, OnItemEquip.Factory>(OnEquipApplyTwoHanded, EventCallbackType.After);
+        eventService.SubscribeAll<OnItemUnequip, OnItemUnequip.Factory>(OnUnequipApplyTwoHanded, EventCallbackType.After);
+        NwModule.Instance.OnEffectApply += OnStrengthGainApplyTwoHanded;
+        NwModule.Instance.OnEffectRemove += OnStrengthRemoveApplyTwoHanded;
+        Log.Info(message: "Monk Static Bonuses Service initialized.");
+    }
+    
+    private static void OnLoadApplyTwoHanded(OnLoadCharacterFinish eventData)
+    {
+        if (eventData.Player.ControlledCreature is not { } creature) return;
+        
+        TwoHandedBonus.ApplyTwoHandedBonusEffect(creature);
+    }
+
+    private static void OnEquipApplyTwoHanded(OnItemEquip eventData)
+    {
+        TwoHandedBonus.ApplyTwoHandedBonusEffect(eventData.EquippedBy);
+    }
+    
+    private static void OnUnequipApplyTwoHanded(OnItemUnequip eventData)
+    {
+        TwoHandedBonus.ApplyTwoHandedBonusEffect(eventData.Creature);
+    }
+
+    private static void OnStrengthGainApplyTwoHanded(OnEffectApply eventData)
+    {
+        if (eventData.Object is not NwCreature creature) return;
+        if (eventData.Effect.IntParams[0] is not (int)Ability.Strength) return;
+        
+        TwoHandedBonus.ApplyTwoHandedBonusEffect(creature);
+    }
+
+    private static void OnStrengthRemoveApplyTwoHanded(OnEffectRemove eventData)
+    {
+        if (eventData.Object is not NwCreature creature) return;
+        if (eventData.Effect.IntParams[0] is not (int)Ability.Strength) return;
+        
+        TwoHandedBonus.ApplyTwoHandedBonusEffect(creature);
+    }
+    
+    
+    
+}
