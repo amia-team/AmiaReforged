0fcdbaca579d69949a3b448ea37301b1a37c66d3
Mitro Hyvärinen <mitro.hyvarinen@gmail.com>
2025-07-19 01:18:16 +0300

    Try a local func to update item appearance

--- AmiaReforged.Classes/Spells/CraftSpell.cs
+++ AmiaReforged.Classes/Spells/CraftSpell.cs
@@ -177,7 +177,7 @@ public class CraftSpell(SpellEvents.OnSpellCast eventData, NwItem targetItem)
                 return;
             }
 
-            BrewPotion(spellPropId);
+            BrewPotion(caster, spellPropId);
             ChargeForSpellCraft(player, caster, brewPotionCost);
             ApplySpellCraftSuccessVfx(caster);
         }
@@ -260,19 +260,25 @@ public class CraftSpell(SpellEvents.OnSpellCast eventData, NwItem targetItem)
     private static int CalculateCraftWandCost(int spellPropCl, int spellInnateLevel) =>
         spellInnateLevel == 0 ? spellPropCl * 1 * 750 : spellPropCl * spellInnateLevel * 750;
     
-    private void BrewPotion(int spellPropId)
+    private void BrewPotion(NwCreature caster, int spellPropId)
     {
         targetItem.BaseItem = BaseItemType.EnchantedPotion!;
-        
         targetItem.AddItemProperty(ItemProperty.CastSpell((IPCastSpell)spellPropId, IPCastSpellNumUses.SingleUse), 
             EffectDuration.Permanent);
+        targetItem.Appearance.SetWeaponColor(ItemAppearanceWeaponColor.Bottom, GetPotionColor());
         
-        targetItem.Name = "Potion of "+_spell.Name;
-        targetItem.Description = _spell.Description.ToString();
+        Location? location = caster.Location;
+        if (location == null) return;
+
+        NwItem brewedPotion = targetItem.Clone(location);
+        NwModule.Instance.MoveObjectToLimbo(brewedPotion);
         
-        // This isn't working!
-        targetItem.Appearance.ChangeAppearance(appearance => 
-            appearance.SetWeaponColor(ItemAppearanceWeaponColor.Bottom, GetPotionColor()));
+        targetItem.Destroy();
+        
+        brewedPotion.Name = "Potion of "+_spell.Name;
+        brewedPotion.Description = _spell.Description.ToString();
+            
+        caster.AcquireItem(brewedPotion);
     }
 
     private byte GetPotionColor()
