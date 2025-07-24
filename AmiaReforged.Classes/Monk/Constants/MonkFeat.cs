406bc389644ca34fced2255c4bbc0f2baa125324
Mitro Hyvärinen <mitro.hyvarinen@gmail.com>
2025-07-25 00:33:50 +0300

    Remove unused monk feat constants

--- AmiaReforged.Classes/Monk/Constants/MonkFeat.cs
+++ AmiaReforged.Classes/Monk/Constants/MonkFeat.cs
@@ -4,15 +4,9 @@ namespace AmiaReforged.Classes.Monk.Constants;
 
 public static class MonkFeat
 {
-    public const int FlurryOfBlows = 204;
-    public const int QuiveringPalm = 1345;
-    public const int WholenessOfBody = 211;
     public const int KiStrike = 213;
     public const int KiStrike2 = 343;
     public const int KiStrike3 = 344;
-    public const int PerfectSelf = 216;
-    public const int EmptyBody = 297;
-    public const int PathOfEnlightenment = 1225;
     public const int StunningStrike = 1341;
     public const int EagleStrike = 1342;
     public const int AxiomaticStrike = 1343;
@@ -23,7 +17,6 @@ public static class MonkFeat
     public const int MonkFightingStyle = 1356;
     public const int MonkSpeedNew = 1339;
     public const int MonkDefense = 1340;
-    public const int StunningStrikeNew = 1341;
     public const int QuiveringPalmNew = 1345;
     public const int BodyKiPoint1 = 1347;
     public const int BodyKiPoint2 = 1348;
