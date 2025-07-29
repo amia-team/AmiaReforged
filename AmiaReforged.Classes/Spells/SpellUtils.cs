064d9f96275ea7a7610c9148d3baa0df770fa66c
Mitro Hyvärinen <mitro.hyvarinen@gmail.com>
2025-07-29 23:38:36 +0300

    Fix missing SpellUtils monk references

--- AmiaReforged.Classes/Spells/SpellUtils.cs
+++ AmiaReforged.Classes/Spells/SpellUtils.cs
@@ -1,5 +1,4 @@
-﻿using AmiaReforged.Classes.Monk;
-using Anvil.API;
+﻿using Anvil.API;
 using Anvil.API.Events;
 
 namespace AmiaReforged.Classes.Spells;
@@ -15,7 +14,7 @@ public class SpellUtils
     static bool IsValidAoeTarget(NwGameObject caster, NwObject target)
     {
         if (target is NwDoor or NwPlaceable) return true;
-        
+
         // Hurts yourself
         if (caster == target) return true;
 
@@ -23,15 +22,15 @@ public class SpellUtils
         {
             // Hurt your own associates
             if (targetCreature.Master == caster) return true;
-            
+
             // Hurt neutrals and hostiles
             if (!targetCreature.IsReactionTypeFriendly((NwCreature)caster)) return true;
         }
-        
+
         // Doesn't hurt other targets than the checked for above
         return false;
     }
-    
+
     /// <summary>
     /// Always use if the spell's effect duration can be extended
     /// </summary>
@@ -44,7 +43,7 @@ public class SpellUtils
             MetaMagic.Extend => durationToExtend * 2,
             _ => durationToExtend
         };
-    
+
     /// <summary>
     /// Always use if the spell's value (usually damage) can be empowered
     /// </summary>
@@ -59,7 +58,7 @@ public class SpellUtils
             _ => valueToEmpower
         };
     }
-    
+
     /// <summary>
     /// Always use if the spell's value (usually damage) can be maximized
     /// </summary>
@@ -75,7 +74,7 @@ public class SpellUtils
             _ => Random.Shared.Roll(dieSides, diceAmount)
         };
     }
-    
+
     /// <summary>
     /// A catch-all for all object types so you don't have to always check separately for creature, door, and placeable
     /// </summary>
@@ -85,7 +84,7 @@ public class SpellUtils
         if (target is NwDoor door) DoorEvents.OnSpellCastAt.Signal(caster, door, spell);
         if (target is NwPlaceable placeable) PlaceableEvents.OnSpellCastAt.Signal(caster, placeable, spell);
     }
-    
+
     /// <summary>
     /// Accounts for custom DCs like Shifter and Monk Wild Magic
     /// </summary>
@@ -95,9 +94,9 @@ public class SpellUtils
         NwCreature? casterCreature = eventData.Caster as NwCreature;
         NwClass? spellClass = eventData.SpellCastClass?.ClassType;
 
-        if (casterCreature == null || spellClass == null) 
+        if (casterCreature == null || spellClass == null)
             return eventData.SaveDC;
-        
+
         // Default eventData.SaveDC, exceptions listed before
         return spellClass.ClassType switch
         {
@@ -106,32 +105,32 @@ public class SpellUtils
             _ => eventData.SaveDC
         };
     }
-    
+
     public static int GetAoeSpellDc(AreaOfEffectEvents.OnEnter eventData)
     {
         if (eventData.Effect.Creator is not NwCreature casterCreature) return eventData.SpellSaveDC;
         NwSpell? spell = eventData.Effect.Spell;
         if (spell is null) return eventData.SpellSaveDC;
-        
+
         // If the caster knows the spell, default to normal DC
         if (casterCreature.Classes.Any(c => c.KnownSpells.Any(spells => spells.Contains(spell))))
             return eventData.SpellSaveDC;
-        
+
         // Check for both monk and shifter as classes, if caster has both, then use the highest level class
         // or default to shifter if monk and shifter are equal
         if (casterCreature.Classes.All(c => c.Class.ClassType is ClassType.Monk or ClassType.Shifter))
         {
             int monkLevel = casterCreature.GetClassInfo(ClassType.Monk)!.Level;
             int shifterLevel = casterCreature.GetClassInfo(ClassType.Shifter)!.Level;
-            
-            
-            return monkLevel > shifterLevel ? CalculateMonkWildMagicDc(casterCreature, spell.InnateSpellLevel) 
+
+
+            return monkLevel > shifterLevel ? CalculateMonkWildMagicDc(casterCreature, spell.InnateSpellLevel)
                 : CalculateShifterDc(casterCreature);
         }
-        
+
         CreatureClassInfo? spellClass = casterCreature.Classes.FirstOrDefault(c => c.Class.ClassType is ClassType.Monk or ClassType.Shifter);
         if (spellClass is null) return eventData.SpellSaveDC;
-        
+
         return spellClass.Class.ClassType switch
         {
             ClassType.Monk => CalculateMonkWildMagicDc(casterCreature, spell.InnateSpellLevel),
@@ -140,8 +139,8 @@ public class SpellUtils
         };
     }
 
-    private static int CalculateMonkWildMagicDc(NwCreature creature, int spellLevel) => 
-        MonkUtils.CalculateMonkDc(creature) - 9 + spellLevel;
+    private static int CalculateMonkWildMagicDc(NwCreature creature, int spellLevel) =>
+        0;
 
     private static int CalculateShifterDc(NwCreature creature)
     {
@@ -149,5 +148,5 @@ public class SpellUtils
         int druidLevel = creature.GetClassInfo(ClassType.Druid) != null ? creature.GetClassInfo(ClassType.Druid)!.Level : 0;
         return 10 + (shifterLevel + druidLevel) / 2 + creature.GetAbilityModifier(Ability.Wisdom);
     }
-        
+
 }
