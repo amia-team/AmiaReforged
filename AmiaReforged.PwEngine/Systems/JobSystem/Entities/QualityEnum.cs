6d03a64d4ca19cd2958dae7bbe865aa6304b6b96
Caelan L. Talmadge <talmadge12@gmail.com>
2025-08-01 11:05:54 -0500

    Add `ToItemPropertyEnum` extension for mapping `QualityEnum` to `IPQuality`.

--- AmiaReforged.PwEngine/Systems/JobSystem/Entities/QualityEnum.cs
+++ AmiaReforged.PwEngine/Systems/JobSystem/Entities/QualityEnum.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.RegularExpressions;
+using Anvil.API;
 
 namespace AmiaReforged.PwEngine.Systems.JobSystem.Entities;
 
@@ -23,4 +24,24 @@ public static class QualityEnumExtensions
 {
     public static string ToHumanizedString(this QualityEnum quality) =>
         Regex.Replace(quality.ToString(), pattern: "(\\B[A-Z])", replacement: " $1");
+
+    public static IPQuality ToItemPropertyEnum(this QualityEnum quality)
+    {
+        return quality switch
+        {
+            QualityEnum.Undefined => IPQuality.Unknown,
+            QualityEnum.Cut => IPQuality.Cut,
+            QualityEnum.Raw => IPQuality.Raw,
+            QualityEnum.VeryPoor => IPQuality.VeryPoor,
+            QualityEnum.Poor => IPQuality.Poor,
+            QualityEnum.BelowAverage => IPQuality.BelowAverage,
+            QualityEnum.Average => IPQuality.Average,
+            QualityEnum.AboveAverage => IPQuality.AboveAverage,
+            QualityEnum.Good => IPQuality.Good,
+            QualityEnum.VeryGood => IPQuality.VeryGood,
+            QualityEnum.Excellent => IPQuality.Excellent,
+            QualityEnum.Masterwork => IPQuality.Masterwork,
+            _ => IPQuality.Unknown
+        };
+    }
 }
