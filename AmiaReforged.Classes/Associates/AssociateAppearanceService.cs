632adc95d09fdd9700226d5cb7b8f282c54ad6a5
Mitro Hyvärinen <mitro.hyvarinen@gmail.com>
2025-08-10 13:15:34 +0300

    Delete lines

--- AmiaReforged.Classes/Associates/AssociateAppearanceService.cs
+++ AmiaReforged.Classes/Associates/AssociateAppearanceService.cs
@@ -35,9 +35,5 @@ public class AssociateAppearanceService
             EpicCompanionAppearance.ApplyEpicCompanionAppearance(eventData.Associate, eventData.Owner, pcKey);
 
         if (associateHasEpicCompanionAppearance) return;
-
-
     }
-
-
 }
