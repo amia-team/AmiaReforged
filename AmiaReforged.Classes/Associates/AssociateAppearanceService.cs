c362a86f42e9300f8b5f3c99fc82cd56196742cf
Mitro Stönky <90032996+Opustus@users.noreply.github.com>
2025-08-25 15:20:40 +0300

    Refactor legacy reskin application to C#

--- AmiaReforged.Classes/Associates/AssociateAppearanceService.cs
+++ AmiaReforged.Classes/Associates/AssociateAppearanceService.cs
@@ -12,6 +12,7 @@ public class AssociateAppearanceService
 
     private const string AssociateCustomizerTag = "ass_customizer";
     private const string PcKeyTag = "ds_pckey";
+    private const string ReskinWidgetTag = "ds_summ_change";
 
 
     public AssociateAppearanceService()
@@ -22,14 +23,21 @@ public class AssociateAppearanceService
 
     private void ChangeAssociateAppearance(OnAssociateAdd eventData)
     {
-        NwItem? pcKey = eventData.Owner.Inventory.Items.FirstOrDefault(item => item.Tag == PcKeyTag);
-
         NwItem? associateCustomizer = eventData.Owner.Inventory.Items.FirstOrDefault(item => item.Tag == AssociateCustomizerTag);
 
         bool associateHasCustomAppearance =
             CustomAppearance.ApplyCustomAppearance(eventData.Associate, eventData.AssociateType, associateCustomizer);
 
         if (associateHasCustomAppearance) return;
+
+        NwItem? legacyReskinWidget =  eventData.Owner.Inventory.Items.FirstOrDefault(item => item.Tag == ReskinWidgetTag);
+
+        bool associateHasLegacyReskin =
+            LegacySummonReskin.ApplySummonReskin(eventData.Associate, legacyReskinWidget);
+
+        if (associateHasLegacyReskin) return;
+
+        NwItem? pcKey = eventData.Owner.Inventory.Items.FirstOrDefault(item => item.Tag == PcKeyTag);
 
         bool associateHasEpicCompanionAppearance =
             EpicCompanionAppearance.ApplyEpicCompanionAppearance(eventData.Associate, eventData.Owner, pcKey);
