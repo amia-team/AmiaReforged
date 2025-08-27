46c469bba51bcfde16ba0de2cc4b157fe3dd8728
Mitro Stönky <90032996+Opustus@users.noreply.github.com>
2025-08-27 15:43:27 +0300

    Fix ./getvfx command and make available to players on test

--- AmiaReforged.PwEngine/Systems/Chat/Commands/DM/GetVfxCommand.cs
+++ AmiaReforged.PwEngine/Systems/Chat/Commands/DM/GetVfxCommand.cs
@@ -2,6 +2,7 @@ using Anvil.API;
 using Anvil.API.Events;
 using Anvil.Services;
 using NLog;
+using NWN.Core.NWNX;
 
 namespace AmiaReforged.PwEngine.Systems.Chat.Commands.DM;
 
@@ -12,79 +13,84 @@ public class GetVfx : IChatCommand
 
     public Task ExecuteCommand(NwPlayer caller, string[] args)
     {
-        if (!caller.IsDM) return Task.CompletedTask;
+        string environment = UtilPlugin.GetEnvironmentVariable("SERVER_MODE");
 
-        caller.EnterTargetMode(GetDurVfx,
+        if (!caller.IsDM && environment == "live")
+        {
+            caller.SendServerMessage
+                ($"Only DMs can use {Command} on the live server. You can use this on the test server.");
+
+            return Task.CompletedTask;
+        }
+
+        caller.EnterTargetMode(
+            targetingData => GetDurVfx(targetingData, caller),
             new TargetModeSettings { ValidTargets = ObjectTypes.Creature | ObjectTypes.Placeable | ObjectTypes.Door });
+
         return Task.CompletedTask;
     }
 
-    private async void GetDurVfx(ModuleEvents.OnPlayerTarget obj)
+    private void GetDurVfx(ModuleEvents.OnPlayerTarget targetingData, NwPlayer caller)
     {
-        try
+        if (targetingData.TargetObject is not (NwCreature or NwDoor or NwPlaceable)) return;
+
+        Dictionary<int, string> vfxIdAndLabel = [];
+
+        NwGameObject targetObject = (NwGameObject)targetingData.TargetObject;
+
+        foreach (Effect effect in targetObject.ActiveEffects)
         {
-            int vfxId;
-            int vfxIndex = 0;
-            string? vfxLabel;
-            string vfxList = $"Visual effects on {obj.TargetObject?.Name}:\n";
-            if (obj.TargetObject is NwCreature targetCreature)
-                foreach (Effect effect in targetCreature.ActiveEffects)
-                {
-                    if (effect.EffectType == EffectType.VisualEffect)
-                    {
-                        vfxId = effect.IntParams[0];
-                        vfxLabel = NwGameTables.VisualEffectTable[vfxId].Label;
-                        vfxIndex++;
-                        vfxList += $"\n{vfxIndex}. ID: {vfxId}. Label: {vfxLabel}\n";
-                    }
-                }
+            if (effect.EffectType != EffectType.VisualEffect) continue;
 
-            if (obj.TargetObject is NwDoor targetDoor)
-                foreach (Effect effect in targetDoor.ActiveEffects)
-                {
-                    if (effect.EffectType == EffectType.VisualEffect)
-                    {
-                        vfxId = effect.IntParams[0];
-                        vfxLabel = NwGameTables.VisualEffectTable[vfxId].Label;
-                        vfxIndex++;
-                        vfxList += $"\n{vfxIndex}. ID: {vfxId}. Label: {vfxLabel}\n";
-                    }
-                }
+            int vfxId = NwGameTables.VisualEffectTable[effect.IntParams[0]].RowIndex;
+            string? vfxLabel = NwGameTables.VisualEffectTable[effect.IntParams[0]].Label;
 
-            if (obj.TargetObject is NwPlaceable targetPlaceable)
-                foreach (Effect effect in targetPlaceable.ActiveEffects)
-                {
-                    if (effect.EffectType == EffectType.VisualEffect)
-                    {
-                        vfxId = effect.IntParams[0];
-                        vfxLabel = NwGameTables.VisualEffectTable[vfxId].Label;
-                        vfxIndex++;
-                        vfxList += $"\n{vfxIndex}. ID: {vfxId}. Label: {vfxLabel}\n";
-                    }
-                }
 
-            NwCreature? playerControlledCreature = obj.Player.ControlledCreature;
-            if(playerControlledCreature is null) return;
-            
-            if(playerControlledCreature.Location is null) return;
-            
-            NwPlaceable? helperObject =
-                NwPlaceable.Create(template: "ds_invis_object3", playerControlledCreature.Location);
-            if(helperObject is null) return;
-            
-            helperObject.Description = vfxList;
-            await obj.Player.ActionExamine(helperObject);
-            await NwTask.SwitchToMainThread();
-            
-            await NwTask.Delay(TimeSpan.FromSeconds(1));
-            await NwTask.SwitchToMainThread();
-            
-            helperObject.Destroy();
+            if (vfxLabel == null) continue;
 
+            vfxIdAndLabel.Add(vfxId, vfxLabel);
         }
-        catch (Exception e)
+
+        if (vfxIdAndLabel.Count == 0)
         {
-            LogManager.GetCurrentClassLogger().Error(e);
+            caller.FloatingTextString($"No visual effects found on {targetObject.Name}!");
+            return;
         }
+
+        string vfxList = "";
+
+        foreach (KeyValuePair<int, string> kvp in vfxIdAndLabel)
+        {
+            vfxList += $"ID: {kvp.Key.ToString()}".ColorString(ColorConstants.Yellow);
+            vfxList += $"\n{kvp.Value}\n\n";
+        }
+
+        _ = PrintVfxList(vfxList, caller, targetObject.Name);
+    }
+
+    private async Task PrintVfxList(string vfxList, NwPlayer caller, string targetObjectName)
+    {
+        NwCreature? controlledCreature = caller.ControlledCreature;
+        if (controlledCreature?.Location == null) return;
+
+        NwPlaceable? helperObject = NwPlaceable.Create(template: "x2_plc_psheet", controlledCreature.Location);
+        if (helperObject == null)
+        {
+            caller.SendServerMessage("Helper object failed to materialize.");
+            return;
+        }
+
+        helperObject.Name = $"{targetObjectName}'s Visual Effects";
+        helperObject.PortraitResRef = "po_plc_x0_tme_";
+        helperObject.Description = vfxList;
+
+        await NwTask.Delay(TimeSpan.FromMilliseconds(50));
+
+        caller.ForceExamine(helperObject);
+
+        await NwTask.Delay(TimeSpan.FromMilliseconds(50));
+
+        await helperObject.WaitForObjectContext();
+        helperObject.Destroy();
     }
 }
