5e98d6a7197bdeab577b53dcdfdb634122890448
Mitro Stönky <90032996+Opustus@users.noreply.github.com>
2025-09-03 21:22:09 +0300

    Create bard song data

---
+++ AmiaReforged.Classes/Bard/SongEffectValues.cs
@@ -0,0 +1,13 @@
+﻿namespace AmiaReforged.Classes.Bard;
+
+public struct SongEffectValues
+{
+    public int Attack { get; set; }
+    public int Damage { get; set; }
+    public int Will { get; set; }
+    public int Fortitude { get; set; }
+    public int Reflex { get; set; }
+    public int Hp { get; set; }
+    public int Ac { get; set; }
+    public int Skill { get; set; }
+}
