9fc2163ab7af4b0b4136e5569a64833fabdb19ac
Mitro Stönky <90032996+Opustus@users.noreply.github.com>
2025-09-08 00:14:20 +0300

    Give up getting portrait from appearance2da

--- AmiaReforged.Classes/Associates/EpicCompanionAppearance.cs
+++ AmiaReforged.Classes/Associates/EpicCompanionAppearance.cs
@@ -10,41 +10,50 @@ public static class EpicCompanionAppearance
     // Wyvern (Badger)
     private const string WyvernDescription = "Wyverns are vicious and deadly predators. Only an exceptional druid or ranger would be able to befriend one.";
     private const int WyvernAppearance = 458;
+    private const string WyvernPortrait = "po_wyvern";
 
     // Displacer Beast (Wolf)
     private const string DisplacerBeastDescription =
     "Displacer Beasts are evil creatures that most don't live long enough to tell about. It would take a terrifyingly skilled individual to claim one as their companion.";
     private const int DisplacerBeastAppearance = 925;
+    private const string DisplacerBeastPortrait = "po_blkpanther";
 
     // Dire Bear (Bear)
     private const string DireBearDescription = "Dire Bears are larger, more vicious versions of their common cousins. They are often found in the company of powerful rangers or druids.";
     private const int DireBearAppearance = 1174;
+    private const string DireBearPortrait = "po_beardire";
 
     // Raptor (Boar)
     private const string RaptorDescription = "Quick, vicious and smart, Raptors make excellent companions as they do enjoy to hunt in company.";
     private const int RaptorAppearance = 1439;
+    private const string RaptorPortrait = "po_allosaurus";
 
     // Giant Eagle (Hawk)
     private const string GiantEagleDescription = "Giant Eagles are terrifying raptors that can lift a full grown deer off the ground with ease. They are smart, fast, and have talons the size of human forearms.";
     private const int GiantEagleAppearance = 914;
+    private const string GiantEaglePortrait = "po_a_bird_rap";
 
     // Male Lion (Panther)
     private const string MaleLionDescription = "Lions are one of the few social cats. Once their trust is earned they make excellent partners in hunts, and other coordinated activities.";
     private const int MaleLionAppearance = 967;
+    private const string MaleLionPortrait = "po_mlion";
 
     // Giant Spider (Spider)
     private const string GiantSpiderDescription = "Large, hungry, and venomous, beware the Gargantuan Spider and the one skilled enough to tame it.";
     private const int GiantSpiderAppearance = 905;
+    private const string GiantSpiderPortrait = "po_spidgiant";
 
     // Guardian Wolf (DireWolf)
     private const string GuardianWolfDescription = "Guardian Wolves are rare, massive, and intelligent super predators. They make indispensable lifelong companions.";
     private const int GuardianWolfAppearance = 1140;
+    private const string GuardianWolfPortrait = "po_phod_fenris";
 
     // Giant Scorpion (DireRat)
     private const string GiantScorpionDescription = "Giant Scorpions are armored beasts that kill, eat and do whatever they please. Stay away unless you are prepared to fight an almost unstoppable mass of armor, pincers, and stinger.";
     private const int GiantScorpionAppearance = 338;
+    private const string GiantScorpionPortrait = "po_giascorp";
 
-    private record CompanionAppearanceData(string Description, int Appearance);
+    private record CompanionAppearanceData(string Description, int Appearance, string Portrait);
 
     public static bool ApplyEpicCompanionAppearance(NwCreature associate, AssociateType associateType, NwCreature owner, NwItem? pcKey)
     {
@@ -58,16 +67,16 @@ public static class EpicCompanionAppearance
 
         CompanionAppearanceData appearanceData = owner.AnimalCompanionType switch
         {
-            AnimalCompanionCreatureType.Badger => new CompanionAppearanceData(WyvernDescription, WyvernAppearance),
-            AnimalCompanionCreatureType.Wolf => new CompanionAppearanceData(DisplacerBeastDescription, DisplacerBeastAppearance),
-            AnimalCompanionCreatureType.Bear => new CompanionAppearanceData(DireBearDescription, DireBearAppearance),
-            AnimalCompanionCreatureType.Boar => new CompanionAppearanceData(RaptorDescription, RaptorAppearance),
-            AnimalCompanionCreatureType.Hawk => new CompanionAppearanceData(GiantEagleDescription, GiantEagleAppearance),
-            AnimalCompanionCreatureType.Panther => new CompanionAppearanceData(MaleLionDescription, MaleLionAppearance),
-            AnimalCompanionCreatureType.Spider => new CompanionAppearanceData(GiantSpiderDescription, GiantSpiderAppearance),
-            AnimalCompanionCreatureType.DireWolf => new CompanionAppearanceData(GuardianWolfDescription, GuardianWolfAppearance),
-            AnimalCompanionCreatureType.DireRat => new CompanionAppearanceData(GiantScorpionDescription, GiantScorpionAppearance),
-            _ => new CompanionAppearanceData(associate.Description, 0)
+            AnimalCompanionCreatureType.Badger => new CompanionAppearanceData(WyvernDescription, WyvernAppearance, WyvernPortrait),
+            AnimalCompanionCreatureType.Wolf => new CompanionAppearanceData(DisplacerBeastDescription, DisplacerBeastAppearance, DisplacerBeastPortrait),
+            AnimalCompanionCreatureType.Bear => new CompanionAppearanceData(DireBearDescription, DireBearAppearance, DireBearPortrait),
+            AnimalCompanionCreatureType.Boar => new CompanionAppearanceData(RaptorDescription, RaptorAppearance, RaptorPortrait),
+            AnimalCompanionCreatureType.Hawk => new CompanionAppearanceData(GiantEagleDescription, GiantEagleAppearance, GiantEaglePortrait),
+            AnimalCompanionCreatureType.Panther => new CompanionAppearanceData(MaleLionDescription, MaleLionAppearance, MaleLionPortrait),
+            AnimalCompanionCreatureType.Spider => new CompanionAppearanceData(GiantSpiderDescription, GiantSpiderAppearance, GiantSpiderPortrait),
+            AnimalCompanionCreatureType.DireWolf => new CompanionAppearanceData(GuardianWolfDescription, GuardianWolfAppearance, GuardianWolfPortrait),
+            AnimalCompanionCreatureType.DireRat => new CompanionAppearanceData(GiantScorpionDescription, GiantScorpionAppearance, GiantScorpionPortrait),
+            _ => new CompanionAppearanceData(associate.Description, 0, "")
         };
 
         switch (owner.AnimalCompanionType)
@@ -81,22 +90,10 @@ public static class EpicCompanionAppearance
                 break;
         }
 
-        AppearanceTableEntry appearanceEntry = NwGameTables.AppearanceTable.GetRow(appearanceData.Appearance);
-        associate.Appearance = appearanceEntry;
+        associate.Appearance = NwGameTables.AppearanceTable.GetRow(appearanceData.Appearance);
         associate.Description = appearanceData.Description;
-
-        string portraitResRef = appearanceEntry.Portrait ?? "po_clsranger_";
-        _ = SetPortrait(associate, portraitResRef);
+        associate.PortraitResRef = appearanceData.Portrait;
 
         return true;
     }
-
-    private static async Task SetPortrait(NwCreature associate, string portraitResRef)
-    {
-        await NwTask.Delay(TimeSpan.FromMilliseconds(1));
-
-        associate.PortraitResRef = portraitResRef;
-    }
 }
-
-
