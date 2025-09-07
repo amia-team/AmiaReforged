bd16e084005b342cdb4b087862f9a656984055a0
Caelan L. Talmadge <talmadge12@gmail.com>
2025-09-07 14:35:49 -0500

    Add `EconomyStartupService` and update `WorldEngineConfig` and `node_ore_native_copper.json`
    - Introduced `EconomyStartupService` to initialize and manage economy-related resources on startup.
    - Updated `WorldEngineConfig` to re-enable `[ServiceBinding]` attribute.
    - Renamed `HarvestContext` to `Requirement` in `node_ore_native_copper.json` for improved clarity.

---
+++ AmiaReforged.PwEngine/Systems/WorldEngine/EconomyStartupService.cs
@@ -0,0 +1,58 @@
+using AmiaReforged.PwEngine.Systems.WorldEngine.Industries;
+using AmiaReforged.PwEngine.Systems.WorldEngine.Items;
+using AmiaReforged.PwEngine.Systems.WorldEngine.ResourceNodes;
+using Anvil.Services;
+using DotNetEnv;
+using Microsoft.IdentityModel.Tokens;
+using NLog;
+using NWN.Core.NWNX;
+
+namespace AmiaReforged.PwEngine.Systems.WorldEngine;
+
+[ServiceBinding(typeof(EconomyStartupService))]
+public class EconomyStartupService
+{
+    private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+    private readonly ResourceDefinitionLoadingService _resourceLoader;
+    private readonly ItemDefinitionLoadingService _itemLoader;
+    private readonly IndustryDefinitionLoadingService _industryLoader;
+    private readonly IWorldConfigProvider _configProvider;
+
+    public EconomyStartupService(ResourceDefinitionLoadingService resourceLoader,
+        ItemDefinitionLoadingService itemLoader,
+        IndustryDefinitionLoadingService industryLoader,
+        IWorldConfigProvider configProvider)
+    {
+        string resourcePath = UtilPlugin.GetEnvironmentVariable("RESOURCE_PATH");
+
+        Environment.SetEnvironmentVariable("RESOURCE_PATH", resourcePath);
+
+        _resourceLoader = resourceLoader;
+        _itemLoader = itemLoader;
+        _industryLoader = industryLoader;
+        _configProvider = configProvider;
+
+        Init();
+    }
+
+    private void Init()
+    {
+        _industryLoader.Load();
+        _itemLoader.Load();
+        _resourceLoader.Load();
+
+        LogErrors(_industryLoader.Failures());
+        LogErrors(_itemLoader.Failures());
+        LogErrors(_resourceLoader.Failures());
+    }
+
+    private void LogErrors(List<FileLoadResult> failures)
+    {
+        foreach (FileLoadResult fileLoadResult in failures)
+        {
+            Log.Error($"Failed to load file {fileLoadResult.FileName}:");
+            Log.Error(fileLoadResult.Message);
+        }
+    }
+}
