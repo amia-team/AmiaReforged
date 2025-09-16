d014d7d342a0155eaeb917db170c099535f1bd88
Mitro Stönky <90032996+Opustus@users.noreply.github.com>
2025-09-16 14:40:27 +0300

    First crack at quillfire

--- AmiaReforged.Classes/Spells/Divine/ThirdCircle/Transmutation/Quillfire.cs
+++ AmiaReforged.Classes/Spells/Divine/ThirdCircle/Transmutation/Quillfire.cs
@@ -1,4 +1,4 @@
-﻿/*using Anvil.API;
+﻿using Anvil.API;
 using Anvil.API.Events;
 using Anvil.Services;
 
@@ -16,18 +16,18 @@ public class Quillfire : ISpell
         if (eventData.TargetObject is not NwCreature targetCreature) return;
 
         if (caster.IsReactionTypeFriendly(targetCreature)) return;
-        
+
         SpellUtils.SignalSpell(caster, targetCreature, eventData.Spell);
-        
+
         int numberOfQuills = caster.CasterLevel / 5;
         int dc = SpellUtils.GetSpellDc(eventData);
-        
+
         float distanceToTarget = caster.Distance(targetCreature);
         float quillTravelDelay = distanceToTarget / (3f * float.Log(distanceToTarget) + 2f);
-        
+
         const VfxType quillProjectile = (VfxType)359;
         Effect quillProjectileVfx = Effect.VisualEffect(quillProjectile);
-        
+
         for (int i = 0; i < numberOfQuills; i++)
             targetCreature.ApplyEffect(EffectDuration.Instant, quillProjectileVfx);
 
@@ -44,36 +44,39 @@ public class Quillfire : ISpell
 
                 SavingThrowResult savingThrowDamage =
                     targetCreature.RollSavingThrow(SavingThrow.Reflex, dc, SavingThrowType.Spell, caster);
-                
+
                 if (savingThrowDamage == SavingThrowResult.Success)
-                    targetCreature.ApplyEffect(EffectDuration.Instant, Effect.VisualEffect(VfxType.ImpReflexSaveThrowUse));
+                    targetCreature.ApplyEffect(EffectDuration.Instant,
+                        Effect.VisualEffect(VfxType.ImpReflexSaveThrowUse));
 
                 if ((hasEvasion || hasImpEvasion) && savingThrowDamage == SavingThrowResult.Success)
                     continue;
-                
-                int quillDamage = CalculateDamage(eventData.MetaMagicFeat);
-                
+
+                int quillDamage = CalculateQuillDamage(eventData.MetaMagicFeat);
+
                 if (hasImpEvasion || savingThrowDamage == SavingThrowResult.Success) quillDamage /= 2;
-                
+
                 Effect quillDamageEffect = Effect.LinkEffects(Effect.VisualEffect(VfxType.ComBloodSparkSmall),
                     Effect.Damage(quillDamage, DamageType.Piercing));
-                
+
                 targetCreature.ApplyEffect(EffectDuration.Instant, quillDamageEffect);
                 targetCreature.ApplyEffect(EffectDuration.Permanent, Effect.Poison(PoisonType.LargeScorpionVenom));
             }
         }
     }
-    
-    private static int CalculateDamage(MetaMagic metaMagic)
+
+    private static int CalculateQuillDamage(MetaMagic metaMagic)
     {
-        int damageByQuill = SpellUtils.MaximizeSpell(metaMagic, 6, 4);
-        damageByQuill = SpellUtils.EmpowerSpell(metaMagic, damageByQuill);
-        
-        return damageByQuill;
+        int quillDamage = SpellUtils.MaximizeSpell(metaMagic, 6, 4);
+        quillDamage = SpellUtils.EmpowerSpell(metaMagic, quillDamage);
+
+        return quillDamage;
     }
-    
+
     // This spell doesn't use Spell Resistance
     public bool CheckedSpellResistance { get; set; }
     public bool ResistedSpell { get; set; }
-    public void SetSpellResisted(bool result){}
-}*/
+
+    public void SetSpellResisted(bool result) { }
+
+}
