60fd3115e4f42aec2e021954d305cd13f7841f57
Mitro Stönky <90032996+Opustus@users.noreply.github.com>
2025-09-16 18:23:37 +0300

    Fix poison data getting

--- AmiaReforged.Classes/Poisons/PoisonData.cs
+++ AmiaReforged.Classes/Poisons/PoisonData.cs
@@ -33,15 +33,14 @@ public static class PoisonData
         TwoDimArray? poisonTable = NwGameTables.GetTable("Poison");
         if (poisonTable == null) return null;
 
-        int? id = poisonTable.GetInt((int)poisonType, "Id");
-        if (id == null) return null;
+        int rowIndex = (int)poisonType;
 
-        string? primaryAbilityString = poisonTable.GetString(id.Value, "Default_1");
+        string? primaryAbilityString = poisonTable.GetString(rowIndex, "Default_1");
         Ability? primaryAbility = null;
         if (primaryAbilityString != null && AbilityAbbreviationMap.TryGetValue(primaryAbilityString, out Ability parsedPrimaryAbility))
             primaryAbility = parsedPrimaryAbility;
 
-        string? secondaryAbilityString = poisonTable.GetString(id.Value, "Default_2");
+        string? secondaryAbilityString = poisonTable.GetString(rowIndex, "Default_2");
         Ability? secondaryAbility = null;
         if (secondaryAbilityString != null && AbilityAbbreviationMap.TryGetValue(secondaryAbilityString, out Ability parsedSecondaryAbility))
             secondaryAbility = parsedSecondaryAbility;
@@ -49,15 +48,15 @@ public static class PoisonData
         PoisonValues poisonValues = new
         (
             PoisonType: poisonType,
-            Name: poisonTable.GetStrRef(id.Value, "Name").ToString(),
-            PrimaryDieSides: poisonTable.GetInt(id.Value, "Dice_1"),
-            PrimaryDiceAmount: poisonTable.GetInt(id.Value, "Dam_1"),
+            Name: poisonTable.GetStrRef(rowIndex, "Name").ToString(),
+            PrimaryDieSides: poisonTable.GetInt(rowIndex, "Dice_1"),
+            PrimaryDiceAmount: poisonTable.GetInt(rowIndex, "Dam_1"),
             PrimaryAbilityDamage: primaryAbility,
-            PrimaryScript: poisonTable.GetString(id.Value, "Script_1"),
-            SecondaryDieSides: poisonTable.GetInt(id.Value, "Dice_2"),
-            SecondaryDiceAmount: poisonTable.GetInt(id.Value, "Dam_2"),
+            PrimaryScript: poisonTable.GetString(rowIndex, "Script_1"),
+            SecondaryDieSides: poisonTable.GetInt(rowIndex, "Dice_2"),
+            SecondaryDiceAmount: poisonTable.GetInt(rowIndex, "Dam_2"),
             SecondaryAbilityDamage: secondaryAbility,
-            SecondaryScript: poisonTable.GetString(id.Value, "Script_2")
+            SecondaryScript: poisonTable.GetString(rowIndex, "Script_2")
         );
 
         return poisonValues;
