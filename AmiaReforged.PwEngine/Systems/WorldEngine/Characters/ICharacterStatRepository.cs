eccf9ca721ef7ea34bf4a6471a4e9df88b0fbe65
Caelan L. Talmadge <talmadge12@gmail.com>
2025-09-17 15:54:39 -0500

    Introduce `CharacterStatRepository` with database-backed implementations
    - Added `CharacterStatRepository` implementing `ICharacterStatRepository`.
    - Includes methods for retrieving, updating, and saving character statistics using `PwEngineContext`.

--- AmiaReforged.PwEngine/Systems/WorldEngine/Characters/ICharacterStatRepository.cs
+++ AmiaReforged.PwEngine/Systems/WorldEngine/Characters/ICharacterStatRepository.cs
@@ -1,3 +1,6 @@
+using AmiaReforged.PwEngine.Database;
+using Anvil.Services;
+
 namespace AmiaReforged.PwEngine.Systems.WorldEngine.Characters;
 
 public interface ICharacterStatRepository
@@ -5,4 +8,25 @@ public interface ICharacterStatRepository
     CharacterStatistics? GetCharacterStatistics(Guid characterId);
     void UpdateCharacterStatistics(CharacterStatistics statistics);
     void SaveChanges();
+}
+
+[ServiceBinding(typeof(ICharacterStatRepository))]
+public class CharacterStatRepository(PwContextFactory factory) : ICharacterStatRepository
+{
+    private readonly PwEngineContext _ctx = factory.CreateDbContext();
+
+    public CharacterStatistics? GetCharacterStatistics(Guid characterId)
+    {
+        return _ctx.CharacterStatistics.FirstOrDefault(x => x.CharacterId == characterId);
+    }
+
+    public void UpdateCharacterStatistics(CharacterStatistics statistics)
+    {
+        _ctx.Update(statistics);
+    }
+
+    public void SaveChanges()
+    {
+        _ctx.SaveChanges();
+    }
 }
