fe4a64598529f4997b4f40640a5d8bae0693b1b8
Caelan L. Talmadge <talmadge12@gmail.com>
2025-09-22 08:33:03 -0500

    Remove `ICommand` and `ICommandHandler` interfaces from `WorldEngine.Types`

--- AmiaReforged.PwEngine/Systems/WorldEngine/Types/ICommandHandler.cs
+++
@@ -1,6 +0,0 @@
-namespace AmiaReforged.PwEngine.Systems.WorldEngine.Types;
-
-public interface ICommandHandler<in TCommand, TResponse> where TCommand : ICommand<TResponse>
-{
-    Task<TResponse> Handle(TCommand command);
-}
