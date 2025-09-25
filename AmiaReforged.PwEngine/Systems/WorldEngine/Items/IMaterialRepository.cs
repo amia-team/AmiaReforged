e252da495922546cd0ee2285ec6a4563285b1853
Caelan L. Talmadge <talmadge12@gmail.com>
2025-09-25 08:52:40 -0500

    Implement `IMaterialRepository` methods and add `InMemoryMaterialRepository`

--- AmiaReforged.PwEngine/Systems/WorldEngine/Items/IMaterialRepository.cs
+++ AmiaReforged.PwEngine/Systems/WorldEngine/Items/IMaterialRepository.cs
@@ -2,5 +2,8 @@ namespace AmiaReforged.PwEngine.Systems.WorldEngine.Items;
 
 public interface IMaterialRepository
 {
-
+    void Upsert(Material material);
+    void Remove(Material material);
+    IList<Material> All();
+    Material? Get(MaterialEnum e);
 }
