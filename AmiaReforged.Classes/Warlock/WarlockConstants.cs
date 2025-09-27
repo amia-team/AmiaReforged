a31b2435e21a2d010c99ac6b9ace98e1aaab630c
Mitro Stönky <90032996+Opustus@users.noreply.github.com>
2025-09-27 12:16:04 +0300

    Create WarlockConstants.cs

---
+++ AmiaReforged.Classes/Warlock/WarlockConstants.cs
@@ -0,0 +1,9 @@
+using Anvil.API;
+
+namespace AmiaReforged.Classes.Warlock;
+
+public class WarlockConstants
+{
+    public const int WarlockClassId = 57;
+    public static readonly NwClass? WarlockClass = NwClass.FromClassId(WarlockClassId);
+}
