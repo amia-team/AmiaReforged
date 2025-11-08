f5ed78fff9851599018a9b245811ee57e063eae6
Mitro Stönky <90032996+Opustus@users.noreply.github.com>
2025-11-08 05:28:49 +0200

    Parse effect tag for name when the effect doesn't originate from spell

--- AmiaReforged.Core/Services/CooldownNotifier.cs
+++ AmiaReforged.Core/Services/CooldownNotifier.cs
@@ -26,11 +26,30 @@ public class CooldownNotifier
         if (!creature.IsPlayerControlled(out NwPlayer? player)) return;
         if (eventData.Effect.Tag is not { } tag || !tag.EndsWith("_cd")) return;
 
-        string effectName = eventData.Effect.Spell?.Name.ToString() ?? "Unknown";
+        string effectName = eventData.Effect.Spell?.Name.ToString() ?? ParseEffectNameFromTag(tag);
 
         string abilityAvailableMessage = $"{effectName} is available!".ColorString(ColorConstants.Lime);
 
         player.FloatingTextString(abilityAvailableMessage, false);
     }
+
+    /// <summary>
+    /// Parses the tag string into a user-readable effect name.
+    /// </summary>
+    /// <param name="tag">The input effect tag string.</param>
+    /// <returns>A formatted effect name.</returns>
+    private static string ParseEffectNameFromTag(string tag)
+    {
+        // Remove the "_cd" suffix
+        tag = tag[..^3];
+
+        // split by underscores
+        string[] words = tag.Split('_');
+        // capitalize each word and lowercase the rest of the word
+        IEnumerable<string> formattedWords = words.Select(word => char.ToUpper(word[0]) + word[1..].ToLower());
+        // join words with spaces
+        return string.Join(" ", formattedWords);
+    }
+
 }
 
