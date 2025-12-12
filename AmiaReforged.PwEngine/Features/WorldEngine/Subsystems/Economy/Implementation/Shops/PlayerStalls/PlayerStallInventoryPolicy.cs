dcac89bd9a7e5a3df5e7a351fbd07aebe2ec8647
Caelan L. Talmadge <talmadge12@gmail.com>
2025-12-12 07:59:53 -0600

    Make it so you can't sell Aelosh your DC rod

--- AmiaReforged.PwEngine/Features/WorldEngine/Subsystems/Economy/Implementation/Shops/PlayerStalls/PlayerStallInventoryPolicy.cs
+++ AmiaReforged.PwEngine/Features/WorldEngine/Subsystems/Economy/Implementation/Shops/PlayerStalls/PlayerStallInventoryPolicy.cs
@@ -11,7 +11,27 @@ internal static class PlayerStallInventoryPolicy
     {
         "ds_pckey",
         "utilitytoken",
-        "utility_token_pactfeat"
+        "utility_token_pactfeat",
+        "ds_dicebag",
+        "pc_dcrod",
+        "ds_party_item",
+        "ds_pvp_tool",
+        "dmfi_pc_emote2",
+        "jj_asn_tool",
+        "umbran_arts_1",
+        "umbran_arts_2",
+        "umbran_arts_3",
+        "umbran_arts_4",
+        "umbran_arts_5",
+        "umbran_arts_6",
+        "umbran_arts_7",
+        "umbran_arts_8",
+        "barb_rages",
+        "alarmsong",
+        "firesong",
+        "icesong",
+        "warsong",
+
     };
 
     /// <summary>
@@ -30,6 +50,6 @@ internal static class PlayerStallInventoryPolicy
             return false;
         }
 
-        return !ResRefBlacklist.Contains(resRef);
+        return !ResRefBlacklist.Contains(resRef) || item.PlotFlag;
     }
 }
