6a1748cc4048dd2adf9db8ca2664d1485bf759f7
Caelan L. Talmadge <talmadge12@gmail.com>
2025-12-13 12:37:05 -0600

    Add support for item stacks in player stall products

---
+++ AmiaReforged.PwEngine/Migrations/20251213183656_SupportItemStacks.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AmiaReforged.PwEngine.Migrations
+{
+    /// <inheritdoc />
+    public partial class SupportItemStacks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasStacks",
+                table: "player_stall_products",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StackSize",
+                table: "player_stall_products",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasStacks",
+                table: "player_stall_products");
+
+            migrationBuilder.DropColumn(
+                name: "StackSize",
+                table: "player_stall_products");
+        }
+    }
+}
