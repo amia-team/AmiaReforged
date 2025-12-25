217ac7461530f68e30801fbe2c95c6fe1d70159d
JesAmia <mydnite_rondera@hotmail.com>
2025-12-25 08:13:35 -0600

    Update rebuild database fields

---
+++ AmiaReforged.PwEngine/Migrations/20251225051547_AddFullRebuildFields.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AmiaReforged.PwEngine.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFullRebuildFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OriginalFirstName",
+                table: "character_rebuilds",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OriginalLastName",
+                table: "character_rebuilds",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PcKeyData",
+                table: "character_rebuilds",
+                type: "bytea",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StoredGold",
+                table: "character_rebuilds",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StoredXp",
+                table: "character_rebuilds",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OriginalFirstName",
+                table: "character_rebuilds");
+
+            migrationBuilder.DropColumn(
+                name: "OriginalLastName",
+                table: "character_rebuilds");
+
+            migrationBuilder.DropColumn(
+                name: "PcKeyData",
+                table: "character_rebuilds");
+
+            migrationBuilder.DropColumn(
+                name: "StoredGold",
+                table: "character_rebuilds");
+
+            migrationBuilder.DropColumn(
+                name: "StoredXp",
+                table: "character_rebuilds");
+        }
+    }
+}
