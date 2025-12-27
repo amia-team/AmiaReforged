16fbd31a1a381b99a37616d3dbb822d89773dd3d
Mitro Stönky <90032996+Opustus@users.noreply.github.com>
2025-12-27 19:01:16 +0200

    Add toggleable Circle Kick

---
+++ AmiaReforged.Classes/GeneralFeats/CircleKick.cs
@@ -0,0 +1,47 @@
+using Anvil.API;
+using Anvil.API.Events;
+using Anvil.Services;
+using NLog;
+using NWN.Core;
+
+namespace AmiaReforged.Classes.GeneralFeats;
+
+[ServiceBinding(typeof(CircleKick))]
+
+public class CircleKick
+{
+    private const int CircleKickId = 0;
+
+    private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+    public CircleKick()
+    {
+        NwModule.Instance.OnUseFeat += ToggleCircleKick;
+        Log.Info(message: "Circle Kick initialized.");
+    }
+
+    private static void ToggleCircleKick(OnUseFeat eventData)
+    {
+        if (eventData.Feat.Id != CircleKickId) return;
+        if (!eventData.Creature.IsPlayerControlled(out NwPlayer? player)) return;
+
+        NwCreature kicker = eventData.Creature;
+
+        Effect? circleKickEffect = kicker.ActiveEffects.FirstOrDefault(e => e.EffectType == EffectType.BonusFeat
+                                                                         && e.IntParams[0] == (int)Feat.CircleKick);
+
+        if (circleKickEffect != null)
+        {
+            kicker.RemoveEffect(circleKickEffect);
+            player.FloatingTextString("*Circle Kick deactivated*", false, false);
+            return;
+        }
+
+        circleKickEffect = Effect.BonusFeat(Feat.CircleKick!);
+        circleKickEffect.SubType = EffectSubType.Unyielding;
+
+        kicker.ApplyEffect(EffectDuration.Permanent, circleKickEffect);
+
+        player.FloatingTextString("*Circle Kick activated*", false, false);
+    }
+}
