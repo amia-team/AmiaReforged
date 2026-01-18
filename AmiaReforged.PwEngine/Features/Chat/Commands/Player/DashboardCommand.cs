b8ca597ae6367cd0d0f51b60f4f82dee3fcfdc2b
JesAmia <mydnite_rondera@hotmail.com>
2026-01-18 16:20:34 -0600

    Add Dashboard command for no-rest situations

---
+++ AmiaReforged.PwEngine/Features/Chat/Commands/Player/DashboardCommand.cs
@@ -0,0 +1,30 @@
+﻿using AmiaReforged.PwEngine.Features.Player.Dashboard;
+using Anvil.API;
+using Anvil.Services;
+
+namespace AmiaReforged.PwEngine.Features.Chat.Commands.Player;
+
+/// <summary>
+/// Command to open the player dashboard
+/// </summary>
+[ServiceBinding(typeof(IChatCommand))]
+public class DashboardCommand : IChatCommand
+{
+    private readonly PlayerDashboardService _dashboardService;
+
+    public DashboardCommand(PlayerDashboardService dashboardService)
+    {
+        _dashboardService = dashboardService;
+    }
+
+    public string Command => "./dashboard";
+    public string Description => "Opens the player dashboard";
+    public string AllowedRoles => "Player";
+
+    public Task ExecuteCommand(NwPlayer caller, string[] args)
+    {
+        if (caller.IsDM) return Task.CompletedTask;
+
+        return _dashboardService.OpenDashboard(caller);
+    }
+}
