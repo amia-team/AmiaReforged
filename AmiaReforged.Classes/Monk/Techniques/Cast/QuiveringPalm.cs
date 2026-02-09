d8ff384e138d6b14c577e05f8f5bf079f93c522a
Mitro Stönky <90032996+Opustus@users.noreply.github.com>
2026-02-09 17:14:15 +0200

    Don't apply damage if death happens

--- AmiaReforged.Classes/Monk/Techniques/Cast/QuiveringPalm.cs
+++ AmiaReforged.Classes/Monk/Techniques/Cast/QuiveringPalm.cs
@@ -49,8 +49,22 @@ public class QuiveringPalm(AugmentationFactory augmentationFactory) : ICastTechn
 
         if (touchAttackResult is TouchAttackResult.Miss) return touchAttackResult;
 
-        ApplyQuiveringDamage(monk, targetCreature, damageType);
-        RollQuiveringDeath(monk, targetCreature);
+        int dc = MonkUtils.CalculateMonkDc(monk);
+
+        SavingThrowResult savingThrowResult =
+            targetCreature.RollSavingThrow(SavingThrow.Fortitude, dc, SavingThrowType.Death, monk);
+
+        if (savingThrowResult is SavingThrowResult.Success or SavingThrowResult.Immune)
+        {
+            ApplyQuiveringDamage(monk, targetCreature, damageType);
+            return touchAttackResult;
+        }
+
+        Effect quiveringEffect = Effect.Death(true);
+        Effect quiveringVfx = Effect.VisualEffect(VfxType.ImpDeath, false, 0.7f);
+
+        targetCreature.ApplyEffect(EffectDuration.Instant, quiveringEffect);
+        targetCreature.ApplyEffect(EffectDuration.Instant, quiveringVfx);
 
         return touchAttackResult;
     }
@@ -64,34 +78,5 @@ public class QuiveringPalm(AugmentationFactory augmentationFactory) : ICastTechn
             Effect.VisualEffect(VfxType.ImpDivineStrikeHoly, false, 0.2f));
 
         targetCreature.ApplyEffect(EffectDuration.Instant, quiveringDamage);
-    }
-
-    private static void RollQuiveringDeath(NwCreature monk, NwCreature targetCreature)
-    {
-        int dc = MonkUtils.CalculateMonkDc(monk);
-
-        SavingThrowResult savingThrowResult =
-            targetCreature.RollSavingThrow(SavingThrow.Fortitude, dc, SavingThrowType.Death, monk);
-
-        switch (savingThrowResult)
-        {
-            case SavingThrowResult.Immune:
-                break;
-            case SavingThrowResult.Success:
-                targetCreature.ApplyEffect(EffectDuration.Instant, Effect.VisualEffect(VfxType.ImpFortitudeSavingThrowUse));
-                break;
-            case SavingThrowResult.Failure:
-                ApplyQuiveringDeath(targetCreature);
-                break;
-        }
-    }
-
-    private static void ApplyQuiveringDeath(NwCreature targetCreature)
-    {
-        Effect quiveringEffect = Effect.Death(true);
-        Effect quiveringVfx = Effect.VisualEffect(VfxType.ImpDeath, false, 0.7f);
-
-        targetCreature.ApplyEffect(EffectDuration.Instant, quiveringEffect);
-        targetCreature.ApplyEffect(EffectDuration.Instant, quiveringVfx);
     }
 }
