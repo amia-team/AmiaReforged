4466b34b1b3879d1c049abd4dce1e93a6c402603
JesAmia <mydnite_rondera@hotmail.com>
2026-02-11 19:26:24 -0600

    Remove an unnecessary service

--- AmiaReforged.PwEngine/Features/WindowingSystem/DevicePropertyService.cs
+++ AmiaReforged.PwEngine/Features/WindowingSystem/DevicePropertyService.cs
@@ -10,17 +10,6 @@ public class DevicePropertyService
 {
     private static readonly Logger Log = LogManager.GetCurrentClassLogger();
 
-    public DevicePropertyService()
-    {
-        NwModule.Instance.OnClientEnter += LogDeviceDebug;
-    }
-
-    private void LogDeviceDebug(ModuleEvents.OnClientEnter obj)
-    {
-        Log.Info($"Gui Scale Weidth {GetGuiWidth(obj.Player)}");
-        Log.Info($"Gui Scale Height {GetGuiHeight(obj.Player)}");
-    }
-
     public int GetGuiWidth(NwPlayer player) => player.GetDeviceProperty(PlayerDeviceProperty.GuiWidth);
 
     public int GetGuiHeight(NwPlayer player) => player.GetDeviceProperty(PlayerDeviceProperty.GuiHeight);
