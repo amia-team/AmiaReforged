ba05f0ffd48a99e20bf6bce21f754c4392515b90
Caelan L. Talmadge <talmadge12@gmail.com>
2026-02-15 15:28:17 -0600

    Update DivineWrath logic

--- AmiaReforged.Classes/DivineChampion/DivineWrath.cs
+++ AmiaReforged.Classes/DivineChampion/DivineWrath.cs
@@ -43,7 +43,7 @@ public class DivineWrath : ISpell
         int chaMod = creature.GetAbilityModifier(Ability.Charisma);
         int durationBonus = chaMod < 2 ? 0 : chaMod / 2;
 
-        int divineWrathRounds = divineChampionLevel == 20 ? 10 + durationBonus + 5 : 10 + durationBonus;
+        int divineWrathRounds = divineChampionLevel >= 18 ? 10 + durationBonus + 5 : 10 + durationBonus;
 
         creature.ApplyEffect(EffectDuration.Temporary, divineWrath, NwTimeSpan.FromRounds(divineWrathRounds));
         creature.ApplyEffect(EffectDuration.Instant, Effect.VisualEffect(VfxType.ImpGoodHelp));
@@ -71,19 +71,13 @@ public class DivineWrath : ISpell
                 AttackBonus: 5,
                 DamageBonus: DamageBonus.Plus5,
                 UniversalSave: 5),
-            >= 15 and < 19 => new DivineWrathBonuses(
+            >= 15 and < 18 => new DivineWrathBonuses(
                 DamagePower: DamagePower.Plus4,
                 DamageReduction: 10,
                 AttackBonus: 7,
                 DamageBonus: DamageBonus.Plus7,
                 UniversalSave: 7),
-            19 => new DivineWrathBonuses(
-                DamagePower: DamagePower.Plus5,
-                DamageReduction: 15,
-                AttackBonus: 7,
-                DamageBonus: DamageBonus.Plus7,
-                UniversalSave: 7),
-            >= 20 => new DivineWrathBonuses(
+            >= 18 => new DivineWrathBonuses(
                 DamagePower: DamagePower.Energy,
                 DamageReduction: 20,
                 AttackBonus: 9,
