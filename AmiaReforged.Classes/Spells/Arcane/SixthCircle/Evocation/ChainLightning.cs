a04976b154719087e4d8a7c5cfe79a7e8dc5f245
Mitro Stönky <90032996+Opustus@users.noreply.github.com>
2026-02-27 00:43:03 +0200

    Fix Chain Lightning to work with shifter

--- AmiaReforged.Classes/Spells/Arcane/SixthCircle/Evocation/ChainLightning.cs
+++ AmiaReforged.Classes/Spells/Arcane/SixthCircle/Evocation/ChainLightning.cs
@@ -6,17 +6,14 @@ namespace AmiaReforged.Classes.Spells.Arcane.SixthCircle.Evocation;
 
 
 [ServiceBinding(typeof(ISpell))]
-public class ChainLightning : ISpell
+public class ChainLightning(ShifterDcService shifterDcService) : ISpell
 {
-    public bool CheckedSpellResistance { get; set; }
-    public bool ResistedSpell { get; set; }
     public string ImpactScript => "NW_S0_ChLightn";
 
-    private readonly Dictionary<uint, int> _spellHitsRemaining = new();
+    private readonly Dictionary<uint, int> _spellHitsRemaining = [];
     public void OnSpellImpact(SpellEvents.OnSpellCast eventData)
     {
         if (eventData.Caster is not NwCreature caster) return;
-        if (caster.Location == null) return;
 
         int arcs =
             caster.KnowsFeat(Feat.EpicSpellFocusEvocation!) ? 6 :
@@ -24,12 +21,12 @@ public class ChainLightning : ISpell
             caster.KnowsFeat(Feat.SpellFocusEvocation!) ? 4 :
             3;
 
+        int casterLevel = shifterDcService.GetShifterCasterLevel(caster, caster.CasterLevel);
+        int spellDc = shifterDcService.GetShifterDc(caster, eventData.SaveDC);
         uint spellKey = caster.ObjectId;
-        byte casterLevel = caster.GetClassInfo(eventData.SpellCastClass)?.Level ?? 0;
         _spellHitsRemaining[spellKey] = casterLevel;
 
         int damageDice = casterLevel > 20 ? 20 : casterLevel;
-        int spellDc = SpellUtils.GetSpellDc(eventData);
         MetaMagic metaMagic = eventData.MetaMagicFeat;
 
         Location? spellLocation = eventData.TargetObject?.Location;
@@ -39,12 +36,10 @@ public class ChainLightning : ISpell
                      .GetObjectsInShapeByType<NwCreature>(Shape.Sphere, RadiusSize.Colossal,true)
                      .Where(caster.IsReactionTypeHostile))
         {
-            if (arcs == 0) break;
-            if (_spellHitsRemaining[spellKey] <= 0) break;
-
+            if (arcs == 0 || _spellHitsRemaining[spellKey] <= 0) break;
             if (hostileCreature.IsDead) continue;
 
-            _ = ShootArc(caster, hostileCreature, damageDice, spellDc, metaMagic, spellKey, eventData.Spell);
+            _ = ShootArc(caster, hostileCreature, damageDice, spellDc, metaMagic, spellKey, eventData.Spell, casterLevel);
 
             arcs--;
             _spellHitsRemaining[spellKey]--;
@@ -52,7 +47,7 @@ public class ChainLightning : ISpell
     }
 
     private async Task ShootArc(NwCreature caster, NwCreature hostileCreature, int damageDice, int spellDc,
-        MetaMagic metaMagic, uint spellKey, NwSpell spell)
+        MetaMagic metaMagic, uint spellKey, NwSpell spell, int casterLevel)
     {
         CreatureEvents.OnSpellCastAt.Signal(caster, hostileCreature, spell);
 
@@ -61,13 +56,15 @@ public class ChainLightning : ISpell
             TimeSpan.FromSeconds(0.5));
 
         await NwTask.Delay(TimeSpan.FromSeconds(0.25f));
-
         await caster.WaitForObjectContext();
+
+        if (hostileCreature.IsDead || caster.SpellResistanceCheck(hostileCreature, spell, casterLevel) ||
+            hostileCreature.Location == null) return;
+
         RollDamage(hostileCreature, caster, damageDice, spellDc, metaMagic);
 
-        if (hostileCreature.Location == null) return;
-
         await NwTask.Delay(TimeSpan.FromSeconds(0.25f));
+        await caster.WaitForObjectContext();
 
         damageDice /= 2;
 
@@ -75,22 +72,20 @@ public class ChainLightning : ISpell
                      .GetObjectsInShapeByType<NwCreature>(Shape.Sphere, RadiusSize.Large,true)
                      .Where(caster.IsReactionTypeHostile))
         {
-            if (secondaryCreature.IsDead) continue;
+            if (_spellHitsRemaining[spellKey] <= 0) break;
 
-            if (secondaryCreature == hostileCreature) continue;
-
-            if (!_spellHitsRemaining.TryGetValue(spellKey, out int value) || value <= 0) break;
+            if (secondaryCreature.IsDead || secondaryCreature == hostileCreature ||
+                caster.SpellResistanceCheck(secondaryCreature, spell, casterLevel)) continue;
 
             CreatureEvents.OnSpellCastAt.Signal(caster, secondaryCreature, spell);
 
-            if (!hostileCreature.IsDead)
+            if (hostileCreature is { IsValid: true, IsDead: false })
             {
                 secondaryCreature.ApplyEffect(EffectDuration.Temporary,
                     Effect.Beam(VfxType.BeamLightning, hostileCreature, BodyNode.Chest),
                     TimeSpan.FromSeconds(0.5));
             }
 
-            await caster.WaitForObjectContext();
             RollDamage(secondaryCreature, caster, damageDice, spellDc, metaMagic);
 
             _spellHitsRemaining[spellKey]--;
@@ -106,29 +101,23 @@ public class ChainLightning : ISpell
         SavingThrowResult savingThrow =
             targetCreature.RollSavingThrow(SavingThrow.Reflex, spellDc, SavingThrowType.Electricity, caster);
 
-        if (savingThrow == SavingThrowResult.Success || targetCreature.KnowsFeat(Feat.ImprovedEvasion!))
+        bool hasEvasion = targetCreature.KnowsFeat(NwFeat.FromFeatType(Feat.Evasion)!);
+        bool hasImprovedEvasion = targetCreature.KnowsFeat(NwFeat.FromFeatType(Feat.ImprovedEvasion)!);
+
+        if ((hasEvasion || hasImprovedEvasion) && savingThrow == SavingThrowResult.Success)
         {
             targetCreature.ApplyEffect(EffectDuration.Instant, Effect.VisualEffect(VfxType.ImpReflexSaveThrowUse));
-            damage /= 2;
-        }
-
-        if (targetCreature.KnowsFeat(Feat.ImprovedEvasion!) || targetCreature.KnowsFeat(Feat.Evasion!)
-            && savingThrow == SavingThrowResult.Success)
-        {
             return;
         }
 
-        Effect damageEffect = Effect.LinkEffects(
-            Effect.Damage(damage, DamageType.Electrical),
-            Effect.VisualEffect(VfxType.ImpLightningS)
-        );
+        if (hasImprovedEvasion || savingThrow == SavingThrowResult.Success)
+            damage /= 2;
 
-        targetCreature.ApplyEffect(EffectDuration.Instant, damageEffect);
+        targetCreature.ApplyEffect(EffectDuration.Instant, Effect.Damage(damage, DamageType.Electrical));
+        targetCreature.ApplyEffect(EffectDuration.Instant, Effect.VisualEffect(VfxType.ImpLightningS));
     }
 
-
-    public void SetSpellResisted(bool result)
-    {
-        ResistedSpell = result;
-    }
+    public bool CheckedSpellResistance { get; set; }
+    public bool ResistedSpell { get; set; }
+    public void SetSpellResisted(bool result) { }
 }
