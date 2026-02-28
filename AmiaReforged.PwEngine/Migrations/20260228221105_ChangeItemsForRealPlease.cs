eae0b7aa8a5c7da5d5e9a68a594e33ae2561bb59
Caelan L. Talmadge <talmadge12@gmail.com>
2026-02-28 16:11:30 -0600

    Update ItemBlueprints migration to change primary key to ItemTag and add ResRef index

---
+++ AmiaReforged.PwEngine/Migrations/20260228221105_ChangeItemsForRealPlease.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AmiaReforged.PwEngine.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeItemsForRealPlease : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ItemBlueprints",
+                table: "ItemBlueprints");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ItemBlueprints_ItemTag",
+                table: "ItemBlueprints");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ItemBlueprints",
+                table: "ItemBlueprints",
+                column: "item_tag");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemBlueprints_ResRef",
+                table: "ItemBlueprints",
+                column: "res_ref");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ItemBlueprints",
+                table: "ItemBlueprints");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ItemBlueprints_ResRef",
+                table: "ItemBlueprints");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ItemBlueprints",
+                table: "ItemBlueprints",
+                column: "res_ref");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemBlueprints_ItemTag",
+                table: "ItemBlueprints",
+                column: "item_tag",
+                unique: true);
+        }
+    }
+}
