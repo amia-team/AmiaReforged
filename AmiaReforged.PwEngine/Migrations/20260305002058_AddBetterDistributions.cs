df5a67af0775bfa6a7b93554ef5fa5f4a5e06130
Caelan L. Talmadge <talmadge12@gmail.com>
2026-03-04 18:22:07 -0600

    Add distribution method column to SpawnGroups for improved spawn logic

---
+++ AmiaReforged.PwEngine/Migrations/20260305002058_AddBetterDistributions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AmiaReforged.PwEngine.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBetterDistributions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "distribution_method",
+                table: "SpawnGroups",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "distribution_method",
+                table: "SpawnGroups");
+        }
+    }
+}
