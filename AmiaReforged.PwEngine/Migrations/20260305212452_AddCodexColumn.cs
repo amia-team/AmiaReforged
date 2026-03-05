d32b404d6ba09b39f5bf2f3b4724616616d2d2e4
Caelan L. Talmadge <talmadge12@gmail.com>
2026-03-05 15:25:09 -0600

    Add 'is_always_available' column to codex lore definitions and create corresponding index

---
+++ AmiaReforged.PwEngine/Migrations/20260305212452_AddCodexColumn.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AmiaReforged.PwEngine.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCodexColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_always_available",
+                table: "codex_lore_definitions",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "codex_lore_definitions_always_available_idx",
+                table: "codex_lore_definitions",
+                column: "is_always_available");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "codex_lore_definitions_always_available_idx",
+                table: "codex_lore_definitions");
+
+            migrationBuilder.DropColumn(
+                name: "is_always_available",
+                table: "codex_lore_definitions");
+        }
+    }
+}
