3d174c3a67da7fedb0eed1c73581d78c99e399b3
Caelan L. Talmadge <talmadge12@gmail.com>
2026-03-05 22:41:24 -0600

    Add ability recharge mechanism for Raven familiar on combat round end

--- AmiaReforged.Classes/Associates/Bonuses/Strategies/Familiars/RavenFamiliarBonuses.cs
+++ AmiaReforged.Classes/Associates/Bonuses/Strategies/Familiars/RavenFamiliarBonuses.cs
@@ -1,4 +1,5 @@
 using Anvil.API;
+using Anvil.API.Events;
 using Anvil.Services;
 using NWN.Core;
 using NWN.Core.NWNX;
@@ -13,16 +14,33 @@ public class RavenFamiliarBonuses : IFamiliarBonusStrategy
 
     public void Apply(NwCreature owner, NwCreature associate)
     {
+        associate.OnCombatRoundEnd += RechargeRandomUsedAbility;
         switch (associate.Level)
         {
             case >= 5 and < 10:
                 NwSpell? fromSpellType = NwSpell.FromSpellType(Spell.AbilityHowlFear);
-                if(fromSpellType is null) return;
+                if (fromSpellType is null) return;
 
                 SpecialAbility specialAbility = new SpecialAbility(fromSpellType, (byte)associate.Level);
 
                 associate.AddSpecialAbility(specialAbility);
                 break;
         }
+    }
+
+    private void RechargeRandomUsedAbility(CreatureEvents.OnCombatRoundEnd obj)
+    {
+        NwCreature creature = obj.Creature;
+        if (creature != obj.Creature) return;
+
+        List<SpecialAbility> usedAbilities = creature.SpecialAbilities.Where(ab => !ab.Ready).ToList();
+        if (usedAbilities.Count == 0) return;
+
+        SpecialAbility abilityToRecharge = usedAbilities[Random.Shared.Next(usedAbilities.Count)];
+
+        Effect vfx = Effect.VisualEffect(VfxType.ImpPdkFinalStand);
+        creature.ApplyEffect(EffectDuration.Instant, vfx);
+
+        abilityToRecharge.Ready = true;
     }
 }
