3d98245ca8223c3a7f3887ca411b8960e7034c1b
Caelan L. Talmadge <talmadge12@gmail.com>
2026-03-08 20:40:32 -0500

    Make level 15 and greater hell hounds more danger, less grrr

--- AmiaReforged.Classes/Associates/Bonuses/Strategies/Familiars/HellHoundStrategy.cs
+++ AmiaReforged.Classes/Associates/Bonuses/Strategies/Familiars/HellHoundStrategy.cs
@@ -16,27 +16,32 @@ public class HellHoundStrategy : IFamiliarBonusStrategy
 
         bite?.RemoveItemProperties();
 
-            int enhancementBonus = 1 + associate.Level / 5;
+        int enhancementBonus = 1 + associate.Level / 5;
 
-            bite?.AddItemProperty(ItemProperty.EnhancementBonus(enhancementBonus), EffectDuration.Permanent);
+        bite?.AddItemProperty(ItemProperty.EnhancementBonus(enhancementBonus), EffectDuration.Permanent);
 
-            ItemProperty fireDamage = enhancementBonus switch
-            {
-                1 => ItemProperty.DamageBonus(IPDamageType.Fire, IPDamageBonus.Plus1),
-                2 => ItemProperty.DamageBonus(IPDamageType.Fire, IPDamageBonus.Plus2),
-                3 => ItemProperty.DamageBonus(IPDamageType.Fire, IPDamageBonus.Plus3),
-                4 => ItemProperty.DamageBonus(IPDamageType.Fire, IPDamageBonus.Plus4),
-                _ => ItemProperty.DamageBonus(IPDamageType.Fire, IPDamageBonus.Plus5)
-            };
-            bite?.AddItemProperty(fireDamage, EffectDuration.Permanent);
+        ItemProperty fireDamage = enhancementBonus switch
+        {
+            1 => ItemProperty.DamageBonus(IPDamageType.Fire, IPDamageBonus.Plus1),
+            2 => ItemProperty.DamageBonus(IPDamageType.Fire, IPDamageBonus.Plus2),
+            3 => ItemProperty.DamageBonus(IPDamageType.Fire, IPDamageBonus.Plus3),
+            4 => ItemProperty.DamageBonus(IPDamageType.Fire, IPDamageBonus.Plus4),
+            _ => ItemProperty.DamageBonus(IPDamageType.Fire, IPDamageBonus.Plus5)
+        };
+        bite?.AddItemProperty(fireDamage, EffectDuration.Permanent);
 
-            ItemProperty monsterDamage = associate.Level switch
-            {
-                >= 10 and < 20 => ItemProperty.MonsterDamage(IPMonsterDamage.Damage1d10),
-                >= 20 and < 29 => ItemProperty.MonsterDamage(IPMonsterDamage.Damage1d12),
-                >= 30 and < 35 => ItemProperty.MonsterDamage(IPMonsterDamage.Damage1d20),
-                _ => ItemProperty.MonsterDamage(IPMonsterDamage.Damage1d8)
-            };
-            bite?.AddItemProperty(monsterDamage, EffectDuration.Permanent);
+        ItemProperty monsterDamage = associate.Level switch
+        {
+            >= 10 and < 20 => ItemProperty.MonsterDamage(IPMonsterDamage.Damage1d10),
+            >= 20 and < 29 => ItemProperty.MonsterDamage(IPMonsterDamage.Damage1d12),
+            >= 30 and < 35 => ItemProperty.MonsterDamage(IPMonsterDamage.Damage1d20),
+            _ => ItemProperty.MonsterDamage(IPMonsterDamage.Damage1d8)
+        };
+        bite?.AddItemProperty(monsterDamage, EffectDuration.Permanent);
+
+        if (associate.Level < 15) return;
+
+        SpecialAbility elementalShield = new(NwSpell.FromSpellType(Spell.ElementalShield)!, 15);
+        associate.AddSpecialAbility(elementalShield);
     }
 }
