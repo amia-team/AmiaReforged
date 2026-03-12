2656f4ec5dcf1992f728e8cf8affd8327764da1b
Caelan L. Talmadge <talmadge12@gmail.com>
2026-03-12 13:50:28 -0500

    Add AppearanceId column to WorkstationDefinitions table

---
+++ AmiaReforged.PwEngine/Migrations/20260312183507_AddPlcIdToWorkstation.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AmiaReforged.PwEngine.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPlcIdToWorkstation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "appearance_id",
+                table: "WorkstationDefinitions",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "appearance_id",
+                table: "WorkstationDefinitions");
+        }
+    }
+}
