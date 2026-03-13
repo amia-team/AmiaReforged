8afe9f669c1a8a98c86d9f6cda276500f80a5042
Caelan L. Talmadge <talmadge12@gmail.com>
2026-03-13 16:08:41 -0500

    Remove 'RequiredProficiency' column from RecipeTemplateDefinitions; update migration

---
+++ AmiaReforged.PwEngine/Migrations/20260313210721_RemodelRecipes.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AmiaReforged.PwEngine.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemodelRecipes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "required_proficiency",
+                table: "RecipeTemplateDefinitions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "required_proficiency",
+                table: "RecipeTemplateDefinitions",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
