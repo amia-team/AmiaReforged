4341e487730b9baf73c50195fcf3abc08f3bc7c4
Caelan L. Talmadge <talmadge12@gmail.com>
2026-03-13 15:40:51 -0500

    Rename 'ProcessId' to 'RequiredTools' in RecipeTemplateDefinitions; update migration and model snapshot

---
+++ AmiaReforged.PwEngine/Migrations/20260313203642_ChangeProcessTotools.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AmiaReforged.PwEngine.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeProcessTotools : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "process_id",
+                table: "RecipeTemplateDefinitions");
+
+            migrationBuilder.AddColumn<string>(
+                name: "required_tools",
+                table: "RecipeTemplateDefinitions",
+                type: "jsonb",
+                nullable: false,
+                defaultValue: "[]");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "required_tools",
+                table: "RecipeTemplateDefinitions");
+
+            migrationBuilder.AddColumn<string>(
+                name: "process_id",
+                table: "RecipeTemplateDefinitions",
+                type: "character varying(128)",
+                maxLength: 128,
+                nullable: true);
+        }
+    }
+}
