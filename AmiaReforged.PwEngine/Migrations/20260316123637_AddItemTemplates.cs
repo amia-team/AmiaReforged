f1cad5ae690761dd9f9311627d16d1ca38f73a7b
Caelan L. Talmadge <talmadge12@gmail.com>
2026-03-16 07:55:27 -0500

    Add VariantsJson column to ItemBlueprints: enable storage of item variant data in JSON format

---
+++ AmiaReforged.PwEngine/Migrations/20260316123637_AddItemTemplates.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AmiaReforged.PwEngine.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddItemTemplates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "VariantsJson",
+                table: "ItemBlueprints",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VariantsJson",
+                table: "ItemBlueprints");
+        }
+    }
+}
