d9d1c33399610064dd53dd5466290f8524191922
Caelan L. Talmadge <talmadge12@gmail.com>
2026-03-16 14:35:18 -0500

    Add tree_properties column to ResourceNodeDefinitions for harvest strategies

---
+++ AmiaReforged.PwEngine/Migrations/20260316193431_AddHarvestStrategies.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AmiaReforged.PwEngine.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHarvestStrategies : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "tree_properties",
+                table: "ResourceNodeDefinitions",
+                type: "jsonb",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "tree_properties",
+                table: "ResourceNodeDefinitions");
+        }
+    }
+}
