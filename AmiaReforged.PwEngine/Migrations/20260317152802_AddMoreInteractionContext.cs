28b038edd08295de7b65d94848b8f757bd5064b8
Caelan L. Talmadge <talmadge12@gmail.com>
2026-03-17 10:28:24 -0500

    Add AllowedAreaResRefsJson, RequiredIndustryTagsJson, and RequiredKnowledgeTagsJson to InteractionDefinitions

---
+++ AmiaReforged.PwEngine/Migrations/20260317152802_AddMoreInteractionContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AmiaReforged.PwEngine.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMoreInteractionContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AllowedAreaResRefsJson",
+                table: "InteractionDefinitions",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RequiredIndustryTagsJson",
+                table: "InteractionDefinitions",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RequiredKnowledgeTagsJson",
+                table: "InteractionDefinitions",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AllowedAreaResRefsJson",
+                table: "InteractionDefinitions");
+
+            migrationBuilder.DropColumn(
+                name: "RequiredIndustryTagsJson",
+                table: "InteractionDefinitions");
+
+            migrationBuilder.DropColumn(
+                name: "RequiredKnowledgeTagsJson",
+                table: "InteractionDefinitions");
+        }
+    }
+}
