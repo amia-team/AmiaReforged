5913f1b1d7e9ac400e292339149534cf18cef219
Caelan L. Talmadge <talmadge12@gmail.com>
2026-03-16 22:55:45 -0500

    Add MinQuality and MaxQuality columns to ResourceNodeDefinitions

---
+++ AmiaReforged.PwEngine/Migrations/20260317035502_AddQualityOverridesToNodes.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AmiaReforged.PwEngine.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddQualityOverridesToNodes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "max_quality",
+                table: "ResourceNodeDefinitions",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "min_quality",
+                table: "ResourceNodeDefinitions",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "max_quality",
+                table: "ResourceNodeDefinitions");
+
+            migrationBuilder.DropColumn(
+                name: "min_quality",
+                table: "ResourceNodeDefinitions");
+        }
+    }
+}
