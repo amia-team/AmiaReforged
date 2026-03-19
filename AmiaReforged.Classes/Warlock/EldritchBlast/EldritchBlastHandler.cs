8fc03b38b2b952512bb12dbc02e27f67e0715ae5
Mitro Stönky <90032996+Opustus@users.noreply.github.com>
2026-03-19 17:08:57 +0200

    Centralise invocation cast routing

---
+++ AmiaReforged.Classes/Warlock/EldritchBlast/EldritchBlastHandler.cs
@@ -0,0 +1,27 @@
+using AmiaReforged.Classes.Warlock.EldritchBlast.Essence;
+using AmiaReforged.Classes.Warlock.EldritchBlast.Shape;
+using Anvil.API;
+using Anvil.API.Events;
+using Anvil.Services;
+using AmiaReforged.Classes.Warlock.Constants;
+
+namespace AmiaReforged.Classes.Warlock.EldritchBlast;
+
+[ServiceBinding(typeof(EldritchBlastHandler))]
+public class EldritchBlastHandler(EssenceFactory essenceFactory, ShapeFactory shapeFactory)
+{
+    public void HandleEldritchBlast(NwCreature warlock, int invocationCl,
+        SpellEvents.OnSpellCast castData)
+    {
+        ShapeType shapeType = (ShapeType)castData.Spell.Id;
+        int dc = warlock.InvocationDc(invocationCl);
+
+        EssenceData essenceData = essenceFactory.GetEssenceData(warlock, invocationCl);
+
+        shapeFactory.GetShapeType(shapeType)?
+            .CastEldritchShape(warlock, invocationCl, dc, essenceData, castData);
+
+        if (warlock.KnowsFeat(WarlockFeats.EldritchMaster!))
+            warlock.ApplyEldritchMasterAttackBonus();
+    }
+}
---
+++ AmiaReforged.Classes/Warlock/InvocationController.cs
@@ -0,0 +1,61 @@
+using AmiaReforged.Classes.Warlock.EldritchBlast;
+using AmiaReforged.Classes.Warlock.Feats;
+using Anvil.API;
+using Anvil.API.Events;
+using Anvil.Services;
+using NLog;
+using NWN.Core.NWNX;
+
+namespace AmiaReforged.Classes.Warlock;
+
+[ServiceBinding(typeof(InvocationController))]
+public class InvocationController
+{
+    private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+    private readonly InvocationFactory _invocationFactory;
+    private readonly EldritchBlastHandler _eldritchBlastHandler;
+
+    public InvocationController(
+        EldritchBlastHandler eldritchBlastHandler,
+        InvocationFactory invocationFactory,
+        ScriptHandleFactory scriptHandleFactory,
+        IEnumerable<IInvocation> invocations)
+    {
+        _invocationFactory = invocationFactory;
+        _eldritchBlastHandler = eldritchBlastHandler;
+
+        scriptHandleFactory.RegisterScriptHandler(WarlockExtensions.EldritchBlastImpactScript, HandleInvocation);
+        Log.Info($"Hooked Warlock script (Eldritch Blast): {WarlockExtensions.EldritchBlastImpactScript}");
+
+        foreach (IInvocation invocation in invocations)
+        {
+            if (string.IsNullOrEmpty(invocation.ImpactScript)) continue;
+
+            scriptHandleFactory.RegisterScriptHandler(invocation.ImpactScript, HandleInvocation);
+            Log.Info($"Hooked Warlock script: {nameof(invocation)}");
+        }
+    }
+
+    private ScriptHandleResult HandleInvocation(CallInfo info)
+    {
+        SpellEvents.OnSpellCast eventData = new();
+        if (eventData.Caster is not NwCreature warlock) return ScriptHandleResult.Handled;
+
+        int invocationCl = warlock.GetInvocationCasterLevel();
+
+        if (eventData.Spell.ImpactScript == WarlockExtensions.EldritchBlastImpactScript)
+        {
+            _eldritchBlastHandler.HandleEldritchBlast(warlock, invocationCl, eventData);
+            return ScriptHandleResult.Handled;
+        }
+
+        _invocationFactory.CastInvocation(
+            info.ScriptName,
+            warlock,
+            invocationCl,
+            eventData);
+
+        return ScriptHandleResult.Handled;
+    }
+}
