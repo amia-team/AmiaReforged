9b9a6b42abfbf339853919a3b5bb779994145d5f
Caelan L. Talmadge <talmadge12@gmail.com>
2026-03-22 13:57:56 -0500

    Refactor CodexQuestDefinitions to use StagesJson instead of ObjectivesJson; remove HintsJson

---
+++ AmiaReforged.PwEngine/Migrations/20260322185651_UseStagesAndHints.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AmiaReforged.PwEngine.Migrations
+{
+    /// <inheritdoc />
+    public partial class UseStagesAndHints : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HintsJson",
+                table: "CodexQuestDefinitions");
+
+            migrationBuilder.RenameColumn(
+                name: "ObjectivesJson",
+                table: "CodexQuestDefinitions",
+                newName: "StagesJson");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StagesJson",
+                table: "CodexQuestDefinitions",
+                newName: "ObjectivesJson");
+
+            migrationBuilder.AddColumn<string>(
+                name: "HintsJson",
+                table: "CodexQuestDefinitions",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
