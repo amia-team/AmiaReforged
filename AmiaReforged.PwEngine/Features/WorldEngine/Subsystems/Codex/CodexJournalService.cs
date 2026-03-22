a23f522e12e84609661438ad22fe701e40c7eca8
Caelan L. Talmadge <talmadge12@gmail.com>
2026-03-22 10:03:56 -0500

    Add more tracing

--- AmiaReforged.PwEngine/Features/WorldEngine/Subsystems/Codex/CodexJournalService.cs
+++ AmiaReforged.PwEngine/Features/WorldEngine/Subsystems/Codex/CodexJournalService.cs
@@ -43,8 +43,10 @@ public class CodexJournalService
         if (gameObject is null)
         {
             Log.Info("Creature somehow null????");
+            return ScriptHandleResult.Handled;
         }
 
+        NWScript.SpeakString("I am going insane!!!!");
         if (gameObject is not NwCreature creature)
         {
             Log.Info("Somehow, a non-creature opened the journal????");
