1b3ac1f30803f701e8454f83c636cf2ebbd42fc8
Caelan L. Talmadge <talmadge12@gmail.com>
2026-03-23 14:57:28 -0500

    Fix row version w/ Migration

---
+++ AmiaReforged.PwEngine/Migrations/20260323195630_FixRowVersion.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AmiaReforged.PwEngine.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixRowVersion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "row_version",
+                table: "dialogue_trees");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<uint>(
+                name: "row_version",
+                table: "dialogue_trees",
+                type: "xid",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: 0u);
+        }
+    }
+}
