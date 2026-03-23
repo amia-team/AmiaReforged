4681c87a528d5bc200b428b7ba952f550822f444
Caelan L. Talmadge <talmadge12@gmail.com>
2026-03-23 10:12:29 -0500

    Add CompletionRewardJson property to CodexQuestDefinitions for quest tracking

---
+++ AmiaReforged.PwEngine/Migrations/20260323151156_AddQuestCompletionTracking.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AmiaReforged.PwEngine.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddQuestCompletionTracking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CompletionRewardJson",
+                table: "CodexQuestDefinitions",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CompletionRewardJson",
+                table: "CodexQuestDefinitions");
+        }
+    }
+}
