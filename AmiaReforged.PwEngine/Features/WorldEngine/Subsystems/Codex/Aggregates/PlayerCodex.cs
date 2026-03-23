115df4be9b2a4600a37aa1af294bc16f0a1c943e
Caelan L. Talmadge <talmadge12@gmail.com>
2026-03-23 11:58:04 -0500

    Delete old Codex reference

--- AmiaReforged.PwEngine/Features/WorldEngine/Subsystems/Codex/Aggregates/PlayerCodex.cs
+++
@@ -1,5 +0,0 @@
-// This file has been moved to Domain/Aggregates/PlayerCodex.cs
-// Please update your using statements to:
-// using AmiaReforged.PwEngine.Features.WorldEngine.Subsystems.Codex.Domain.Aggregates;
-//
-// This file can be safely deleted.
