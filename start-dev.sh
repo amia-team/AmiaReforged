3460def0510013ad3de024c624dad2eafc14dae8
Caelan L. Talmadge <talmadge12@gmail.com>
2026-03-25 14:57:42 -0500

    Update server directory variable to use DEV_SERVER_BASE in start-dev.sh and stop-dev.sh

--- start-dev.sh
+++ start-dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Restarts the dev server
 # Uses AMIA_SERVER_DIR environment variable if set, otherwise defaults to /storage/amiadev
-SERVER_DIR="${AMIA_SERVER_DIR:-/storage/amiadev}"
+SERVER_DIR="${DEV_SERVER_BASE:-/storage/amiadev}"
 pushd "$SERVER_DIR" || exit
 docker compose up -d dev-server
 popd || exit
