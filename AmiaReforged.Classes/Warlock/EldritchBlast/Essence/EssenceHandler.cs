bd577420052e9e832539437d273495effad148b3
Mitro Stönky <90032996+Opustus@users.noreply.github.com>
2026-05-22 06:04:02 +0300

    Add Hideous Blow vfx toggle to essence handler

--- AmiaReforged.Classes/Warlock/EldritchBlast/Essence/EssenceHandler.cs
+++ AmiaReforged.Classes/Warlock/EldritchBlast/Essence/EssenceHandler.cs
@@ -1,8 +1,8 @@
+using AmiaReforged.Classes.Warlock.EldritchBlast.Shape;
 using Anvil.API;
 using Anvil.API.Events;
 using Anvil.Services;
 using NLog;
-using NWN.Core.NWNX;
 
 namespace AmiaReforged.Classes.Warlock.EldritchBlast.Essence;
 
@@ -12,9 +12,11 @@ public class EssenceHandler
     private const string EssenceVar = "warlock_essence";
     private const int RemoveEssenceId = 1299;
     private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+    private static EssenceFactory? _essenceFactory;
 
-    public EssenceHandler()
+    public EssenceHandler(EssenceFactory essenceFactory)
     {
+        _essenceFactory = essenceFactory;
         NwModule.Instance.OnUseFeat += OnRemoveEldritchEssence;
         NwModule.Instance.OnSpellAction += OnEldritchEssence;
         Log.Info(message: "Warlock Essence Handler initialized.");
@@ -34,15 +36,41 @@ public class EssenceHandler
         int spellId = eventData.Spell.Id;
         if (!Enum.IsDefined(typeof(EssenceType), spellId)) return;
 
-        eventData.Caster.GetObjectVariable<LocalVariableInt>(EssenceVar).Value = spellId;
+        NwCreature warlock = eventData.Caster;
 
-        if (!eventData.Caster.IsPlayerControlled(out NwPlayer? player)) return;
+        warlock.GetObjectVariable<LocalVariableInt>(EssenceVar).Value = spellId;
+
+        if (!warlock.IsPlayerControlled(out NwPlayer? player)) return;
         string essenceName = eventData.Spell.Name.ToString();
 
         player.SendServerMessage($"{essenceName} applied.".ColorWarlock());
         player.FloatingTextString($"*{essenceName} Activated*".ColorWarlock(), false, false);
 
         eventData.PreventSpellCast = true;
+
+        if (eventData.Caster.GetItemInSlot(InventorySlot.RightHand) is not { } weapon
+            || weapon.ItemProperties.All(ip => ip.Tag != nameof(ShapeType.HideousBlow)))
+            return;
+
+        EssenceData? essence = _essenceFactory?.GetEssenceData(warlock, warlock.GetInvocationCasterLevel());
+        if (essence == null) return;
+        ChangeHideousBlowGlow(weapon, essence.Value);
+    }
+
+    /// <summary>
+    /// Hideous Blow recharges every two rounds, and it's possible to change the essence on the fly, so we have to
+    /// change the visual effect to match that
+    /// </summary>
+    private static void ChangeHideousBlowGlow(NwItem weapon, EssenceData essence)
+    {
+        foreach (ItemProperty ip in weapon.ItemProperties)
+        {
+            if (ip is { Tag: nameof(ShapeType.HideousBlow), Property.PropertyType: ItemPropertyType.VisualEffect })
+                weapon.RemoveItemProperty(ip);
+        }
+        ItemProperty weaponGlow = ItemProperty.VisualEffect(essence.HideousBlowVfx);
+        weaponGlow.Tag = nameof(ShapeType.HideousBlow);
+        weapon.AddItemProperty(weaponGlow, EffectDuration.Temporary, TimeSpan.FromHours(8));
     }
 }
 
