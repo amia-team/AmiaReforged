5bff981a661c18e113317ec044e1317a51ff0d9f
Mitro Stönky <90032996+Opustus@users.noreply.github.com>
2026-06-07 23:04:21 +0300

    Fix Wis AB giving one less than Dex/Str

--- AmiaReforged.Classes/Monk/WisdomAttackBonus.cs
+++ AmiaReforged.Classes/Monk/WisdomAttackBonus.cs
@@ -8,20 +8,23 @@ public static class WisdomAttackBonus
     public static async Task AdjustWisdomAttackBonus(NwCreature monk)
     {
         await NwTask.Delay(TimeSpan.FromMilliseconds(10));
+        await monk.WaitForObjectContext();
         UnsetWisdomAttackBonus(monk);
 
-        if (monk.IsRangedWeaponEquipped || MonkUtils.AbilityRestricted(monk, "Floating Leaf attack bonus"))
+        if (monk.IsRangedWeaponEquipped || MonkUtils.AbilityRestricted(monk, disabledWhat: "Floating Leaf attack bonus"))
             return;
 
         int wisModifier = monk.GetAbilityModifier(Ability.Wisdom);
         int strModifier =  monk.GetAbilityModifier(Ability.Strength);
         int dexModifier =  monk.GetAbilityModifier(Ability.Dexterity);
 
-        if (wisModifier <= strModifier || monk.FinesseApplies(wisModifier, dexModifier)) return;
+        int currentAttackModifier = monk.FinesseApplies(strModifier, dexModifier)
+            ? dexModifier
+            : strModifier;
 
-        int wisAttackBonus = monk.FinesseApplies(strModifier, dexModifier)
-                ? wisModifier - dexModifier
-                : wisModifier - strModifier;
+        if (wisModifier <= currentAttackModifier) return;
+
+        int wisAttackBonus = wisModifier - currentAttackModifier;
 
         Effect wisAbEffect = Effect.AttackIncrease(wisAttackBonus);
         wisAbEffect.SubType = EffectSubType.Unyielding;
@@ -34,16 +37,13 @@ public static class WisdomAttackBonus
     {
         NwItem? weapon = monk.GetItemInSlot(InventorySlot.RightHand);
         bool hasFinesseWeapon = weapon is null || monk.Size <= weapon.BaseItem.WeaponFinesseMinimumCreatureSize;
-
         return hasFinesseWeapon && abilityModifier <= dexModifier;
     }
 
     private static void UnsetWisdomAttackBonus(NwCreature monk)
     {
         Effect? existingAbEffect = monk.ActiveEffects.FirstOrDefault(e => e.Tag == WisdomAbTag);
-
         if (existingAbEffect == null) return;
-
         monk.RemoveEffect(existingAbEffect);
     }
 }
