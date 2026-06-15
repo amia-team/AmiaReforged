0114e906a5b863aea26577d2e9629bfc9b39ed15
Mitro Stönky <90032996+Opustus@users.noreply.github.com>
2026-06-15 23:48:48 +0300

    Add NwColors constants

---
+++ AmiaReforged.Classes/EffectUtils/NwColors.cs
@@ -0,0 +1,11 @@
+using Anvil.API;
+
+namespace AmiaReforged.Classes.EffectUtils;
+
+public static class NwColors
+{
+    public static readonly Color CombatOrange = new(255, 102, 1);
+    public static readonly Color NameCyan = new(153, 255, 255);
+    public static readonly Color NamePurple = new(204, 153, 204);
+    public static readonly Color MagicPurple = new(204, 119, 255);
+}
