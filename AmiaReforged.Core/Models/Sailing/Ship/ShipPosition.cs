511eb3cde2560564da423922462689429410dd27
Mitro Stönky <90032996+Opustus@users.noreply.github.com>
2026-08-27 17:46:45 +0300

    Create ShipPosition

--- AmiaReforged.Core/Models/Sailing/Ship/ShipPosition.cs
+++ AmiaReforged.Core/Models/Sailing/Ship/ShipPosition.cs
@@ -1,6 +1,15 @@
 namespace AmiaReforged.Core.Models.Sailing.Ship;
 
-public class ShipPosition
+public sealed record ShipPosition(string AreaResRef, float X, float Y, float Z)
 {
-    
+    /// <summary>
+    /// The sailing area containing the ship.
+    /// </summary>
+    public string AreaResRef { get; } = AreaResRef;
+
+    public float X { get; } = X;
+
+    public float Y { get; } = Y;
+
+    public float Z { get; } = Z;
 }
