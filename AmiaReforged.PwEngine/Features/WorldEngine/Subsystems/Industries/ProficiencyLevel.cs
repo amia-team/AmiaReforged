6e5733155fe7c7346145609832dd8ac3c2055ab7
Caelan Talmadge <caelan.talmadge@gmail.com>
2026-09-24 16:13:08 -0500

    Update ProficiencyLevel.cs

--- AmiaReforged.PwEngine/Features/WorldEngine/Subsystems/Industries/ProficiencyLevel.cs
+++ AmiaReforged.PwEngine/Features/WorldEngine/Subsystems/Industries/ProficiencyLevel.cs
@@ -2,7 +2,9 @@ namespace AmiaReforged.PwEngine.Features.WorldEngine.Subsystems.Industries;
 
 public enum ProficiencyLevel
 {
+    // Layman is the default non-professional state for every industry.
     Layman = -1,
+    // Novice is the default entry state when joining an industry
     Novice = 0,
     Apprentice = 1,
     Journeyman = 2,
